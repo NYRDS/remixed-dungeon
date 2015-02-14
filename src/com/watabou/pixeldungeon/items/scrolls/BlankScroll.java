5431a74858e52d0cf5df6e6ea2bcfa2f44b904cc
mdanilov <mdanilov@3detection.ru>
2015-02-14 19:34:56 +0300

    BlankScroll

---
+++ src/com/watabou/pixeldungeon/items/scrolls/BlankScroll.java
@@ -0,0 +1,20 @@
+package com.watabou.pixeldungeon.items.scrolls;
+
+import com.watabou.pixeldungeon.items.Item;
+import com.watabou.pixeldungeon.sprites.ItemSpriteSheet;
+
+public class BlankScroll extends Item {
+	{
+		image = ItemSpriteSheet.SCROLL_BLANK;
+	}
+	
+	@Override
+	public boolean isUpgradable() {
+		return false;
+	}
+	
+	@Override
+	public boolean isIdentified() {
+		return true;
+	}
+}
