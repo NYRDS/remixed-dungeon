93bdc7f21944329b5c78327a4b4639a666ea2d43
Mikhael-Danilov <mikhael.danilov@gmail.com>
2015-03-15 14:55:27 +0300

    Oleg Dolya has no relation to this file

--- src/com/watabou/pixeldungeon/items/rings/RingOfStoneWalking.java
+++ src/com/watabou/pixeldungeon/items/rings/RingOfStoneWalking.java
@@ -1,25 +1,17 @@
-/*
- * Pixel Dungeon
- * Copyright (C) 2012-2014  Oleg Dolya
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
- */
 package com.watabou.pixeldungeon.items.rings;
 
-import com.watabou.pixeldungeon.sprites.ItemSpriteSheet;
+import android.R;
 
-public class RingOfStoneWalking extends Artifact {
+import com.watabou.noosa.Game;
+import com.watabou.pixeldungeon.Badges;
+import com.watabou.pixeldungeon.Dungeon;
+import com.watabou.pixeldungeon.ResultDescriptions;
+import com.watabou.pixeldungeon.actors.hero.Hero;
+import com.watabou.pixeldungeon.sprites.ItemSpriteSheet;
+import com.watabou.pixeldungeon.utils.GLog;
+import com.watabou.pixeldungeon.utils.Utils;
+
+public class RingOfStoneWalking extends Artifact{
 	{
 		image = ItemSpriteSheet.RING_OF_STONE_WALKING;
 	}
@@ -29,6 +21,15 @@ public class RingOfStoneWalking extends Artifact {
 		return new StoneWalking();
 	}
 	
-	public class StoneWalking extends ArtifactBuff {
+	public class StoneWalking extends ArtifactBuff implements Hero.Doom{
+
+		@Override
+		public void onDeath() {
+			Badges.validateDeathInStone();
+			
+			Dungeon.fail( Utils.format( ResultDescriptions.IMMURED, Dungeon.depth ) );
+			GLog.n( Game.getVar(R.string.RingOfStoneWalking_ImmuredInStone));
+			
+		}
 	}
 }
