a4d667c3e6c3ea46e06ca5dc9174ee98656d49a1
Mikhael-Danilov <mikhael.danilov@gmail.com>
2015-04-05 22:49:24 +0300

    cleanup

--- src/com/watabou/pixeldungeon/items/TomeOfMastery.java
+++ src/com/watabou/pixeldungeon/items/TomeOfMastery.java
@@ -19,14 +19,12 @@ package com.watabou.pixeldungeon.items;
 
 import java.util.ArrayList;
 
+import com.nyrds.pixeldungeon.ml.R;
 import com.watabou.noosa.Game;
 import com.watabou.noosa.audio.Sample;
 import com.watabou.pixeldungeon.Assets;
 import com.watabou.pixeldungeon.Badges;
-import com.nyrds.pixeldungeon.ml.R;
 import com.watabou.pixeldungeon.actors.buffs.Blindness;
-import com.watabou.pixeldungeon.actors.buffs.Buff;
-import com.watabou.pixeldungeon.actors.buffs.Fury;
 import com.watabou.pixeldungeon.actors.hero.Hero;
 import com.watabou.pixeldungeon.actors.hero.HeroSubClass;
 import com.watabou.pixeldungeon.effects.Speck;
