e4df57eb96925e1b9e8452487d278ac31f0743e0
Mikhael-Danilov <mikhael.danilov@gmail.com>
2015-04-11 20:45:24 +0300

    donation window

--- src/com/watabou/pixeldungeon/windows/WndDonate.java
+++ src/com/watabou/pixeldungeon/windows/WndDonate.java
@@ -11,126 +11,92 @@ import com.watabou.pixeldungeon.ui.RedButton;
 import com.watabou.pixeldungeon.windows.elements.RankingTab;
 import com.watabou.pixeldungeon.windows.elements.Tab;
 
+public class WndDonate extends WndTabbed {
 
-public class WndDonate extends WndTabbed  {
-
-	private static final String RUBY   = Game.getVar(R.string.WndDonate_ruby);
-	private static final String GOLD   = Game.getVar(R.string.WndDonate_gold);
+	private static final String RUBY = Game.getVar(R.string.WndDonate_ruby);
+	private static final String GOLD = Game.getVar(R.string.WndDonate_gold);
 	private static final String SILVER = Game.getVar(R.string.WndDonate_silver);
-	
-	private static final String RUBY_DONATE   = Game.getVar(R.string.WndDonate_rubyDonate);
-	private static final String GOLD_DONATE   = Game.getVar(R.string.WndDonate_goldDonate);
-	private static final String SILVER_DONATE = Game.getVar(R.string.WndDonate_silverDonate);
-	
-	private static final String SILVER_DONATE_TEXT = Game.getVar(R.string.WndDonate_silverDonateText);
-	private static final String GOLD_DONATE_TEXT   = Game.getVar(R.string.WndDonate_goldDonateText);
-	private static final String RUBY_DONATE_TEXT   = Game.getVar(R.string.WndDonate_rubyDonateText);
-	
-	private static final int WIDTH      = 112;
+
+	private static final String DONATE = Game.getVar(R.string.WndDonate_donate);
+
+	private static final String RUBY_DONATE = Game
+			.getVar(R.string.WndDonate_rubyDonate);
+	private static final String GOLD_DONATE = Game
+			.getVar(R.string.WndDonate_goldDonate);
+	private static final String SILVER_DONATE = Game
+			.getVar(R.string.WndDonate_silverDonate);
+
+	private static final String SILVER_DONATE_TEXT = Game
+			.getVar(R.string.WndDonate_silverDonateText);
+	private static final String GOLD_DONATE_TEXT = Game
+			.getVar(R.string.WndDonate_goldDonateText);
+	private static final String RUBY_DONATE_TEXT = Game
+			.getVar(R.string.WndDonate_rubyDonateText);
+
+	private static final int WIDTH = 111;
+	private static final int HEIGHT = 111;
+
 	private static final int BTN_HEIGHT = 20;
-	private static final int GAP        = 2;
-	private static final int TAB_WIDTH  = 35;
-
-	public void  createTabs() {
-
-		String[] labels = 
-			{SILVER, GOLD, RUBY};
-		Group[] pages = 
-			{new SilverTab(), new SilverTab(), new SilverTab()};
-		
-		for (int i=0; i < pages.length; i++) {
-			
-			add( pages[i] );
-			
-			Tab tab = new RankingTab(this, labels[i], pages[i] );
-			tab.setSize( TAB_WIDTH , tabHeight() );
-			add( tab );
-		}
-	}
+	private static final int BTN_WIDTH  = 60;
 	
+	private static final int GAP = 2;
+	private static final int TAB_WIDTH = 41;
+
+	private static final Icons[] icons = { Icons.CHEST_SILVER,
+			Icons.CHEST_GOLD, Icons.CHEST_RUBY };
+	private static final String[] title = { SILVER_DONATE, GOLD_DONATE,
+			RUBY_DONATE };
+	private static final String[] text = { SILVER_DONATE_TEXT,
+			GOLD_DONATE_TEXT, RUBY_DONATE_TEXT };
+
 	public WndDonate() {
 		super();
+		String[] labels = { SILVER, GOLD, RUBY };
+		Group[] pages = { new DonateTab(1), new DonateTab(2), new DonateTab(3) };
 
-		int donationLevel = PixelDungeon.donated();
-		/*
-		RedButton silverDonate = new RedButton(SILVER_DONATE) {
-			@Override
-			protected void onClick() {
-				//PixelDungeon.donate(1);
-			}
-		};
-		add(silverDonate.setRect(0, 0, WIDTH, BTN_HEIGHT));
-		
-		if(donationLevel > 0){
-			silverDonate.enable(false);
-		}
-		
-		RedButton goldDonate = new RedButton(GOLD_DONATE) {
-			@Override
-			protected void onClick() {
-				//PixelDungeon.donate(2);
-			}
-		};
-		add(goldDonate.setRect(0, silverDonate.bottom() + GAP, WIDTH,
-				BTN_HEIGHT));
+		for (int i = 0; i < pages.length; i++) {
+			add(pages[i]);
 
-		if(donationLevel > 1){
-			silverDonate.enable(false);
+			Tab tab = new RankingTab(this, labels[i], pages[i]);
+			tab.setSize(TAB_WIDTH, tabHeight());
+			add(tab);
 		}
-		
-		RedButton rubyDonate = new RedButton(RUBY_DONATE) {
-			@Override
-			protected void onClick() {
-				//PixelDungeon.donate(3);
-			}
-		};
-		add(rubyDonate.setRect(0, goldDonate.bottom() + GAP, WIDTH, BTN_HEIGHT));
 
-		if(donationLevel > 2){
-			goldDonate.enable(false);
-		}
-		
-		resize(WIDTH, (int) rubyDonate.bottom());
-		*/
-		createTabs();
-		resize(WIDTH, WIDTH);
+		resize(HEIGHT, WIDTH);
+
+		select(0);
 	}
-	
-	private class SilverTab extends Group {
-		
-		private static final int GAP	= 4;
-		
-		public SilverTab() {
-			super();
-			
-			IconTitle title = new IconTitle(Icons.get(Icons.CHEST_SILVER), SILVER_DONATE);
-			title.setRect( 0, 0, WIDTH, 0 );
-			add( title );
-			
-			float pos = title.bottom();
-			
-			pos += GAP;
-			
-			BitmapTextMultiline text = PixelScene.createMultiline( SILVER_DONATE_TEXT, 7 );
-			text.measure();
-			text.setPos(0,pos);
-			add(text);
-			
-			pos += text.height() + GAP;
-			
-			RedButton silverDonate = new RedButton(SILVER_DONATE) {
-				@Override
-				protected void onClick() {
-					//PixelDungeon.donate(1);
-				}
-			};
-			add(silverDonate.setRect(0, pos, WIDTH, BTN_HEIGHT));
-			
-			if(PixelDungeon.donated() > 0){
-				silverDonate.enable(false);
-			}
-		}
 
+	private class DonateTab extends Group {
+
+		public DonateTab(int level) {
+			super();
+
+			IconTitle tabTitle = new IconTitle(Icons.get(icons[level - 1]),
+					title[level - 1]);
+			tabTitle.setRect(0, 0, WIDTH, 0);
+			add(tabTitle);
+
+			float pos = tabTitle.bottom();
+
+			pos += GAP;
+
+			if (PixelDungeon.donated() < level) {
+				RedButton donate = new RedButton(DONATE) {
+					@Override
+					protected void onClick() {
+						// PixelDungeon.donate(level);
+					}
+				};
+				add(donate.setRect(WIDTH - BTN_WIDTH ,HEIGHT - BTN_HEIGHT, BTN_WIDTH, BTN_HEIGHT));
+			}
+
+			BitmapTextMultiline tabText = PixelScene.createMultiline(
+					text[level - 1], 7);
+			tabText.measure();
+			tabText.setPos(0, pos);
+			add(tabText);
+		}
 	}
 
 }
