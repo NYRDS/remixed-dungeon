9e6c004d5db58312a430cd05b2335ac62534b4e0
Mikhael-Danilov <mikhael.danilov@gmail.com>
2015-05-17 21:08:44 +0300

    adjust servant anim

--- src/com/nyrds/pixeldungeon/mobs/spiders/sprites/SpiderServantSprite.java
+++ src/com/nyrds/pixeldungeon/mobs/spiders/sprites/SpiderServantSprite.java
@@ -19,14 +19,14 @@ public class SpiderServantSprite extends MobSprite {
 		idle = new Animation( 5, true );
 		idle.frames(shift, frames, 0, 1, 0, 2 );
 		
-		run = new Animation( 15, true );
-		run.frames(shift, frames, 3, 4, 5, 6 );
+		run = new Animation( 10, true );
+		run.frames(shift, frames, 3, 4, 5 );
 		
 		attack = new Animation( 12, false );
-		attack.frames(shift, frames, 7, 8, 9 );
+		attack.frames(shift, frames, 6, 7, 8 );
 		
-		die = new Animation( 12, false );
-		die.frames(shift, frames, 10, 11, 12, 13 );
+		die = new Animation( 15, false );
+		die.frames(shift, frames, 0, 9, 10, 11, 12, 13 );
 		
 		play( idle );
 	}
