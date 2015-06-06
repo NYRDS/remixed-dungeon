3bae536df451f54d18260ab147b0d6f495445642
Mikhael-Danilov <mikhael.danilov@gmail.com>
2015-06-07 00:03:41 +0300

    air & earth elemental's

---
+++ src/com/watabou/pixeldungeon/actors/mobs/AirElemental.java
@@ -0,0 +1,84 @@
+package com.watabou.pixeldungeon.actors.mobs;
+
+import java.util.HashSet;
+
+import com.watabou.pixeldungeon.Dungeon;
+import com.watabou.pixeldungeon.actors.Char;
+import com.watabou.pixeldungeon.actors.blobs.Freezing;
+import com.watabou.pixeldungeon.actors.buffs.Buff;
+import com.watabou.pixeldungeon.actors.buffs.Burning;
+import com.watabou.pixeldungeon.actors.buffs.Frost;
+import com.watabou.pixeldungeon.effects.Speck;
+import com.watabou.pixeldungeon.items.potions.PotionOfFrost;
+import com.watabou.pixeldungeon.items.scrolls.ScrollOfPsionicBlast;
+import com.watabou.pixeldungeon.levels.Level;
+import com.watabou.pixeldungeon.sprites.AirElementalSprite;
+import com.watabou.utils.Random;
+
+public class AirElemental extends Mob {
+
+	public AirElemental() {
+		spriteClass = AirElementalSprite.class;
+		
+		adjustLevel(Dungeon.depth);
+		
+		loot = new PotionOfFrost();
+		lootChance = 0.1f;
+	}
+	
+	private void adjustLevel(int depth) {
+		hp(ht(depth * 5));
+		defenseSkill = depth * 2;
+		EXP = depth;
+		maxLvl = depth + 2;
+		
+		
+	}
+	
+	@Override
+	public int damageRoll() {
+		return Random.NormalIntRange( hp() / 2, ht() / 2 );
+	}
+	
+	@Override
+	public int attackSkill( Char target ) {
+		return defenseSkill / 2;
+	}
+	
+	@Override
+	public int dr() {
+		return 5;
+	}
+	
+	@Override
+	public int attackProc( Char enemy, int damage ) {
+		/*
+		if (Random.Int( 2 ) == 0) {
+			Freezing.affect( enemy.pos, null );
+		}
+		*/
+		return damage;
+	}
+	
+	@Override
+	public boolean act() {
+		/*
+		if (Level.water[pos] && hp() < ht()) {
+			getSprite().emitter().burst( Speck.factory( Speck.HEALING ), 1 );
+			hp(hp() + EXP);
+		}
+		*/
+		return super.act();
+	}
+	
+	private static final HashSet<Class<?>> IMMUNITIES = new HashSet<Class<?>>();
+	static {
+		IMMUNITIES.add( Frost.class );
+		IMMUNITIES.add( ScrollOfPsionicBlast.class );
+	}
+	
+	@Override
+	public HashSet<Class<?>> immunities() {
+		return IMMUNITIES;
+	}
+}
