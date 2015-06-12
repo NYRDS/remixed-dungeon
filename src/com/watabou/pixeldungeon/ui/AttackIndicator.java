85d0bb707a5c5727b00e8a5af6ac6b20001a61e8
Mikhael-Danilov <mikhael.danilov@gmail.com>
2015-06-12 23:52:07 +0300

    refactor

--- src/com/watabou/pixeldungeon/ui/AttackIndicator.java
+++ src/com/watabou/pixeldungeon/ui/AttackIndicator.java
@@ -118,7 +118,7 @@ public class AttackIndicator extends Tag {
 		}
 		
 		try {
-			sprite = lastTarget.spriteClass.newInstance();
+			sprite = lastTarget.sprite();
 			sprite.idle();
 			sprite.paused = true;
 			add( sprite );
