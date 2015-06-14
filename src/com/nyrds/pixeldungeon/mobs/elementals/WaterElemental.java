9f7267c270979ab7bb94870e69f916c136133045
Mikhael-Danilov <mikhael.danilov@gmail.com>
2015-06-14 19:12:59 +0300

    Fix water elemental overhealing

--- src/com/nyrds/pixeldungeon/mobs/elementals/WaterElemental.java
+++ src/com/nyrds/pixeldungeon/mobs/elementals/WaterElemental.java
@@ -62,7 +62,7 @@ public class WaterElemental extends Mob {
 	public boolean act() {
 		if (Level.water[pos] && hp() < ht()) {
 			getSprite().emitter().burst( Speck.factory( Speck.HEALING ), 1 );
-			hp(hp() + EXP);
+			hp(Math.max(hp() + EXP,ht()));
 		}
 		
 		return super.act();
