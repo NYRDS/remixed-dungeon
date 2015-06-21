78b1515542f4c6ad258d35448cf683900e726845
Mikhael-Danilov <mikhael.danilov@gmail.com>
2015-06-22 01:09:43 +0300

    prepare for beta

--- src/com/watabou/pixeldungeon/actors/mobs/Bestiary.java
+++ src/com/watabou/pixeldungeon/actors/mobs/Bestiary.java
@@ -17,6 +17,9 @@
  */
 package com.watabou.pixeldungeon.actors.mobs;
 
+import com.nyrds.pixeldungeon.mobs.elementals.AirElemental;
+import com.nyrds.pixeldungeon.mobs.elementals.EarthElemental;
+import com.nyrds.pixeldungeon.mobs.elementals.WaterElemental;
 import com.watabou.pixeldungeon.actors.Char;
 import com.watabou.utils.Random;
 
@@ -172,8 +175,11 @@ public class Bestiary {
 			break;
 			
 		default:
-			chances = new float[]{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
-			classes = new Class<?>[]{ Rat.class, Gnoll.class, Crab.class, Swarm.class, Skeleton.class, Thief.class, Swarm.class, Shadow.class, Bat.class, Brute.class, Spinner.class, Elemental.class, Monk.class, Golem.class, Warlock.class, Succubus.class, Eye.class, Scorpio.class  };
+			//chances = new float[]{ 1,1 };
+			//classes = new Class<?>[]{ Shaman.class, Warlock.class  };
+
+			chances = new float[]{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+			classes = new Class<?>[]{ Rat.class, Gnoll.class, Crab.class, Swarm.class, Skeleton.class, Thief.class, Swarm.class, Shadow.class, Bat.class, Brute.class, Spinner.class, Elemental.class, Monk.class, Golem.class, Warlock.class, Succubus.class, Eye.class, Scorpio.class, AirElemental.class, WaterElemental.class, EarthElemental.class  };
 		}
 		
 		return classes[ Random.chances( chances )];
