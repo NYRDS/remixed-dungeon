473489edb57816b78dd7b46c332efc4707921a38
Mikhael-Danilov <mikhael.danilov@gmail.com>
2015-09-16 22:20:17 +0300

    revert FloatingText add SystemFloatingText

--- src/com/watabou/pixeldungeon/effects/SystemFloatingText.java
+++ src/com/watabou/pixeldungeon/effects/SystemFloatingText.java
@@ -1,31 +1,16 @@
-/*
- * Pixel Dungeon
- * Copyright (C) 2012-2014  Oleg Dolya
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
- */
 package com.watabou.pixeldungeon.effects;
 
 import java.util.ArrayList;
+
 import com.watabou.noosa.BitmapText;
 import com.watabou.noosa.Game;
+import com.watabou.noosa.SystemText;
 import com.watabou.pixeldungeon.DungeonTilemap;
 import com.watabou.pixeldungeon.scenes.GameScene;
 import com.watabou.pixeldungeon.scenes.PixelScene;
 import com.watabou.utils.SparseArray;
 
-public class FloatingText extends BitmapText {
+public class SystemFloatingText extends SystemText {
 
 	private static final float LIFESPAN	= 1f;
 	private static final float DISTANCE	= DungeonTilemap.SIZE;
@@ -34,15 +19,14 @@ public class FloatingText extends BitmapText {
 	
 	private int key = -1;
 	
-	private static SparseArray<ArrayList<FloatingText>> stacks = new SparseArray<ArrayList<FloatingText>>();
+	private static SparseArray<ArrayList<SystemFloatingText>> stacks = new SparseArray<ArrayList<SystemFloatingText>>();
 	
-	public FloatingText() {
-		PixelScene.chooseFont( 9 );
-		font = PixelScene.font();
-		scale.set( PixelScene.scale );
+	public SystemFloatingText() {
+		super(null,9,false);
 		
 		speed.y = - DISTANCE / LIFESPAN;
 	}
+	
 	
 	@Override
 	public void update() {
@@ -94,26 +78,26 @@ public class FloatingText extends BitmapText {
 	}
 	
 	public static void show( float x, float y, int key, String text, int color ) {
-		FloatingText txt = GameScene.status();
+		SystemFloatingText txt = GameScene.status();
 		txt.reset( x,  y,  text, color );
 		push( txt, key );
 	}
 	
-	private static void push( FloatingText txt, int key ) {
+	private static void push( SystemFloatingText txt, int key ) {
 		
 		txt.key = key;
 		
-		ArrayList<FloatingText> stack = stacks.get( key );
+		ArrayList<SystemFloatingText> stack = stacks.get( key );
 		if (stack == null) {
-			stack = new ArrayList<FloatingText>();
+			stack = new ArrayList<SystemFloatingText>();
 			stacks.put( key, stack );
 		}
 		
 		if (stack.size() > 0) {
-			FloatingText below = txt;
+			SystemFloatingText below = txt;
 			int aboveIndex = stack.size() - 1;
 			while (aboveIndex >= 0) {
-				FloatingText above = stack.get( aboveIndex );
+				SystemFloatingText above = stack.get( aboveIndex );
 				if (above.y + above.height() > below.y) {
 					above.y = below.y - above.height();
 					
