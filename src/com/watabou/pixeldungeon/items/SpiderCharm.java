66e83b1dec20950b249c55a4e14dc06be4446229
Mikhael-Danilov <mikhael.danilov@gmail.com>
2015-10-18 19:03:55 +0300

    SpiderCharm use

--- src/com/watabou/pixeldungeon/items/SpiderCharm.java
+++ src/com/watabou/pixeldungeon/items/SpiderCharm.java
@@ -2,27 +2,37 @@ package com.watabou.pixeldungeon.items;
 
 import java.util.ArrayList;
 
+import com.nyrds.pixeldungeon.ml.R;
 import com.nyrds.pixeldungeon.mobs.spiders.SpiderServant;
+import com.watabou.noosa.Game;
+import com.watabou.noosa.audio.Sample;
+import com.watabou.pixeldungeon.Assets;
 import com.watabou.pixeldungeon.Dungeon;
 import com.watabou.pixeldungeon.actors.Actor;
 import com.watabou.pixeldungeon.actors.hero.Hero;
 import com.watabou.pixeldungeon.actors.mobs.Mob;
 import com.watabou.pixeldungeon.effects.Pushing;
+import com.watabou.pixeldungeon.effects.Speck;
 import com.watabou.pixeldungeon.effects.Wound;
 import com.watabou.pixeldungeon.items.rings.Artifact;
 import com.watabou.pixeldungeon.levels.Level;
 import com.watabou.pixeldungeon.scenes.GameScene;
 import com.watabou.pixeldungeon.sprites.ItemSprite.Glowing;
+import com.watabou.pixeldungeon.sprites.CharSprite;
 import com.watabou.pixeldungeon.sprites.ItemSpriteSheet;
+import com.watabou.pixeldungeon.utils.GLog;
 import com.watabou.utils.Random;
 
 public class SpiderCharm extends Artifact {
 	
 	public static final float TIME_TO_USE = 1;
+	public static final String AC_USE = Game.getVar(R.string.SpiderCharm_Use);
 	
 	public SpiderCharm() {
 		image = ItemSpriteSheet.SPIDER_CHARM;
 		unique = true;
+		
+		
 	}
 
 	@Override
@@ -38,28 +48,39 @@ public class SpiderCharm extends Artifact {
 	}
 	
 	@Override
-	public boolean doEquip(Hero ch) {
-		boolean ret = super.doEquip(ch);
-		
-		Wound.hit(ch);
-		ch.damage(ch.ht()/4, this);
-		
-		ArrayList<Integer> spawnPoints = new ArrayList<Integer>();
-		
-		for (int i=0; i < Level.NEIGHBOURS8.length; i++) {
-			int p = ch.pos + Level.NEIGHBOURS8[i];
-			if (Actor.findChar( p ) == null && (Dungeon.level.passable[p] || Dungeon.level.avoid[p])) {
-				spawnPoints.add( p );
-			}
-		}
-		
-		if (spawnPoints.size() > 0) {
-			Mob pet = Mob.makePet(new SpiderServant());
-			pet.pos = Random.element( spawnPoints );
+	public void execute( final Hero ch, String action ) {
+		if (action.equals( AC_USE )) {
+			Wound.hit(ch);
+			ch.damage(ch.ht()/4, this);
 			
-			GameScene.add(Dungeon.level, pet );
-			Actor.addDelayed( new Pushing( pet, ch.pos, pet.pos ), -1 );
+			ArrayList<Integer> spawnPoints = new ArrayList<Integer>();
+			
+			for (int i=0; i < Level.NEIGHBOURS8.length; i++) {
+				int p = ch.pos + Level.NEIGHBOURS8[i];
+				if (Actor.findChar( p ) == null && (Dungeon.level.passable[p] || Dungeon.level.avoid[p])) {
+					spawnPoints.add( p );
+				}
+			}
+			
+			if (spawnPoints.size() > 0) {
+				Mob pet = Mob.makePet(new SpiderServant());
+				pet.pos = Random.element( spawnPoints );
+				
+				GameScene.add(Dungeon.level, pet );
+				Actor.addDelayed( new Pushing( pet, ch.pos, pet.pos ), -1 );
+			}
+
+		} else {
+			
+			super.execute( ch, action );
+			
 		}
-		return ret;
+	}
+	
+	@Override
+	public ArrayList<String> actions( Hero hero ) {
+		ArrayList<String> actions = super.actions( hero );
+		actions.add( AC_USE );
+		return actions;
 	}
 }
