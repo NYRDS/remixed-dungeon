476c29b39ac70bd8d267ec31070516bf451d613b
Mikhael-Danilov <mikhael.danilov@gmail.com>
2015-10-24 12:29:23 +0300

    improve save|load window

---
+++ src/com/watabou/pixeldungeon/windows/WndOptionsColumns.java
@@ -0,0 +1,86 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2014  Oleg Dolya
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+package com.watabou.pixeldungeon.windows;
+
+import java.util.ArrayList;
+
+import com.watabou.noosa.Text;
+import com.watabou.pixeldungeon.scenes.PixelScene;
+import com.watabou.pixeldungeon.ui.RedButton;
+import com.watabou.pixeldungeon.ui.SystemRedButton;
+import com.watabou.pixeldungeon.ui.TextButton;
+import com.watabou.pixeldungeon.ui.Window;
+
+public class WndOptionsColumns extends Window {
+
+	private static final int WIDTH			= 120;
+	private static final int MARGIN 		= 2;
+	private static final int BUTTON_HEIGHT	= 20;
+	private static final int BUTTON_WIDTH	= 58;
+	
+	private ArrayList<TextButton> buttons = new ArrayList<TextButton>();
+		
+	public WndOptionsColumns( String title, String message, String... options ) {
+		super();
+		
+		Text tfTitle = PixelScene.createMultiline( title, 9 );
+		tfTitle.hardlight( TITLE_COLOR );
+		tfTitle.x = tfTitle.y = MARGIN;
+		tfTitle.maxWidth(WIDTH - MARGIN * 2);
+		tfTitle.measure();
+		add( tfTitle );
+		
+		Text tfMesage = PixelScene.createMultiline( message, 8 );
+		tfMesage.maxWidth(WIDTH - MARGIN * 2);
+		tfMesage.measure();
+		tfMesage.x = MARGIN;
+		tfMesage.y = tfTitle.y + tfTitle.height() + MARGIN;
+		add( tfMesage );
+		
+		float pos = tfMesage.y + tfMesage.height() + MARGIN;
+		
+		for (int i = 0; i < options.length / 2 + 1; i++) {
+			for(int j =0;j<2;j++) {
+				final int index = i*2+j;
+				if(!(index<options.length)) {
+					break;
+				}
+				RedButton btn = new RedButton( options[index] ) {
+					@Override
+					protected void onClick() {
+						hide();
+						onSelect( index );
+					}
+				};
+				buttons.add(btn);
+				
+				btn.setRect( MARGIN + j*(BUTTON_WIDTH+MARGIN), pos, BUTTON_WIDTH, BUTTON_HEIGHT );
+				add( btn );
+			}
+			pos += BUTTON_HEIGHT + MARGIN;
+		}
+		
+		resize( WIDTH, (int)pos );
+	}
+	
+	public void setEnabled(int i, boolean enabled) {
+		buttons.get(i).enable(enabled);
+	}
+	
+	protected void onSelect( int index ) {};
+}
