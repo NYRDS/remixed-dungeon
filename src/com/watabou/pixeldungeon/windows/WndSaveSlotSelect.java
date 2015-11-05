da84348434a0335c488727d99c1c3553e4ec542d
Mikhael-Danilov <mikhael.danilov@gmail.com>
2015-11-05 20:12:17 +0300

    better consistency for save&load

--- src/com/watabou/pixeldungeon/windows/WndSaveSlotSelect.java
+++ src/com/watabou/pixeldungeon/windows/WndSaveSlotSelect.java
@@ -8,72 +8,61 @@ import com.watabou.pixeldungeon.PixelDungeon;
 import com.watabou.pixeldungeon.SaveUtils;
 import com.watabou.pixeldungeon.ui.DonateButton;
 
-public class WndSaveSlotSelect extends WndOptionsColumns implements GameWithGoogleIap.IntersitialPoint{
-	
+public class WndSaveSlotSelect extends WndOptionsColumns implements GameWithGoogleIap.IntersitialPoint {
+
 	private boolean saving;
 	private String slot;
-	
+
 	WndSaveSlotSelect(boolean _saving) {
 		super(Game.getVar(R.string.WndSaveSlotSelect_SelectSlot), windowText(), slotInfos());
 		saving = _saving;
-		
-		if(!saving) {
-			for (int i = 0; i<10; i++) {
-				if(!SaveUtils.slotUsed(slotNameFromIndex(i), Dungeon.heroClass)) {
+
+		if (!saving) {
+			for (int i = 0; i < 10; i++) {
+				if (!SaveUtils.slotUsed(slotNameFromIndex(i), Dungeon.heroClass)) {
 					setEnabled(i, false);
 				}
 			}
 		}
-		
-		if(PixelDungeon.donated() == 0 && PixelDungeon.canDonate()) {
+
+		if (PixelDungeon.donated() == 0 && PixelDungeon.canDonate()) {
 			DonateButton btn = new DonateButton();
 			add(btn);
-			btn.setPos(width/2 - btn.width()/2, height);
-			resize(width,(int) (height+btn.height()));
+			btn.setPos(width / 2 - btn.width() / 2, height);
+			resize(width, (int) (height + btn.height()));
 		}
 	}
-	
+
 	private static String windowText() {
-		if(PixelDungeon.donated() == 0 && PixelDungeon.canDonate()) {
+		if (PixelDungeon.donated() == 0 && PixelDungeon.canDonate()) {
 			return Game.getVar(R.string.WndSaveSlotSelect_dontLike);
 		}
 		return "";
 	}
-	
+
 	private static String slotNameFromIndex(int i) {
-		return Integer.toString(i+1);
+		return Integer.toString(i + 1);
 	}
-	
+
 	private static String[] slotInfos() {
 		String[] ret = new String[10];
-		
-		for(int i = 0;i<ret.length;i++) {
+
+		for (int i = 0; i < ret.length; i++) {
 			ret[i] = SaveUtils.slotInfo(slotNameFromIndex(i), Dungeon.heroClass);
 		}
-		
+
 		return ret;
 	}
-	
+
 	@Override
-	protected void onSelect( int index ) {
+	protected void onSelect(int index) {
 		slot = slotNameFromIndex(index);
-		
+
 		final GameWithGoogleIap.IntersitialPoint returnTo = this;
-		
-		if(saving) {
-			try {
-				
-				Dungeon.saveAll();
-				SaveUtils.copySaveToSlot(slot, Dungeon.heroClass);
-				
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-		}
-		
+
 		Game.paused = true;
-		
-		if(PixelDungeon.donated() < 1) {
+
+		if (PixelDungeon.donated() < 1) {
 			Game.instance().runOnUiThread(new Runnable() {
 				@Override
 				public void run() {
@@ -91,7 +80,17 @@ public class WndSaveSlotSelect extends WndOptionsColumns implements GameWithGoog
 			@Override
 			public void run() {
 				Game.paused = false;
-				if(!saving) {
+
+				if (saving) {
+					try {
+
+						Dungeon.saveAll();
+						SaveUtils.copySaveToSlot(slot, Dungeon.heroClass);
+
+					} catch (Exception e) {
+						throw new RuntimeException(e);
+					}
+				} else {
 					SaveUtils.loadGame(slot, Dungeon.hero.heroClass);
 				}
 			}
