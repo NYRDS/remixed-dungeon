ab28022c82f56d8b44fbb7be62e329d9c61b8fd0
Mikhael-Danilov <mikhael.danilov@gmail.com>
2015-11-05 19:55:52 +0300

    ScrollOfDomination do not affect npc's

--- src/com/watabou/pixeldungeon/items/scrolls/ScrollOfDomination.java
+++ src/com/watabou/pixeldungeon/items/scrolls/ScrollOfDomination.java
@@ -8,6 +8,7 @@ import com.watabou.pixeldungeon.Dungeon;
 import com.watabou.pixeldungeon.actors.buffs.Invisibility;
 import com.watabou.pixeldungeon.actors.mobs.Boss;
 import com.watabou.pixeldungeon.actors.mobs.Mob;
+import com.watabou.pixeldungeon.actors.mobs.npcs.NPC;
 import com.watabou.pixeldungeon.effects.Flare;
 import com.watabou.utils.Random;
 
@@ -22,7 +23,7 @@ public class ScrollOfDomination extends Scroll {
 		ArrayList<Mob> mobsInSight = new ArrayList<Mob>();
 		
 		for (Mob mob : Dungeon.level.mobs.toArray( new Mob[0] )) {
-			if (Dungeon.level.fieldOfView[mob.pos] && !(mob instanceof Boss) && !mob.isPet()) {
+			if (Dungeon.level.fieldOfView[mob.pos] && !(mob instanceof Boss) && !mob.isPet() && !(mob instanceof NPC)) {
 				mobsInSight.add(mob);
 			}
 		}
