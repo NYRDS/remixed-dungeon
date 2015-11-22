9900fc34e06bbd8610a412484461c2a3e6793f86
Mikhael-Danilov <mikhael.danilov@gmail.com>
2015-11-23 00:51:44 +0300

    WelcomeScene

--- src/com/watabou/pixeldungeon/scenes/WelcomeScene.java
+++ src/com/watabou/pixeldungeon/scenes/WelcomeScene.java
@@ -15,108 +15,89 @@ import com.watabou.pixeldungeon.ui.ScrollPane;
 //TODO: update this class with relevant info as new versions come out.
 public class WelcomeScene extends PixelScene {
 
-    private static final String TTL_Welcome = Game.getVar(R.string.Welcome_Title);
+	private static final String TTL_Welcome = Game.getVar(R.string.Welcome_Title);
 
-    private static final String TXT_Welcome    = Game.getVar(R.string.Welcome_Text);
-    private static final String TXT_Welcome_19 = Game.getVar(R.string.Welcome_Text_19);
-    private static final String TXT_Welcome_20 = Game.getVar(R.string.Welcome_Text_20);
-    private static final String TXT_Welcome_20_1 = Game.getVar(R.string.Welcome_Text_20_1);
-    private static final String TXT_Welcome_21 = Game.getVar(R.string.Welcome_Text_20_2);
-    private static final String TXT_Welcome_21_1 = Game.getVar(R.string.Welcome_Text_21_1);
+	private static final String TXT_Welcome = Game.getVar(R.string.Welcome_Text);
+	private static final String TXT_Welcome_19 = Game.getVar(R.string.Welcome_Text_19);
+	private static final String TXT_Welcome_20 = Game.getVar(R.string.Welcome_Text_20);
+	private static final String TXT_Welcome_20_1 = Game.getVar(R.string.Welcome_Text_20_1);
+	private static final String TXT_Welcome_21 = Game.getVar(R.string.Welcome_Text_20_2);
+	private static final String TXT_Welcome_21_1 = Game.getVar(R.string.Welcome_Text_21_1);
+	private static final String TXT_Welcome_21_2 = Game.getVar(R.string.Welcome_Text_21_2);
 
-    
-    private static final int GAP = 4;
+	private static final int GAP = 4;
 
-    @Override
-    public void create() {
-        super.create();
+	@Override
+	public void create() {
+		super.create();
 
-        Text text    = createMultiline(TXT_Welcome, 8);
-        Text text_19 = createMultiline(TXT_Welcome_19, 8);
-        Text text_20 = createMultiline(TXT_Welcome_20, 8);
-        Text text_20_1 = createMultiline(TXT_Welcome_20_1, 8);
-        Text text_21 = createMultiline(TXT_Welcome_21, 8);
-        Text text_21_1 = createMultiline(TXT_Welcome_21_1, 8);
-        
-        Text title   = createMultiline(TTL_Welcome, 16);
+		String[] upds = { TXT_Welcome, TXT_Welcome_19, TXT_Welcome_20, TXT_Welcome_20_1, TXT_Welcome_21,
+				TXT_Welcome_21_1,TXT_Welcome_21_2 };
 
-        int w = Camera.main.width;
-        int h = Camera.main.height;
+		Text[] updTexts = new Text[upds.length];
 
-        int pw = w - 10;
-        int ph = h - 50;
+		for (int i = 0; i < upds.length; i++) {
+			updTexts[i] = createMultiline(upds[i], 8);
+		}
 
-        title.maxWidth(pw);
-        title.measure();
+		Text title = createMultiline(TTL_Welcome, 16);
 
-        title.x = align( (w - title.width()) / 2 );
-        title.y = align( 8 );
-        add( title );
+		int w = Camera.main.width;
+		int h = Camera.main.height;
 
-        NinePatch panel = Chrome.get(Chrome.Type.WINDOW);
-        panel.size( pw, ph );
-        panel.x = (w - pw) / 2;
-        panel.y = (h - ph) / 2;
-        add( panel );
+		int pw = w - 10;
+		int ph = h - 50;
 
-        ScrollPane list = new ScrollPane( new Component() );
-        add( list );
-        list.setRect(
-                panel.x + panel.marginLeft(),
-                panel.y + panel.marginTop(),
-                panel.innerWidth(),
-                panel.innerHeight());
-        list.scrollTo( 0, 0 );
+		title.maxWidth(pw);
+		title.measure();
 
-        Component content = list.content();
-        content.clear();
-        
-        text.maxWidth((int) panel.innerWidth());
-        text_19.maxWidth((int) panel.innerWidth());
-        text_20.maxWidth((int) panel.innerWidth());
-        text_20_1.maxWidth((int) panel.innerWidth());
-        text_21.maxWidth((int) panel.innerWidth());
-        text_21_1.maxWidth((int) panel.innerWidth());
-        
-        text.measure();
-        text_19.measure();
-        text_20.measure();
-        text_20_1.measure();
-        text_21.measure();
-        text_21_1.measure();
-      
-        text_19.setPos(0, text.height() + GAP);
-        text_20.setPos(0,  text.height()+text_19.height() + 2*GAP);
-        text_20_1.setPos(0,  text.height()+text_19.height() + text_20.height() + 3*GAP);
-        text_21.setPos(0,  text.height()+text_19.height() + text_20.height() + text_20_1.height() + 4*GAP);
-        text_21_1.setPos(0,  text.height()+text_19.height() + text_20.height() + text_20_1.height() +text_21.height() + 5*GAP);
-        
-        content.add(text);
-        content.add(text_19);
-        content.add(text_20);
-        content.add(text_20_1);
-        content.add(text_21);
-        content.add(text_21_1);
+		title.x = align((w - title.width()) / 2);
+		title.y = align(8);
+		add(title);
 
-        content.setSize( panel.innerWidth(), text.height() + text_19.height() + text_20.height() + text_20_1.height()+text_21.height()+text_21_1.height()+ 5*GAP );
+		NinePatch panel = Chrome.get(Chrome.Type.WINDOW);
+		panel.size(pw, ph);
+		panel.x = (w - pw) / 2;
+		panel.y = (h - ph) / 2;
+		add(panel);
 
-        RedButton okay = new RedButton("Okay!") {
-            @Override
-            protected void onClick() {
-                PixelDungeon.version(Game.versionCode);
-                Game.switchScene(TitleScene.class);
-            }
-        };
+		ScrollPane list = new ScrollPane(new Component());
+		add(list);
+		list.setRect(panel.x + panel.marginLeft(), panel.y + panel.marginTop(), panel.innerWidth(),
+				panel.innerHeight());
+		list.scrollTo(0, 0);
 
-        okay.setRect((w - pw) / 2, h - 22, pw, 18);
-        add(okay);
+		Component content = list.content();
+		content.clear();
 
-        Archs archs = new Archs();
-        archs.setSize( Camera.main.width, Camera.main.height );
-        addToBack( archs );
+		float yPos = 0;
+		for (int i = 0; i < upds.length; i++) {
+			updTexts[i].maxWidth((int) panel.innerWidth());
+			updTexts[i].measure();
 
-        fadeIn();
-    }
+			updTexts[i].setPos(0, yPos);
+			yPos += updTexts[i].height() + GAP;
+			content.add(updTexts[i]);
+
+		}
+
+		content.setSize(panel.innerWidth(), yPos);
+
+		RedButton okay = new RedButton("Okay!") {
+			@Override
+			protected void onClick() {
+				PixelDungeon.version(Game.versionCode);
+				Game.switchScene(TitleScene.class);
+			}
+		};
+
+		okay.setRect((w - pw) / 2, h - 22, pw, 18);
+		add(okay);
+
+		Archs archs = new Archs();
+		archs.setSize(Camera.main.width, Camera.main.height);
+		addToBack(archs);
+
+		fadeIn();
+	}
 }
-
-
