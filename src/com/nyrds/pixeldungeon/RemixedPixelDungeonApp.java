964b5cc32751db9307d3041218f733eafb187b37
Mikhael-Danilov <mikhael.danilov@gmail.com>
2015-12-22 21:05:40 +0300

    acra integration

---
+++ src/com/nyrds/pixeldungeon/RemixedPixelDungeonApp.java
@@ -0,0 +1,18 @@
+package com.nyrds.pixeldungeon;
+
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+import android.app.Application;
+import com.nyrds.pixeldungeon.ml.R;
+
+@ReportsCrashes(mailTo = "nyrdsofficial@gmail.com", mode = ReportingInteractionMode.TOAST, resToastText = R.string.RemixedPixelDungeonApp_sendCrash)
+public class RemixedPixelDungeonApp extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		ACRA.init(this);
+	}
+}
