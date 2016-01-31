549259a59b38491e7fdc3f004a172e2b79ea447a
Mikhael-Danilov <mikhael.danilov@gmail.com>
2016-01-31 23:02:42 +0300

    do not apply ChaosStaff specials to npcs and bosses

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/chaos/ChaosStaff.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/chaos/ChaosStaff.java
@@ -7,6 +7,7 @@ import com.watabou.pixeldungeon.actors.Actor;
 import com.watabou.pixeldungeon.actors.Char;
 import com.watabou.pixeldungeon.actors.mobs.Boss;
 import com.watabou.pixeldungeon.actors.mobs.Mob;
+import com.watabou.pixeldungeon.actors.mobs.npcs.NPC;
 import com.watabou.pixeldungeon.items.potions.PotionOfHealing;
 import com.watabou.pixeldungeon.items.wands.Wand;
 import com.watabou.pixeldungeon.items.wands.WandOfTeleportation;
@@ -15,103 +16,104 @@ import com.watabou.utils.Random;
 
 public class ChaosStaff extends Wand implements IChaosItem {
 
-	private int charge = 0;
+    private int charge = 0;
 
-	public ChaosStaff() {
-		imageFile = "items/chaosStaff.png";
-		image = 0;
-	}
+    public ChaosStaff() {
+        imageFile = "items/chaosStaff.png";
+        image = 0;
+    }
 
-	@Override
-	protected void updateLevel() {
-		super.updateLevel();
-		selectImage();
-	};
+    @Override
+    protected void updateLevel() {
+        super.updateLevel();
+        selectImage();
+    }
 
-	@Override
-	public void ownerTakesDamage(int damage) {
-		charge++;
-	}
+    @Override
+    public void ownerTakesDamage(int damage) {
+        charge++;
+    }
 
-	@Override
-	public void ownerDoesDamage(Char ch,int damage) {
-	}
+    @Override
+    public void ownerDoesDamage(Char ch, int damage) {
+    }
 
-	private void selectImage() {
-		image = Math.max(0, Math.min(level() / 3, 4));
-	}
+    private void selectImage() {
+        image = Math.max(0, Math.min(level() / 3, 4));
+    }
 
-	@Override
-	public void storeInBundle(Bundle bundle) {
-		super.storeInBundle(bundle);
+    @Override
+    public void storeInBundle(Bundle bundle) {
+        super.storeInBundle(bundle);
 
-		bundle.put(ChaosCommon.CHARGE_KEY, charge);
-	}
+        bundle.put(ChaosCommon.CHARGE_KEY, charge);
+    }
 
-	@Override
-	public void restoreFromBundle(Bundle bundle) {
-		super.restoreFromBundle(bundle);
+    @Override
+    public void restoreFromBundle(Bundle bundle) {
+        super.restoreFromBundle(bundle);
 
-		charge = bundle.getInt(ChaosCommon.CHARGE_KEY);
-	}
+        charge = bundle.getInt(ChaosCommon.CHARGE_KEY);
+    }
 
-	@Override
-	public int getCharge() {
-		return charge;
-	}
+    @Override
+    public int getCharge() {
+        return charge;
+    }
 
-	@Override
-	protected void onZap(int cell) {
+    @Override
+    protected void onZap(int cell) {
 
-		ChaosCommon.doChaosMark(cell, 10 + level() * 10 + charge);
-		charge = 0;
+        ChaosCommon.doChaosMark(cell, 10 + level() * 10 + charge);
+        charge = 0;
 
-		if (Math.random() < 0.1f) {
-			Char ch = Actor.findChar(cell);
-			if (ch instanceof Mob) {
-				Mob mob = (Mob) ch;
+        if (Math.random() < 0.1f) {
+            Char ch = Actor.findChar(cell);
+            if (ch instanceof Mob) {
+                Mob mob = (Mob) ch;
 
-				switch (Random.Int(0, 4)) {
+                if ((mob instanceof Boss) || (mob instanceof NPC)) {
+                    return;
+                }
 
-				case 0:
-					if (!(mob instanceof Boss)) {
-						mob.die(getCurUser());
-					}
-					break;
-				case 1:
-					Mob.makePet(mob, getCurUser());
-					break;
+                switch (Random.Int(0, 4)) {
+                    case 0:
+                        mob.die(getCurUser());
+                        break;
+                    case 1:
+                        Mob.makePet(mob, getCurUser());
+                        break;
 
-				case 2:
-					int nextCell = Dungeon.level.getEmptyCellNextTo(cell);
+                    case 2:
+                        int nextCell = Dungeon.level.getEmptyCellNextTo(cell);
 
-					if (Dungeon.level.cellValid(nextCell)) {
-						try {
-							Mob newMob = mob.getClass().newInstance();
-							Dungeon.level.spawnMob(newMob);
-						} catch (Exception e) {
-							throw new RuntimeException(e);
-						}
-					}
-					break;
-				case 3:
-					WandOfTeleportation.teleport(mob);
-					break;
-				case 4:
-					PotionOfHealing.heal(ch, 1);
-					break;
-				}
-			}
-		}
-	}
+                        if (Dungeon.level.cellValid(nextCell)) {
+                            try {
+                                Mob newMob = mob.getClass().newInstance();
+                                Dungeon.level.spawnMob(newMob);
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                        break;
+                    case 3:
+                        WandOfTeleportation.teleport(mob);
+                        break;
+                    case 4:
+                        PotionOfHealing.heal(ch, 1);
+                        break;
+                }
+            }
+        }
+    }
 
-	@Override
-	public String name() {
-		return Game.getVar(R.string.ChaosStaff_Name);
-	}
+    @Override
+    public String name() {
+        return Game.getVar(R.string.ChaosStaff_Name);
+    }
 
-	@Override
-	public String info() {
-		return Game.getVar(R.string.ChaosStaff_Info);
-	}
+    @Override
+    public String info() {
+        return Game.getVar(R.string.ChaosStaff_Info);
+    }
 }
