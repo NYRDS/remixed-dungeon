2f5439995025df114957913e9d16e4cfa5346d0b
Mikhael-Danilov <mikhael.danilov@gmail.com>
2016-03-28 00:54:07 +0300

    cleanup

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mobs/guts/Nightmare.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mobs/guts/Nightmare.java
@@ -1,23 +1,17 @@
 package com.nyrds.pixeldungeon.mobs.guts;
 
-import com.watabou.noosa.audio.Sample;
-import com.watabou.pixeldungeon.Assets;
 import com.watabou.pixeldungeon.actors.Char;
-import com.watabou.pixeldungeon.actors.buffs.Bleeding;
 import com.watabou.pixeldungeon.actors.buffs.Buff;
 import com.watabou.pixeldungeon.actors.buffs.Paralysis;
 import com.watabou.pixeldungeon.actors.buffs.Roots;
-import com.watabou.pixeldungeon.actors.mobs.Boss;
 import com.watabou.pixeldungeon.actors.mobs.Mob;
-import com.watabou.pixeldungeon.effects.CellEmitter;
-import com.watabou.pixeldungeon.effects.Speck;
-import com.watabou.pixeldungeon.items.Gold;
 import com.watabou.utils.Random;
 
 /**
  * Created by DeadDie on 12.02.2016
  */
 public class Nightmare extends Mob {
+
     {
         hp(ht(80));
         defenseSkill = 24;
