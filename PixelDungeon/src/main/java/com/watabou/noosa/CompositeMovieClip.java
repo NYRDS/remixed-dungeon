e848d24a672158521c1cdf1781c39783af0471aa
Mikhael-Danilov <mikhael.danilov@gmail.com>
2016-04-02 13:34:31 +0300

    layers state

--- PixelDungeon/src/main/java/com/watabou/noosa/CompositeMovieClip.java
+++ PixelDungeon/src/main/java/com/watabou/noosa/CompositeMovieClip.java
@@ -1,25 +1,49 @@
 package com.watabou.noosa;
 
 import com.watabou.gltextures.SmartTexture;
-
-import java.util.ArrayList;
+import com.watabou.utils.SparseArray;
 
 /**
  * Created by mike on 29.03.2016.
  */
 public class CompositeMovieClip extends MovieClip {
 
-	private ArrayList<SmartTexture> mLayers;
+	private SparseArray<LayerDesc> mLayers;
+
+	private class LayerDesc {
+		String       id;
+		boolean      enabled = false;
+		SmartTexture texture;
+
+		LayerDesc(String _id, SmartTexture _tex) {
+			id =_id;
+			texture = _tex;
+		}
+	}
 
 	public CompositeMovieClip() {
 		super();
 	}
 
-	public void addLayer(SmartTexture img) {
+	public void addLayer(String id, int z, SmartTexture img) {
 		if (mLayers == null) {
-			mLayers = new ArrayList<>();
+			mLayers = new SparseArray<>();
 		}
-		mLayers.add(img);
+
+		LayerDesc layerDesc = new LayerDesc(id, img);
+
+		mLayers.put(z, layerDesc);
+	}
+
+	public void setLayerState(String id, boolean state) {
+		if(mLayers!=null) {
+			for(int i=0;i<mLayers.size();++i){
+				LayerDesc layer = mLayers.valueAt(i);
+				if(layer.id.equals(id)) {
+					layer.enabled = state;
+				}
+			}
+		}
 	}
 
 	@Override
@@ -40,14 +64,16 @@ public class CompositeMovieClip extends MovieClip {
 
 		updateVerticesBuffer();
 
-		script.drawQuad( verticesBuffer );
+		script.drawQuad(verticesBuffer);
 
 		if(mLayers!=null) {
-			for(SmartTexture img:mLayers) {
-				img.bind();
-				script.drawQuad( verticesBuffer );
+			for(int i=0;i<mLayers.size();++i){
+				LayerDesc layer = mLayers.valueAt(i);
+				if(layer.enabled) {
+					layer.texture.bind();
+					script.drawQuad(verticesBuffer);
+				}
 			}
 		}
 	}
-
 }
