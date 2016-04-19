169ad881fa4e03bfe0ff71226f6908126b884f49
DeadDie <deaddie@inbox.ru>
2016-04-19 12:36:04 +0400

    cleanup

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/guts/HeartOfDarkness.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/guts/HeartOfDarkness.java
@@ -1,21 +1,11 @@
 package com.nyrds.pixeldungeon.items.guts;
 
-import com.nyrds.pixeldungeon.ml.R;
-import com.nyrds.pixeldungeon.mobs.guts.MimicAmulet;
-import com.watabou.noosa.Game;
-import com.watabou.noosa.audio.Sample;
-import com.watabou.pixeldungeon.Assets;
-import com.watabou.pixeldungeon.Dungeon;
-import com.watabou.pixeldungeon.actors.Char;
 import com.watabou.pixeldungeon.actors.buffs.Buff;
 import com.watabou.pixeldungeon.actors.buffs.MindVision;
 import com.watabou.pixeldungeon.actors.buffs.Vertigo;
 import com.watabou.pixeldungeon.actors.hero.Hero;
-import com.watabou.pixeldungeon.effects.CellEmitter;
-import com.watabou.pixeldungeon.effects.Speck;
 import com.watabou.pixeldungeon.items.Item;
 import com.watabou.pixeldungeon.items.food.Food;
-import com.watabou.pixeldungeon.sprites.ItemSpriteSheet;
 
 import java.util.ArrayList;
 
