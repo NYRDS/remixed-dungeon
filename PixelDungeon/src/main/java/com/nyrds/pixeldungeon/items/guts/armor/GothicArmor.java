869364254a0a04f710d938e4c72dc7b50bc5b144
DeadDie <deaddie@inbox.ru>
2016-05-11 19:31:17 +0400

    Tier 6 and crossbows (tier 2,4,6) class placeholders

---
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/guts/armor/GothicArmor.java
@@ -0,0 +1,36 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2014  Oleg Dolya
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+package com.nyrds.pixeldungeon.items.guts.armor;
+
+import com.nyrds.pixeldungeon.ml.R;
+import com.watabou.noosa.Game;
+import com.watabou.pixeldungeon.items.armor.Armor;
+
+
+public class GothicArmor extends Armor {
+
+	public GothicArmor() {
+		super( 6 );
+		image = 4;
+	}
+	
+	@Override
+	public String desc() {
+		return Game.getVar(R.string.PlateArmor_Desc);
+	}
+}
---
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/guts/weapon/ranged/CompositeCrossbow.java
@@ -0,0 +1,50 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2014  Oleg Dolya
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+package com.nyrds.pixeldungeon.items.guts.weapon.ranged;
+
+import com.watabou.pixeldungeon.items.Item;
+import com.watabou.pixeldungeon.items.weapon.melee.Bow;
+
+
+public class CompositeCrossbow extends Bow {
+
+	public CompositeCrossbow() {
+		super(4, 1.1f, 1.6f);
+		imageFile = "items/ranged.png";
+		image = 3;
+	}
+
+	@Override
+	public Item burn(int cell) {
+		return null;
+	}
+
+	@Override
+	public double acuFactor() {
+		return 1 + level() * 0.5;
+	}
+
+	@Override
+	public double dmgFactor() {
+		return 1 + level() * 0.75;
+	}
+
+	public double dlyFactor() {
+		return 1.1;
+	}
+}
