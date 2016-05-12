2e76caee49544d96944dec5699cb74f2bd36ba63
DeadDie <deaddie@inbox.ru>
2016-05-12 23:17:20 +0400

    clearing up them code

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/guts/weapon/ranged/RubyCrossbow.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/guts/weapon/ranged/RubyCrossbow.java
@@ -1,27 +1,6 @@
-/*
- * Pixel Dungeon
- * Copyright (C) 2012-2014  Oleg Dolya
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
- */
 package com.nyrds.pixeldungeon.items.guts.weapon.ranged;
 
-import com.nyrds.pixeldungeon.ml.R;
-import com.watabou.noosa.Game;
 import com.watabou.pixeldungeon.items.weapon.melee.Bow;
-import com.watabou.pixeldungeon.items.weapon.melee.MeleeWeapon;
-import com.watabou.pixeldungeon.sprites.ItemSpriteSheet;
 
 public class RubyCrossbow extends Bow {
 
