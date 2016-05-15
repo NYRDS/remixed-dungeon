643dca79e9d3bea9ce4ed5ea8f014dc3c7aea9f4
Mikhael-Danilov <mikhael.danilov@gmail.com>
2016-05-16 00:19:18 +0300

    * Spiders nest update by Pavel Provorotov * Spider exploding & servant update from Pavel Provorotov

--- PixelDungeon/src/main/java/com/watabou/pixeldungeon/Assets.java
+++ PixelDungeon/src/main/java/com/watabou/pixeldungeon/Assets.java
@@ -100,7 +100,7 @@ public class Assets {
 	public static final String ITEMS = "items.png";
 	public static final String PLANTS = "plants.png";
 
-	public static final String TILES_SPIDER_NEST = "tiles_spider_nest.png";
+	public static final String TILES_SPIDER_NEST_X = "tiles_spider_nest_x.png";
 	public static final String TILES_SEWERS = "tiles0.png";
 	public static final String TILES_SEWERS_X = "tiles0_x.png";
 	public static final String TILES_PRISON = "tiles1.png";
