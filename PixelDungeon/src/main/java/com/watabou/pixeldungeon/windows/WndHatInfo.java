2b9b668118842e642f2e10759c0f8872a51c4eae
DeadDie <deaddie@inbox.ru>
2016-05-31 23:49:11 +0400

    Removing unnecessary stuff

--- PixelDungeon/src/main/java/com/watabou/pixeldungeon/windows/WndHatInfo.java
+++ PixelDungeon/src/main/java/com/watabou/pixeldungeon/windows/WndHatInfo.java
@@ -2,14 +2,22 @@ package com.watabou.pixeldungeon.windows;
 
 import com.nyrds.pixeldungeon.items.accessories.Accessory;
 import com.watabou.noosa.Text;
+import com.watabou.pixeldungeon.PixelDungeon;
 import com.watabou.pixeldungeon.items.Item;
 import com.watabou.pixeldungeon.scenes.PixelScene;
+import com.watabou.pixeldungeon.sprites.ItemSprite;
+import com.watabou.pixeldungeon.ui.ItemSlot;
 import com.watabou.pixeldungeon.ui.Window;
+import com.watabou.pixeldungeon.utils.Utils;
 
 public class WndHatInfo extends Window {
 
 	private static final int WIDTH = 120;
 	private static final int MARGIN = 2;
+	private static final float GAP	= 2;
+
+	private static final int WIDTH_P = 120;
+	private static final int WIDTH_L = 160;
 
 	public WndHatInfo( final Accessory accessory ) {
 
@@ -25,5 +33,8 @@ public class WndHatInfo extends Window {
 		add(tfTitle);
 
 		resize( WIDTH,  yPos);
+
+
+
 	}
 }
