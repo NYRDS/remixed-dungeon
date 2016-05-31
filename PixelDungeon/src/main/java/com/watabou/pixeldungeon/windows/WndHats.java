33e49091283f88b8bdb359c606c65b16fd32107a
DeadDie <deaddie@inbox.ru>
2016-06-01 01:37:12 +0400

    Wrapping list of accessories on Hats window into Scroll Pane

--- PixelDungeon/src/main/java/com/watabou/pixeldungeon/windows/WndHats.java
+++ PixelDungeon/src/main/java/com/watabou/pixeldungeon/windows/WndHats.java
@@ -4,8 +4,10 @@ import com.nyrds.pixeldungeon.items.accessories.Accessory;
 import com.nyrds.pixeldungeon.support.Iap;
 import com.watabou.noosa.Image;
 import com.watabou.noosa.Text;
+import com.watabou.noosa.ui.Component;
 import com.watabou.pixeldungeon.scenes.PixelScene;
 import com.watabou.pixeldungeon.ui.RedButton;
+import com.watabou.pixeldungeon.ui.ScrollPane;
 import com.watabou.pixeldungeon.ui.Window;
 
 import java.util.List;
@@ -13,25 +15,27 @@ import java.util.List;
 public class WndHats extends Window {
 
 	private static final int WIDTH = 120;
+	private static final int HEIGHT = 180;
 	private static final int MARGIN = 2;
-	private static final int ICON_GAP = 28;
-	private static final int BUTTON_HEIGHT = 20;
+	private static final int BUTTON_HEIGHT = 16;
 
 	public WndHats() {
 
 		int yPos = 0;
 
-		Text tfTitle = PixelScene.createMultiline("Accessories", 9);
+		Text tfTitle = PixelScene.createMultiline("Accessories", 10);
 		tfTitle.hardlight(TITLE_COLOR);
-		tfTitle.x = tfTitle.y = MARGIN;
 		tfTitle.maxWidth(WIDTH - MARGIN * 2);
 		tfTitle.measure();
+		tfTitle.x = WIDTH/2 - tfTitle.width()/2;
+		tfTitle.y = MARGIN;
 
 		yPos += tfTitle.height() + MARGIN;
 		add(tfTitle);
 
 		List<String> hats = Accessory.getAccessoriesList();
 
+		Component content = new Component();
 
 		for (final String item: hats) {
 			String price = Iap.getSkuPrice(item);
@@ -39,17 +43,17 @@ public class WndHats extends Window {
 
 				Image hat = Accessory.getByName(item).getImage();
 				hat.setPos(0,yPos);
-				add(hat);
+				content.add(hat);
 
-				Text info = PixelScene.createMultiline("Stuff that kinda written in here, like a description or something", 8 );
+				Text info = PixelScene.createMultiline("Stuff that kinda written in here, like a description or something", 9 );
 
-				info.hardlight(0xFFFF00);
+				info.hardlight(0xFFFFFF);
 				info.x = hat.x + hat.width();
 				info.y =  hat.y;
-				info.maxWidth(WIDTH - ICON_GAP);
+				info.maxWidth(WIDTH - (int)hat.width());
 				info.measure();
 
-				add(info);
+			content.add(info);
 
 				RedButton rb = new RedButton(item + " " + price) {
 					@Override
@@ -59,13 +63,23 @@ public class WndHats extends Window {
 					}
 				};
 
-				rb.setRect(hat.width() + MARGIN,info.y + info.height(),WIDTH - hat.width() - MARGIN * 2, BUTTON_HEIGHT );
+				rb.setRect(hat.width(),info.y + info.height() + MARGIN, WIDTH / 2, BUTTON_HEIGHT );
 
-				add(rb);
-				yPos += rb.height() + info.height() + MARGIN;
+			content.add(rb);
+				yPos += rb.height() + info.height() + MARGIN * 8;
 			//}
 		}
 
-		resize( WIDTH,  yPos);
+		int h = (int) Math.min(HEIGHT - MARGIN, yPos);
+
+		resize( WIDTH,  h);
+
+		content.setSize(WIDTH, yPos);
+		ScrollPane list = new ScrollPane(content);
+
+		add(list);
+
+		list.setRect(0, tfTitle.y + tfTitle.height() + MARGIN, WIDTH, HEIGHT - tfTitle.height() - MARGIN * 2);
+
 	}
 }
