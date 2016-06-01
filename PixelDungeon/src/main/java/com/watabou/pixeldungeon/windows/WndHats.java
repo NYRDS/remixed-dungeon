9fedce6d5e58173ecd8b0cace5af428253b651b8
DeadDie <deaddie@inbox.ru>
2016-06-02 00:52:28 +0400

    WndHats: Moving price from button to pricetag field, adjusting layout

--- PixelDungeon/src/main/java/com/watabou/pixeldungeon/windows/WndHats.java
+++ PixelDungeon/src/main/java/com/watabou/pixeldungeon/windows/WndHats.java
@@ -7,10 +7,12 @@ import com.nyrds.pixeldungeon.ml.R;
 import com.nyrds.pixeldungeon.support.Iap;
 import com.watabou.noosa.Game;
 import com.watabou.noosa.Image;
+import com.watabou.noosa.SystemText;
 import com.watabou.noosa.Text;
 import com.watabou.noosa.ui.Component;
 import com.watabou.pixeldungeon.scenes.GameScene;
 import com.watabou.pixeldungeon.scenes.PixelScene;
+import com.watabou.pixeldungeon.ui.RedButton;
 import com.watabou.pixeldungeon.ui.ScrollPane;
 import com.watabou.pixeldungeon.ui.SystemRedButton;
 import com.watabou.pixeldungeon.ui.TextButton;
@@ -23,7 +25,7 @@ public class WndHats extends Window {
 	private static final int WIDTH = 120;
 	private static final int HEIGHT = 180;
 	private static final int MARGIN = 2;
-	private static final int BUTTON_HEIGHT = 16;
+	private static final int BUTTON_HEIGHT = 14;
 
 	public WndHats() {
 
@@ -44,36 +46,48 @@ public class WndHats extends Window {
 		Component content = new Component();
 
 		for (final String item: hats) {
-			final String price = "9999";// Iap.getSkuPrice(item);
+			final String price = "$ 999.99";// Iap.getSkuPrice(item);
 			if(price!=null) {
 
 				Image hat = Accessory.getByName(item).getImage();
 				hat.setPos(0,yPos);
 				content.add(hat);
 
-				Text info = PixelScene.createMultiline(Accessory.getByName(item).desc(), 9 );
+				String hatText = Accessory.getByName(item).name() + "\n" + Accessory.getByName(item).desc();
+
+				Text info = PixelScene.createMultiline(hatText, 10 );
 
 				info.hardlight(0xFFFFFF);
-				info.x = hat.x + hat.width();
+				info.x = hat.x + hat.width() + MARGIN;
 				info.y = hat.y;
-				info.maxWidth(WIDTH - (int)hat.width());
+				info.maxWidth(WIDTH - (int)hat.width() - MARGIN);
 				info.measure();
 
 			    content.add(info);
 
-				TextButton rb = new SystemRedButton(Accessory.getByName(item).name() + " " + price) {
+				SystemText priceTag = new SystemText(14);
+				priceTag.text(price);
+
+				priceTag.hardlight(0xFFFF00);
+				priceTag.x = hat.x;
+				priceTag.y = hat.y + hat.height();
+				priceTag.maxWidth((int)hat.width());
+				priceTag.measure();
+
+				content.add(priceTag);
+
+				TextButton rb = new RedButton(Game.getVar(R.string.WndHats_InfoButton)) {
 					@Override
 					protected void onClick() {
 						super.onClick();
 						GameScene.show( new WndHatInfo(item, price) );
-						//Iap.doPurchase(item);
 					}
 				};
 
-				rb.setRect(hat.width(),info.y + info.height() + MARGIN, WIDTH / 2, BUTTON_HEIGHT );
+				rb.setRect(info.x, info.y + info.height() + MARGIN * 2, WIDTH - hat.width() - MARGIN, BUTTON_HEIGHT );
 
 			    content.add(rb);
-				yPos += rb.height() + info.height() + MARGIN * 8;
+				yPos += rb.height() + info.height() + MARGIN * 9;
 			}
 			else{
 				Text info = PixelScene.createMultiline( Game.getVar(R.string.WndHats_NoConnectionMsg), 10 );
