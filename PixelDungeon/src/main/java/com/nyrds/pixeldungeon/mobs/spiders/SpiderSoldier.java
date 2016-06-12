4507de0d091816301d11c4cccfbcda060a24630b
DeadDie <deaddie@inbox.ru>
2016-06-12 23:51:12 +0400

    Spider soldier class and json

---
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mobs/spiders/SpiderSoldier.java
@@ -0,0 +1,50 @@
+package com.nyrds.pixeldungeon.mobs.spiders;
+
+import com.nyrds.pixeldungeon.mobs.common.MultiKindMob;
+import com.watabou.pixeldungeon.actors.Char;
+import com.watabou.pixeldungeon.actors.buffs.Buff;
+import com.watabou.pixeldungeon.actors.buffs.Paralysis;
+import com.watabou.pixeldungeon.actors.buffs.Poison;
+import com.watabou.pixeldungeon.items.food.MysteryMeat;
+import com.watabou.utils.Random;
+
+public class SpiderSoldier extends MultiKindMob {
+
+	public SpiderSoldier() {
+		hp(ht(35));
+		defenseSkill = 15;
+		baseSpeed = 1.2f;
+		
+		EXP = 4;
+		maxLvl = 10;
+		
+		kind = 1;
+		
+		loot = new MysteryMeat();
+		lootChance = 0.067f;
+	}
+	
+	@Override
+	public int attackProc( Char enemy, int damage ) {
+		if (Random.Int( 10 ) == 0) {
+			Buff.prolong( enemy, Paralysis.class, 3);
+		}
+		return damage;
+	}
+	
+	@Override
+	public int damageRoll() {
+		return Random.NormalIntRange( 8, 14 );
+	}
+	
+	@Override
+	public int attackSkill( Char target ) {
+		return 17;
+	}
+	
+	@Override
+	public int dr() {
+		return 7;
+	}
+
+}
