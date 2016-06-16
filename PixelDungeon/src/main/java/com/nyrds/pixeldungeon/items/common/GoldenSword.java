e9e9c90d651d989fbf3e7d94b53b7783d0009e58
DeadDie <deaddie@inbox.ru>
2016-06-16 11:03:30 +0400

    Golden sword gives gold on attack

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/common/GoldenSword.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/common/GoldenSword.java
@@ -7,8 +7,10 @@ import com.watabou.pixeldungeon.actors.hero.HeroSubClass;
 import com.watabou.pixeldungeon.actors.mobs.Boss;
 import com.watabou.pixeldungeon.actors.mobs.Mob;
 import com.watabou.pixeldungeon.actors.mobs.npcs.NPC;
+import com.watabou.pixeldungeon.items.Gold;
 import com.watabou.pixeldungeon.items.weapon.melee.SpecialWeapon;
 import com.watabou.pixeldungeon.sprites.ItemSprite.Glowing;
+import com.watabou.utils.Random;
 
 public class GoldenSword extends SpecialWeapon {
 	{
@@ -24,5 +26,15 @@ public class GoldenSword extends SpecialWeapon {
 	public Glowing glowing() {
 		float period = 1;
 		return new Glowing(0xFFFF66, period);
+	}
+
+	@Override
+	public void applySpecial(Hero hero, Char tgt ) {
+		//Gold proc
+		if (Random.Int(10) == 1){
+		int price = this.price() / 10;
+		if ( price > 500) { price = 500;}
+			Dungeon.level.drop(new Gold(price), tgt.getPos());
+		}
 	}
 }
