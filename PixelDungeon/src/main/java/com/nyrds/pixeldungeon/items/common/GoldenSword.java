636f74547d63b6e5b5f8e57abfb385d207816d84
DeadDie <deaddie@inbox.ru>
2016-06-16 10:26:26 +0400

    Placeholder for golden sword and testing it

---
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/common/GoldenSword.java
@@ -0,0 +1,52 @@
+package com.nyrds.pixeldungeon.items.common;
+
+import com.watabou.pixeldungeon.Dungeon;
+import com.watabou.pixeldungeon.actors.Char;
+import com.watabou.pixeldungeon.actors.hero.Hero;
+import com.watabou.pixeldungeon.actors.hero.HeroSubClass;
+import com.watabou.pixeldungeon.actors.mobs.Boss;
+import com.watabou.pixeldungeon.actors.mobs.Mob;
+import com.watabou.pixeldungeon.actors.mobs.npcs.NPC;
+import com.watabou.pixeldungeon.items.weapon.melee.SpecialWeapon;
+import com.watabou.pixeldungeon.sprites.ItemSprite.Glowing;
+
+public class GoldenSword extends SpecialWeapon {
+	{
+		imageFile = "items/swords.png";
+		image = 5;
+	}
+
+	public GoldenSword() {
+		super( 3, 1f, 1f );
+	}
+	
+	@Override
+	public Glowing glowing() {
+		
+		float period = (float) Math.max(0.1, 0.1/baseChance(Dungeon.hero));
+		//GLog.i("base chance: %.3f %.3f",baseChance(Dungeon.hero), period);
+		
+		return new Glowing(0xFF4466, period);
+		
+	}
+
+	private double baseChance(Hero hero) {
+		double armorPenalty = 1;
+
+		if(hero.belongings.armor != null) {
+			armorPenalty += hero.belongings.armor.tier;
+		}
+
+		double classBonus = 1;
+		if(hero.subClass == HeroSubClass.WARDEN ) {
+			classBonus = 1.5;
+		}
+
+		if(hero.subClass == HeroSubClass.SHAMAN) {
+			classBonus = 2.0;
+		}
+
+		return (0.25 + (hero.lvl() * 4 + Math.pow(level(),2)) * 0.01) * classBonus / armorPenalty;
+	}
+
+}
--- PixelDungeon/src/main/java/com/watabou/pixeldungeon/actors/hero/HeroClass.java
+++ PixelDungeon/src/main/java/com/watabou/pixeldungeon/actors/hero/HeroClass.java
@@ -19,6 +19,7 @@
 package com.watabou.pixeldungeon.actors.hero;
 
 import com.nyrds.android.util.TrackedRuntimeException;
+import com.nyrds.pixeldungeon.items.common.GoldenSword;
 import com.nyrds.pixeldungeon.items.guts.weapon.melee.Claymore;
 import com.nyrds.pixeldungeon.ml.BuildConfig;
 import com.nyrds.pixeldungeon.ml.R;
@@ -132,6 +133,7 @@ public enum HeroClass {
 		hero.collect(new Claymore().upgrade(100));
 
 		hero.collect(new ScaleArmor().upgrade(5));
+		hero.collect(new GoldenSword().upgrade(5));
 
 		hero.ht(1000);
 		hero.hp(1000);
