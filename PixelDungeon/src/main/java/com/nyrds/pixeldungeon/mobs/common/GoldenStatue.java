8d537fbb663d701c49369fda8026d0d8ddaf949a
DeadDie <deaddie@inbox.ru>
2016-06-16 15:06:43 +0400

    Golden no longer extends statue, ima no smart

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mobs/common/GoldenStatue.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mobs/common/GoldenStatue.java
@@ -66,6 +66,58 @@ public class GoldenStatue extends Statue {
 		IMMUNITIES.add( Leech.class );
 	}
 
+	private static final String WEAPON	= "weapon";
+
+	@Override
+	public void storeInBundle( Bundle bundle ) {
+		super.storeInBundle( bundle );
+		bundle.put( WEAPON, weapon );
+	}
+
+	@Override
+	public void restoreFromBundle( Bundle bundle ) {
+		super.restoreFromBundle( bundle );
+		weapon = (Weapon)bundle.get( WEAPON );
+	}
+
+	@Override
+	protected boolean act() {
+		if (Dungeon.visible[getPos()]) {
+			Journal.add( Journal.Feature.STATUE );
+		}
+		return super.act();
+	}
+
+	@Override
+	public int damageRoll() {
+		return Random.NormalIntRange( weapon.MIN, weapon.MAX );
+	}
+
+	@Override
+	public int attackSkill( Char target ) {
+		return (int)((9 + Dungeon.depth) * weapon.ACU);
+	}
+
+	@Override
+	protected float attackDelay() {
+		return weapon.DLY;
+	}
+
+	@Override
+	public int dr() {
+		return Dungeon.depth;
+	}
+
+	@Override
+	public void damage( int dmg, Object src ) {
+
+		if (state == PASSIVE) {
+			state = HUNTING;
+		}
+
+		super.damage( dmg, src );
+	}
+
 	@Override
 	public int attackProc( Char enemy, int damage ) {
 
@@ -77,7 +129,29 @@ public class GoldenStatue extends Statue {
 	}
 
 	@Override
+	public void beckon( int cell ) {
+	}
+
+	@Override
+	public void die( Object cause ) {
+		Dungeon.level.drop( weapon, getPos() ).sprite.drop();
+		super.die( cause );
+	}
+
+	@Override
+	public void destroy() {
+		Journal.remove( Journal.Feature.STATUE );
+		super.destroy();
+	}
+
+	@Override
+	public boolean reset() {
+		state = PASSIVE;
+		return true;
+	}
+
+	@Override
 	public String description() {
 		return Utils.format(Game.getVar(R.string.GoldenStatue_Desc), weapon.name());
-	}	
+	}
 }
