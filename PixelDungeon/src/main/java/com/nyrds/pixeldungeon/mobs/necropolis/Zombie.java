fa49f1fc359e7b82e725adbb456c7c6463f111ed
DeadDie <deaddie@inbox.ru>
2016-07-01 23:03:39 +0300

    Zombie balancing and placeholder for zombie attack proc

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mobs/necropolis/Zombie.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mobs/necropolis/Zombie.java
@@ -7,11 +7,15 @@ import com.watabou.pixeldungeon.Assets;
 import com.watabou.pixeldungeon.Dungeon;
 import com.watabou.pixeldungeon.actors.Char;
 import com.watabou.pixeldungeon.actors.blobs.ToxicGas;
+import com.watabou.pixeldungeon.actors.buffs.Buff;
 import com.watabou.pixeldungeon.actors.buffs.Paralysis;
+import com.watabou.pixeldungeon.actors.buffs.Roots;
+import com.watabou.pixeldungeon.actors.buffs.Terror;
 import com.watabou.pixeldungeon.actors.mobs.Mob;
 import com.watabou.pixeldungeon.effects.CellEmitter;
 import com.watabou.pixeldungeon.effects.Speck;
 import com.watabou.pixeldungeon.items.Gold;
+import com.watabou.pixeldungeon.items.weapon.enchantments.Death;
 import com.watabou.pixeldungeon.sprites.CharSprite;
 import com.watabou.pixeldungeon.utils.GLog;
 import com.watabou.utils.Random;
@@ -21,27 +25,39 @@ import com.watabou.utils.Random;
  */
 public class Zombie extends Mob {
     {
-        hp(ht(210));
+        hp(ht(45));
         defenseSkill = 27;
 
-        EXP = 7;
-        maxLvl = 35;
+        EXP = 10;
+        maxLvl = 15;
 
         loot = Gold.class;
         lootChance = 0.02f;
 
         IMMUNITIES.add(Paralysis.class);
         IMMUNITIES.add(ToxicGas.class);
+        IMMUNITIES.add( Terror.class );
+        IMMUNITIES.add( Death.class );
+    }
+
+    @Override
+    public int attackProc( Char enemy, int damage ) {
+        //Roots proc
+        if (Random.Int(10) == 1){
+            Buff.affect(enemy, Roots.class);
+        }
+        
+        return damage;
     }
 
     @Override
     public int damageRoll() {
-        return Random.NormalIntRange(15, 35);
+        return Random.NormalIntRange(5, 15);
     }
 
     @Override
     public int attackSkill( Char target ) {
-        return 25;
+        return 10;
     }
 
     @Override
