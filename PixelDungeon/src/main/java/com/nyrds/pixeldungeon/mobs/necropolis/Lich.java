a77398f8bff897d2e16cfee75452d3f1d65e7c65
DeadDie <deaddie@inbox.ru>
2016-07-07 02:34:56 +0300

    Lich can shoot stuff

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mobs/necropolis/Lich.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mobs/necropolis/Lich.java
@@ -1,5 +1,6 @@
 package com.nyrds.pixeldungeon.mobs.necropolis;
 
+import com.watabou.pixeldungeon.Dungeon;
 import com.watabou.pixeldungeon.actors.Char;
 import com.watabou.pixeldungeon.actors.blobs.ToxicGas;
 import com.watabou.pixeldungeon.actors.buffs.Amok;
@@ -12,6 +13,7 @@ import com.watabou.pixeldungeon.actors.mobs.Boss;
 import com.watabou.pixeldungeon.actors.mobs.Mob;
 import com.watabou.pixeldungeon.items.Gold;
 import com.watabou.pixeldungeon.items.weapon.enchantments.Death;
+import com.watabou.pixeldungeon.mechanics.Ballistica;
 import com.watabou.utils.Random;
 
 /**
@@ -23,6 +25,8 @@ public class Lich extends Boss {
         EXP = 20;
         defenseSkill = 20;
 
+        baseSpeed = 0f;
+
         IMMUNITIES.add( Paralysis.class );
         IMMUNITIES.add( ToxicGas.class );
         IMMUNITIES.add( Terror.class );
@@ -30,6 +34,30 @@ public class Lich extends Boss {
         IMMUNITIES.add( Amok.class );
         IMMUNITIES.add( Blindness.class );
         IMMUNITIES.add( Sleep.class );
+    }
+
+
+    @Override
+    protected boolean canAttack(Char enemy) {
+        return Dungeon.level.distance(getPos(), enemy.getPos()) < 4 && Ballistica.cast(getPos(), enemy.getPos(), false, true) == enemy.getPos();
+    }
+
+    @Override
+    protected boolean doAttack(Char enemy) {
+
+        if (Dungeon.level.distance(getPos(), enemy.getPos()) <= 1) {
+            return super.doAttack(enemy);
+        } else {
+
+            getSprite().zap(enemy.getPos());
+
+            spend(1);
+
+            if (hit(this, enemy, true)) {
+                enemy.damage(damageRoll(), this);
+            }
+            return true;
+        }
     }
 
     @Override
