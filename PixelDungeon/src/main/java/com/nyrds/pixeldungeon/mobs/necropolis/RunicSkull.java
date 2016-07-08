cc8d4036f9047e83b1bdff16c663d048bf583019
DeadDie <deaddie@inbox.ru>
2016-07-09 01:21:34 +0300

    preparing runic skull for use

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mobs/necropolis/RunicSkull.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mobs/necropolis/RunicSkull.java
@@ -25,15 +25,15 @@ import com.watabou.utils.Random;
 
 public class RunicSkull extends MultiKindMob {
 
-	static private int ctr = 0;
+	private boolean activated = false;
 
 	public RunicSkull() {
-		adjustLevel(Dungeon.depth);
-		
-		loot = SimpleWand.createRandomSimpleWand();
-		((Wand)loot).upgrade(Dungeon.depth);
-		
-		lootChance = 0.25f;
+
+		hp(ht(50));
+		EXP = 5;
+		defenseSkill = 10;
+
+		pacified = true;
 
 		IMMUNITIES.add( Paralysis.class );
 		IMMUNITIES.add( ToxicGas.class );
@@ -44,99 +44,17 @@ public class RunicSkull extends MultiKindMob {
 		IMMUNITIES.add( Sleep.class );
 	}
 
-	static public RunicSkull makeShadowLordCrystal() {
-		RunicSkull skull = new RunicSkull();
-		skull.kind = 2;
-		skull.lootChance = 0;
-
-		return skull;
+	public void setActivated(boolean state){
+		activated = state;
 	}
 
-	private void adjustLevel(int depth) {
-		kind = (ctr++)%2;
-		
-		hp(ht(Dungeon.depth * 4 + 1));
-		defenseSkill = depth * 2 + 1;
-		EXP = depth + 1;
-		maxLvl = depth + 2;
-		
-		IMMUNITIES.add( ScrollOfPsionicBlast.class );
-		IMMUNITIES.add( ToxicGas.class );
-		IMMUNITIES.add( ParalyticGas.class );
+	public boolean getActivated(){
+		return activated;
 	}
-	
+
 	@Override
 	public int getKind() {
 		return kind;
 	}
-	
-	@Override
-	public int damageRoll() {
-		return Random.NormalIntRange( hp() / 2, ht() / 2 );
-	}
-	
-	@Override
-	protected boolean canAttack( Char enemy ) {
-		return Ballistica.cast( getPos(), enemy.getPos(), false, true ) == enemy.getPos();
-	}
-	
-	@Override
-	public int attackSkill( Char target ) {
-		if(kind < 2 ) {
-			return 1000;
-		} else {
-			return 35;
-		}
-	}
-	
-	@Override
-	public int dr() {
-		return EXP / 3;
-	}
 
-	@Override
-	public int attackProc( final Char enemy, int damage ) {
-
-		if(kind < 2) {
-			final Wand wand = ((Wand) loot);
-
-			wand.mobWandUse(this, enemy.getPos());
-
-			return 0;
-		} else {
-			getSprite().zap(enemy.getPos());
-			if (enemy == Dungeon.hero && Random.Int(2) == 0) {
-				Buff.prolong(enemy, Weakness.class, Weakness.duration(enemy));
-			}
-			return damage;
-		}
-	}
-	
-	@Override
-	protected boolean getCloser( int target ) {
-		return false;
-	}
-
-	@Override
-	protected boolean getFurther( int target ) {
-		return false;
-	}
-
-	@Override
-	public void die(Object cause) {
-		int pos = getPos();
-
-		if(Dungeon.level.map[pos]== Terrain.PEDESTAL) {
-			Dungeon.level.set(pos,Terrain.EMBERS);
-			int x,y;
-			x = Dungeon.level.cellX(pos);
-			y = Dungeon.level.cellY(pos);
-
-			Dungeon.level.clearAreaFrom(Darkness.class, x - 2, y - 2, 5, 5);
-			Dungeon.level.fillAreaWith(Foliage.class, x - 2, y - 2, 5, 5, 1);
-
-			GameScene.updateMap();
-		}
-		super.die(cause);
-	}
 }
