b96b53d72a9076b335f52bb6009339c5f8b2561a
DeadDie <deaddie@inbox.ru>
2016-07-10 18:00:25 +0300

    Awakened skull concept added in comments

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/necropolis/BlackSkull.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/necropolis/BlackSkull.java
@@ -5,6 +5,11 @@ import com.watabou.pixeldungeon.sprites.ItemSprite.Glowing;
 
 public class BlackSkull extends Artifact {
 
+	//Supposed to accumulate charge when player kills mobs, and once charge is full goes "Awakened" state
+	//Once Awakened Skull gets 25 charges and 10% chance to revive next slain enemy as an ally
+	//Each try to revive consumes 1 charge of Awakend Skull. Once charges goes down to 0, it becomes regular "non-Awakened" Black Skull
+	//If Black Skull was discovered in bones of previous hero, it will instantly get full charge and thus become an Awakened Skull
+
 	public BlackSkull() {
 		imageFile = "items/artifacts.png";
 		image = 19;
