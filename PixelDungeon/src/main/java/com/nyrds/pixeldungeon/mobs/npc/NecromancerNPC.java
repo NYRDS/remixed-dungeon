91d34d47c8bb2bc8525eb6f7b97043f6429c7048
Mikhael-Danilov <mikhael.danilov@gmail.com>
2016-07-21 21:35:06 +0300

    Make code simpler

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mobs/npc/NecromancerNPC.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mobs/npc/NecromancerNPC.java
@@ -1,15 +1,12 @@
 package com.nyrds.pixeldungeon.mobs.npc;
 
-import com.nyrds.pixeldungeon.levels.NecroBossLevel;
 import com.nyrds.pixeldungeon.ml.R;
 import com.watabou.noosa.Game;
-import com.watabou.pixeldungeon.Dungeon;
 import com.watabou.pixeldungeon.actors.Actor;
 import com.watabou.pixeldungeon.actors.Char;
 import com.watabou.pixeldungeon.actors.buffs.Buff;
 import com.watabou.pixeldungeon.actors.hero.Hero;
 import com.watabou.pixeldungeon.actors.mobs.npcs.NPC;
-import com.watabou.pixeldungeon.items.TomeOfMastery;
 import com.watabou.pixeldungeon.items.keys.SkeletonKey;
 import com.watabou.pixeldungeon.levels.RegularLevel;
 import com.watabou.pixeldungeon.levels.Room;
@@ -21,7 +18,7 @@ import com.watabou.utils.Random;
 
 public class NecromancerNPC extends NPC {
 
-	private static final String TXT_INTRO = Game.getVar(R.string.NecromancerNPC_Intro2);
+	private static final String TXT_INTRO    = Game.getVar(R.string.NecromancerNPC_Intro2);
 	private static final String TXT_MESSAGE1 = Game.getVar(R.string.NecromancerNPC_Message1);
 	private static final String TXT_MESSAGE2 = Game.getVar(R.string.NecromancerNPC_Message2);
 	private static final String TXT_MESSAGE3 = Game.getVar(R.string.NecromancerNPC_Message3);
@@ -29,72 +26,67 @@ public class NecromancerNPC extends NPC {
 
 	private static String[] TXT_PHRASES = {TXT_MESSAGE1, TXT_MESSAGE2, TXT_MESSAGE3, TXT_MESSAGE4};
 
-	{
-	}
-	
 	@Override
 	protected Char chooseEnemy() {
 		return DUMMY;
 	}
-	
+
 	@Override
-	public void damage( int dmg, Object src ) {
+	public void damage(int dmg, Object src) {
 	}
 
 	@Override
-	public int defenseSkill( Char enemy ) {
+	public int defenseSkill(Char enemy) {
 		return 1000;
 	}
 
 	@Override
-	public void add( Buff buff ) {
+	public void add(Buff buff) {
 	}
 
-	private static final String NODE	   = "necromancer";
+	private static final String NODE       = "necromancer";
 	private static final String INTRODUCED = "introduced";
 	private static final String SPAWNED    = "spawned";
 
 	private static boolean spawned    = false;
-	private boolean        introduced = false;
+	private        boolean introduced = false;
 
 	@Override
-	public void storeInBundle( Bundle bundle ) {
+	public void storeInBundle(Bundle bundle) {
 		super.storeInBundle(bundle);
 
 		Bundle node = new Bundle();
-		node.put( SPAWNED, spawned );
-
 		node.put(SPAWNED, spawned);
-		if (spawned) {
-			node.put(INTRODUCED, introduced);
-		}
+		node.put(INTRODUCED, introduced);
 
-		bundle.put( NODE, node );
+		bundle.put(NODE, node);
 	}
 
 	@Override
-	public void restoreFromBundle( Bundle bundle ) {
+	public void restoreFromBundle(Bundle bundle) {
 		super.restoreFromBundle(bundle);
 
-		Bundle node = bundle.getBundle( NODE );
+		Bundle node = bundle.getBundle(NODE);
 
-		if (!node.isNull() && (spawned = node.getBoolean( SPAWNED ))) {
-			introduced = node.getBoolean( INTRODUCED );
-		} else {
-			spawned    = false;
+		if(node.isNull()){
+			return;
 		}
 
+		spawned = node.getBoolean(SPAWNED);
+		introduced = node.getBoolean(INTRODUCED);
 	}
 
-	public static void spawn( RegularLevel level, Room room ) {
-		if (!spawned && Dungeon.depth == 7 && room != null) {
+	public static void spawn(RegularLevel level, Room room) {
+		if (!spawned) {
+
 			NecromancerNPC npc = new NecromancerNPC();
 			do {
 				int cell = room.random(level);
 				npc.setPos(cell);
 			} while (level.map[npc.getPos()] == Terrain.LOCKED_EXIT);
-			level.mobs.add( npc );
-			Actor.occupyCell( npc );
+
+			level.mobs.add(npc);
+			Actor.occupyCell(npc);
 
 			spawned = true;
 		}
@@ -102,19 +94,16 @@ public class NecromancerNPC extends NPC {
 
 	@Override
 	public boolean interact(final Hero hero) {
-		getSprite().turnTo( getPos(), hero.getPos() );
+		getSprite().turnTo(getPos(), hero.getPos());
 
-		if (!introduced)
-		{
-			GameScene.show( new WndQuest( this, TXT_INTRO ) );
+		if (!introduced) {
+			GameScene.show(new WndQuest(this, TXT_INTRO));
 			introduced = true;
 			hero.collect(new SkeletonKey());
-		}
-		else{
+		} else {
 			int index = Random.Int(0, TXT_PHRASES.length);
 			say(TXT_PHRASES[index]);
 		}
 		return true;
 	}
-
 }
