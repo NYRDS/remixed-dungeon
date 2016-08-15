3384f6182ab049c658b9fdd2e5c0d511d8e5571a
DeadDie <deaddie@inbox.ru>
2016-08-15 21:49:13 +0300

    Placeholder for necromancer armor

---
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/common/armor/NecromancerArmor.java
@@ -0,0 +1,67 @@
+package com.nyrds.pixeldungeon.items.common.armor;
+
+import com.nyrds.pixeldungeon.ml.R;
+import com.watabou.noosa.Game;
+import com.watabou.noosa.audio.Sample;
+import com.watabou.pixeldungeon.Assets;
+import com.watabou.pixeldungeon.Dungeon;
+import com.watabou.pixeldungeon.actors.Actor;
+import com.watabou.pixeldungeon.actors.blobs.Blob;
+import com.watabou.pixeldungeon.actors.blobs.Regrowth;
+import com.watabou.pixeldungeon.actors.hero.Hero;
+import com.watabou.pixeldungeon.actors.hero.HeroClass;
+import com.watabou.pixeldungeon.actors.mobs.Mob;
+import com.watabou.pixeldungeon.items.armor.ClassArmor;
+import com.watabou.pixeldungeon.scenes.GameScene;
+import com.watabou.pixeldungeon.utils.GLog;
+
+public class NecromancerArmor extends ClassArmor {
+
+	private static final String TXT_NOT_NECROMANCER = Game.getVar(R.string.NecromancerArmor_NotNecromancer);
+	private static final String AC_SPECIAL = Game.getVar(R.string.NecromancerArmor_ACSpecial);
+
+	public NecromancerArmor() {
+		image = 17;
+		hasHelmet = true;
+	}	
+	
+	@Override
+	public String special() {
+		return AC_SPECIAL;
+	}
+	
+	@Override
+	public void doSpecial() {
+		
+		for (Mob mob : Dungeon.level.mobs) {
+			if (Dungeon.level.fieldOfView[mob.getPos()]) {
+				GameScene.add( Blob.seed( mob.getPos(), 100, Regrowth.class ) );
+			}
+		}
+		
+		getCurUser().hp(getCurUser().hp() - (getCurUser().hp() / 3));
+		
+		getCurUser().spend( Actor.TICK );
+		getCurUser().getSprite().operate( getCurUser().getPos() );
+		getCurUser().busy();
+		
+		Sample.INSTANCE.play( Assets.SND_READ );
+		
+		GameScene.add( Blob.seed( getCurUser().getPos(), 100, Regrowth.class ) );
+	}
+	
+	@Override
+	public boolean doEquip( Hero hero ) {
+		if (hero.heroClass == HeroClass.NECROMANCER) {
+			return super.doEquip( hero );
+		} else {
+			GLog.w( TXT_NOT_NECROMANCER );
+			return false;
+		}
+	}
+	
+	@Override
+	public String desc() {
+		return Game.getVar(R.string.ElfArmor_Desc);
+	}
+}
