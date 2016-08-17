ab4d6c34158aedfccb5bc0f42dff862cc085763e
DeadDie <deaddie@inbox.ru>
2016-08-17 07:55:28 +0300

    Necromancer armor class is more cozy now

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/common/armor/NecromancerArmor.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/common/armor/NecromancerArmor.java
@@ -2,17 +2,9 @@ package com.nyrds.pixeldungeon.items.common.armor;
 
 import com.nyrds.pixeldungeon.ml.R;
 import com.watabou.noosa.Game;
-import com.watabou.noosa.audio.Sample;
-import com.watabou.pixeldungeon.Assets;
-import com.watabou.pixeldungeon.Dungeon;
-import com.watabou.pixeldungeon.actors.Actor;
-import com.watabou.pixeldungeon.actors.blobs.Blob;
-import com.watabou.pixeldungeon.actors.blobs.Regrowth;
 import com.watabou.pixeldungeon.actors.hero.Hero;
 import com.watabou.pixeldungeon.actors.hero.HeroClass;
-import com.watabou.pixeldungeon.actors.mobs.Mob;
 import com.watabou.pixeldungeon.items.armor.ClassArmor;
-import com.watabou.pixeldungeon.scenes.GameScene;
 import com.watabou.pixeldungeon.utils.GLog;
 
 public class NecromancerArmor extends ClassArmor {
@@ -33,21 +25,7 @@ public class NecromancerArmor extends ClassArmor {
 	@Override
 	public void doSpecial() {
 		
-		for (Mob mob : Dungeon.level.mobs) {
-			if (Dungeon.level.fieldOfView[mob.getPos()]) {
-				GameScene.add( Blob.seed( mob.getPos(), 100, Regrowth.class ) );
-			}
-		}
-		
-		getCurUser().hp(getCurUser().hp() - (getCurUser().hp() / 3));
-		
-		getCurUser().spend( Actor.TICK );
-		getCurUser().getSprite().operate( getCurUser().getPos() );
-		getCurUser().busy();
-		
-		Sample.INSTANCE.play( Assets.SND_READ );
-		
-		GameScene.add( Blob.seed( getCurUser().getPos(), 100, Regrowth.class ) );
+		// Do stuff
 	}
 	
 	@Override
@@ -62,6 +40,6 @@ public class NecromancerArmor extends ClassArmor {
 	
 	@Override
 	public String desc() {
-		return Game.getVar(R.string.ElfArmor_Desc);
+		return Game.getVar(R.string.NecromancerArmor_Desc);
 	}
 }
