9c55972badd7a25df7623dc49a8902ffdee792b1
DeadDie <deaddie@inbox.ru>
2016-08-17 23:11:39 +0300

    cleanup

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/common/armor/NecromancerRobe.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/common/armor/NecromancerRobe.java
@@ -4,17 +4,17 @@ import com.nyrds.pixeldungeon.ml.R;
 import com.nyrds.pixeldungeon.mobs.common.Deathling;
 import com.watabou.noosa.Game;
 import com.watabou.pixeldungeon.Dungeon;
-import com.watabou.pixeldungeon.actors.Actor;
 import com.watabou.pixeldungeon.actors.Char;
 import com.watabou.pixeldungeon.actors.buffs.Buff;
 import com.watabou.pixeldungeon.actors.hero.Hero;
 import com.watabou.pixeldungeon.actors.hero.HeroClass;
 import com.watabou.pixeldungeon.actors.mobs.Mob;
-import com.watabou.pixeldungeon.effects.Pushing;
 import com.watabou.pixeldungeon.effects.Wound;
 import com.watabou.pixeldungeon.items.armor.ClassArmor;
 import com.watabou.pixeldungeon.plants.Sungrass;
 import com.watabou.pixeldungeon.utils.GLog;
+
+//TODO: Limit number of deathling to 2, make it so that if necromancer exceeds limit of deathlings he will lose health each turn. Number of health lost is equal to (deathlingExcess * heroLevel)
 
 public class NecromancerRobe extends ClassArmor {
 
@@ -35,19 +35,17 @@ public class NecromancerRobe extends ClassArmor {
 		Char ch = getCurUser();
 
 		Wound.hit(ch);
-			ch.damage(4 + Dungeon.hero.lvl(), this);
-			Buff.detach(ch, Sungrass.Health.class);
+		ch.damage(4 + Dungeon.hero.lvl(), this);
+		Buff.detach(ch, Sungrass.Health.class);
 
-			int spawnPos = Dungeon.level.getEmptyCellNextTo(ch.getPos());
+		int spawnPos = Dungeon.level.getEmptyCellNextTo(ch.getPos());
 
-			if (Dungeon.level.cellValid(spawnPos)) {
-				Mob pet = Mob.makePet(new Deathling(), getCurUser());
-				pet.setPos(spawnPos);
+		if (Dungeon.level.cellValid(spawnPos)) {
+			Mob pet = Mob.makePet(new Deathling(), getCurUser());
+			pet.setPos(spawnPos);
 
-				Dungeon.level.spawnMob(pet );
-				Actor.addDelayed( new Pushing( pet, ch.getPos(), pet.getPos() ), -1 );
-			}
-			return;
+			Dungeon.level.spawnMob(pet);
+		}
 	}
 
 	@Override
