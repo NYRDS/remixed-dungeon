583b309d33a53029fd239943ad52e0df9ef9dc3b
DeadDie <deaddie@inbox.ru>
2016-08-17 23:12:21 +0300

    Black skull shows messages to signalize player of it's abilities

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/necropolis/BlackSkull.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/necropolis/BlackSkull.java
@@ -5,6 +5,7 @@ import com.watabou.noosa.Game;
 import com.watabou.pixeldungeon.actors.hero.Hero;
 import com.watabou.pixeldungeon.actors.mobs.Mob;
 import com.watabou.pixeldungeon.items.rings.Artifact;
+import com.watabou.pixeldungeon.utils.GLog;
 import com.watabou.utils.Bundle;
 
 public class BlackSkull extends Artifact {
@@ -17,6 +18,9 @@ public class BlackSkull extends Artifact {
 
 	private static final String CHARGE_KEY      = "charge";
 	private static final String ACTIVATED_KEY   = "activated";
+	private static final String TXT_SKULL_ACTIVATED = Game.getVar(R.string.BlackSkull_Activated);
+	private static final String TXT_SKULL_DEACTIVATED = Game.getVar(R.string.BlackSkull_Deactivated);
+	private static final String TXT_SKULL_RESSURRECT = Game.getVar(R.string.BlackSkull_Ressurrect);
 
 	private boolean activated = false;
 
@@ -32,14 +36,16 @@ public class BlackSkull extends Artifact {
 		if (mob.canBePet()) {
 			if (activated) {
 				mob.ressurrect(hero);
-
+				GLog.w( TXT_SKULL_RESSURRECT );
 				charge = charge - RESSURRECTION_COST;
 				if (charge <= 0) {
+					GLog.w( TXT_SKULL_DEACTIVATED );
 					activated = false;
 				}
 			} else {
 				charge++;
 				if (charge >= MAXIMUM_CHARGE) {
+					GLog.w( TXT_SKULL_ACTIVATED );
 					activated = true;
 				}
 			}
