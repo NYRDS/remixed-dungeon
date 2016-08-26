e5ee2b6afc2f33d0653c457a742b7639a8d5e1e5
Mikhael-Danilov <mikhael.danilov@gmail.com>
2016-08-27 01:26:23 +0300

    get rid of mipmaps at all

--- PixelDungeon/src/main/java/com/watabou/noosa/SystemTextLine.java
+++ PixelDungeon/src/main/java/com/watabou/noosa/SystemTextLine.java
@@ -21,7 +21,6 @@ import android.graphics.Bitmap;
 import android.graphics.RectF;
 import android.opengl.GLES20;
 
-import com.nyrds.android.util.GlUtils;
 import com.watabou.gltextures.SmartTexture;
 import com.watabou.glwrap.Quad;
 
@@ -49,12 +48,12 @@ public class SystemTextLine extends Visual {
 
 		texture = new SmartTexture(bitmap);
 
-		if(GlUtils.isNpotMipmapsSupported()) {
-			texture.filter(GLES20.GL_LINEAR_MIPMAP_LINEAR, GLES20.GL_NEAREST);
-			GLES20.glGenerateMipmap(GLES20.GL_TEXTURE_2D);
-		} else {
+		//if(GlUtils.isNpotMipmapsSupported()) {
+		//	texture.filter(GLES20.GL_LINEAR_MIPMAP_LINEAR, GLES20.GL_NEAREST);
+		//	GLES20.glGenerateMipmap(GLES20.GL_TEXTURE_2D);
+		//} else {
 			texture.filter(GLES20.GL_LINEAR, GLES20.GL_LINEAR);
-		}
+		//}
 		frame( new RectF( 0, 0, 1, 1 ) );
 	}
 
