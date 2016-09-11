ccd81d239b5b9cb5b210c3f3e4204895ca52ca5a
Mikhael-Danilov <mikhael.danilov@gmail.com>
2016-09-12 00:24:16 +0300

    Goo room generation

--- PixelDungeon/src/main/java/com/watabou/pixeldungeon/levels/painters/BossExitPainter.java
+++ PixelDungeon/src/main/java/com/watabou/pixeldungeon/levels/painters/BossExitPainter.java
@@ -24,7 +24,7 @@ import com.watabou.pixeldungeon.levels.Terrain;
 public class BossExitPainter extends ExitPainter {
 
 	public static void paint( Level level, Room room ) {
-		fill( level, room, Terrain.WALL );
+		//fill( level, room, Terrain.WALL );
 		fill( level, room, 1, Terrain.EMPTY );
 		
 		for (Room.Door door : room.connected.values()) {
