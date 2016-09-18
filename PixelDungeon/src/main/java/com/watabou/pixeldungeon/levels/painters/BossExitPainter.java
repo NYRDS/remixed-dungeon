cb78dc42bb120692989cb3fea2e6d9d8aaeb5188
Mikhael-Danilov <mikhael.danilov@gmail.com>
2016-09-19 00:29:40 +0300

    remade Goo room generation

--- PixelDungeon/src/main/java/com/watabou/pixeldungeon/levels/painters/BossExitPainter.java
+++ PixelDungeon/src/main/java/com/watabou/pixeldungeon/levels/painters/BossExitPainter.java
@@ -23,15 +23,30 @@ import com.watabou.pixeldungeon.levels.Terrain;
 
 public class BossExitPainter extends ExitPainter {
 
-	public static void paint( Level level, Room room ) {
-		//fill( level, room, Terrain.WALL );
-		fill( level, room, 1, Terrain.EMPTY );
-		
+	public static void paint(Level level, Room room) {
+
+		fill(level, room, 1, Terrain.EMPTY);
+
 		for (Room.Door door : room.connected.values()) {
-			door.set( Room.Door.Type.REGULAR );
+			door.set(Room.Door.Type.REGULAR);
 		}
-		
-		level.setExit(room.top * level.getWidth() + (room.left + room.right) / 2,0);
-		set( level, level.getExit(0), Terrain.LOCKED_EXIT );
+
+		int centerX = (room.left + room.right) / 2;
+		int centerY = (room.top  + room.bottom) / 2;
+
+		set(level,centerX-1,centerY,Terrain.WALL_DECO);
+		set(level,centerX+1,centerY,Terrain.WALL_DECO);
+
+		set(level,centerX-1, centerY-1,Terrain.WALL);
+		set(level,centerX,   centerY-1,Terrain.WALL);
+		set(level,centerX+1, centerY-1,Terrain.WALL);
+
+
+		set(level,centerX-1, centerY+1,Terrain.WATER);
+		set(level,centerX,   centerY+1,Terrain.WATER);
+		set(level,centerX+1, centerY+1,Terrain.WATER);
+
+		level.setExit(level.cell(centerX,centerY), 0);
+		set(level, level.getExit(0), Terrain.LOCKED_EXIT);
 	}
 }
