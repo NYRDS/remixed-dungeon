51a357816c5844bb865dc9e02bf9f515ce92fa1a
Mikhael-Danilov <mikhael.danilov@gmail.com>
2016-09-24 01:47:19 +0300

    play with webView...

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/support/OwnAds.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/support/OwnAds.java
@@ -1,5 +1,6 @@
 package com.nyrds.pixeldungeon.support;
 
+import android.view.ViewGroup;
 import android.webkit.WebView;
 
 import com.watabou.noosa.Game;
@@ -15,7 +16,16 @@ public class OwnAds {
 			public void run() {
 				if (Game.instance().getLayout().getChildCount() == 1) {
 					WebView adView = new WebView(Game.instance());
-					adView.loadDataWithBaseURL(null,"Рекламко", "text/html", "utf-8", null);
+					ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 50);
+					adView.setLayoutParams(params);
+					adView.getSettings().setUseWideViewPort(true);
+					//adView.getSettings().setLoadWithOverviewMode(true);
+
+					String html = "<head><meta name=\"viewport\" content=\"user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi\" /></head>" +
+							"<div width=100%>Рекламко</div>" +
+							"<div align=\"right\">.</div>                                               ";
+
+					adView.loadDataWithBaseURL(null,html, "text/html", "utf-8", null);
 					Game.instance().getLayout().addView(adView, 0);
 					Game.setNeedSceneRestart(true);
 				}
