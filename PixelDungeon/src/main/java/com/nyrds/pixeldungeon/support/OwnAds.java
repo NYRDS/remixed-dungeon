cb1e353960d29b6eb801ab8c32969f3da2215a56
Mikhael-Danilov <mikhael.danilov@gmail.com>
2016-10-08 23:17:40 +0300

    own interstitial (untested)

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/support/OwnAds.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/support/OwnAds.java
@@ -1,10 +1,13 @@
 package com.nyrds.pixeldungeon.support;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.LinearLayout;
 
 import com.watabou.noosa.Game;
+import com.watabou.noosa.InterstitialPoint;
 import com.watabou.pixeldungeon.utils.Utils;
 
 /**
@@ -16,6 +19,11 @@ public class OwnAds {
 			"<div width=100%%>%s</div>" +
 			"<div align=\"right\">.</div>";
 
+
+	private static final String isAdTemplate = "<head><meta name=\"viewport\" content=\"user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi\" /></head>" +
+			"<div width=100%%>%s</div>" +
+			"<div align=\"right\">.</div>";
+
 	public static void displayBanner() {
 		Game.instance().runOnUiThread(new Runnable() {
 			@Override
@@ -24,21 +32,47 @@ public class OwnAds {
 				if (layout.getChildCount() == 1) {
 					WebView adView = new WebView(Game.instance());
 
-					int adViewHeight = Math.max(50,layout.getHeight()/10);
+					int adViewHeight = Math.max(50, layout.getHeight() / 10);
 
 					ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, adViewHeight);
 					adView.setLayoutParams(params);
 
-/*
-					String html = "<head><meta name=\"viewport\" content=\"user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi\" /></head>" +
-							"<div width=100%>Рекламко</div>" +
-							"<div align=\"right\">.</div>                                               ";
-*/
-					adView.loadDataWithBaseURL(null, Utils.format(adTemplate,"Рекламко"), "text/html", "utf-8", null);
+					adView.loadDataWithBaseURL(null, Utils.format(adTemplate, "Рекламко"), "text/html", "utf-8", null);
 					Game.instance().getLayout().addView(adView, 0);
 					Game.setNeedSceneRestart(true);
 				}
 			}
 		});
+	}
+
+	private static void displayIsAd(final InterstitialPoint work) {
+		Game.instance().runOnUiThread(new Runnable() {
+
+			                              @Override
+			                              public void run() {
+				                              AlertDialog.Builder alert = new AlertDialog.Builder(Game.instance());
+				                              alert.setTitle("Title here");
+
+				                              WebView adView = new WebView(Game.instance());
+
+				                              ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+				                              adView.setLayoutParams(params);
+
+				                              adView.loadDataWithBaseURL(null, Utils.format(adTemplate, "Рекламко"), "text/html", "utf-8", null);
+
+
+				                              alert.setView(adView);
+				                              alert.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+					                              @Override
+					                              public void onClick(DialogInterface dialog, int id) {
+						                              dialog.dismiss();
+						                              work.returnToWork(true);
+					                              }
+				                              });
+				                              alert.show();
+			                              }
+		                              }
+		);
+
 	}
 }
