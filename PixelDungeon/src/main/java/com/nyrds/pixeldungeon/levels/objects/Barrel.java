01453ae4784650c8b77015a04a4f7e3e7407bd1c
DeadDie <deaddie@inbox.ru>
2016-10-25 01:22:14 +0300

    testing barrel explosion sound

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/levels/objects/Barrel.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/levels/objects/Barrel.java
@@ -3,6 +3,8 @@ package com.nyrds.pixeldungeon.levels.objects;
 import com.nyrds.android.util.ModdingMode;
 import com.nyrds.pixeldungeon.ml.R;
 import com.watabou.noosa.Game;
+import com.watabou.noosa.audio.Sample;
+import com.watabou.pixeldungeon.Assets;
 import com.watabou.pixeldungeon.Dungeon;
 import com.watabou.pixeldungeon.actors.hero.Hero;
 import com.watabou.pixeldungeon.items.potions.PotionOfLiquidFlame;
@@ -87,7 +89,8 @@ public class Barrel extends LevelObject {
 
 			}
 		}, image()+0,  image()+1,  image()+2,  image()+3,  image()+4);
-		new PotionOfLiquidFlame().shatter(getPos());
+		Sample.INSTANCE.play( Assets.SND_EXPLOSION );
+		//new PotionOfLiquidFlame().shatter(getPos());
 	}
 
 	@Override
