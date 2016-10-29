f8550577cce8c39001b1f05f624c64be8cad5dbe
Mikhael-Danilov <mikhael.danilov@gmail.com>
2016-10-30 00:01:51 +0300

    cleanup

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mobs/common/Deathling.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mobs/common/Deathling.java
@@ -3,19 +3,6 @@ package com.nyrds.pixeldungeon.mobs.common;
 import com.nyrds.pixeldungeon.mobs.necropolis.UndeadMob;
 import com.watabou.pixeldungeon.Dungeon;
 import com.watabou.pixeldungeon.actors.Char;
-import com.watabou.pixeldungeon.actors.blobs.ParalyticGas;
-import com.watabou.pixeldungeon.actors.blobs.ToxicGas;
-import com.watabou.pixeldungeon.actors.buffs.Blindness;
-import com.watabou.pixeldungeon.actors.buffs.Buff;
-import com.watabou.pixeldungeon.actors.buffs.Charm;
-import com.watabou.pixeldungeon.actors.buffs.FlavourBuff;
-import com.watabou.pixeldungeon.actors.buffs.Roots;
-import com.watabou.pixeldungeon.actors.buffs.Slow;
-import com.watabou.pixeldungeon.actors.buffs.Vertigo;
-import com.watabou.pixeldungeon.actors.buffs.Weakness;
-import com.watabou.pixeldungeon.actors.hero.Hero;
-import com.watabou.pixeldungeon.items.Gold;
-import com.watabou.pixeldungeon.items.scrolls.ScrollOfPsionicBlast;
 import com.watabou.utils.Random;
 
 /**
