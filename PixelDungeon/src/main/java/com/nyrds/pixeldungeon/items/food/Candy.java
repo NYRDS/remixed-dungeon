290a27edb15b0e2ad006b0e3111ac0f1f265e2e3
Mikhael-Danilov <mikhael.danilov@gmail.com>
2016-10-30 00:26:34 +0300

    cleanup

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/food/Candy.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/food/Candy.java
@@ -4,20 +4,15 @@ import com.nyrds.pixeldungeon.ml.R;
 import com.watabou.noosa.Game;
 import com.watabou.noosa.audio.Sample;
 import com.watabou.pixeldungeon.Assets;
-import com.watabou.pixeldungeon.Badges;
-import com.watabou.pixeldungeon.Statistics;
-import com.watabou.pixeldungeon.actors.buffs.Hunger;
 import com.watabou.pixeldungeon.actors.hero.Hero;
-import com.watabou.pixeldungeon.effects.Speck;
 import com.watabou.pixeldungeon.effects.SpellSprite;
 import com.watabou.pixeldungeon.items.Item;
 import com.watabou.pixeldungeon.items.food.Food;
-import com.watabou.pixeldungeon.items.scrolls.ScrollOfRecharging;
 import com.watabou.pixeldungeon.utils.GLog;
 
 public class Candy extends Food {
 
-	public String fMessage = Game.getVar(R.string.Candy_Warning_1);
+	private String fMessage = Game.getVar(R.string.Candy_Warning_1);
 
 	public Candy() {
 		imageFile = "items/artifacts.png";
