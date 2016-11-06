5585b808e0505f9bca73a638c119c267ec8d268b
DeadDie <deaddie@inbox.ru>
2016-11-07 00:30:08 +0300

    Even more restructuring of About screen xml strings

--- PixelDungeon/src/main/java/com/watabou/pixeldungeon/scenes/AboutScene.java
+++ PixelDungeon/src/main/java/com/watabou/pixeldungeon/scenes/AboutScene.java
@@ -38,17 +38,17 @@ import com.watabou.pixeldungeon.ui.Window;
 public class AboutScene extends PixelScene {
 
 	private static final String TXT          = Game.getVar(R.string.AboutScene_Txt_Intro);
-	private static final String TXT_CODE     = Game.getVar(R.string.AboutScene_Code);
-	private static final String TXT_GRAPHICS = Game.getVar(R.string.AboutScene_Graphics);
-	private static final String TXT_MUSIC    = Game.getVar(R.string.AboutScene_Music);
-	private static final String TXT_SOUND    = Game.getVar(R.string.AboutScene_Sound);
-	private static final String TXT_THANKS   = Game.getVar(R.string.AboutScene_Thanks);
+	private static final String TXT_CODE     = Game.getVar(R.string.AboutScene_Code) + Game.getVar(R.string.AboutScene_Code_Names);
+	private static final String TXT_GRAPHICS = Game.getVar(R.string.AboutScene_Graphics) + Game.getVar(R.string.AboutScene_Graphics_Names);
+	private static final String TXT_MUSIC    = Game.getVar(R.string.AboutScene_Music) + Game.getVar(R.string.AboutScene_Music_Names);
+	private static final String TXT_SOUND    = Game.getVar(R.string.AboutScene_Sound) + Game.getVar(R.string.AboutScene_Sound_Names);
+	private static final String TXT_THANKS   = Game.getVar(R.string.AboutScene_Thanks) + Game.getVar(R.string.AboutScene_Thanks_Names);
 	private static final String TXT_EMAIL_US = Game.getVar(R.string.AboutScene_Email_Us);
 
 	private static final String OUR_SITE = Game.getVar(R.string.AboutScene_OurSite);
 	private static final String LNK = Game.getVar(R.string.AboutScene_Lnk);
 	private static final String SND = Game.getVar(R.string.AboutScene_Snd);
-	private static final String TRN = Game.getVar(R.string.AboutScene_TranslatedBy);
+	private static final String TRN = Game.getVar(R.string.AboutScene_Translation) + Game.getVar(R.string.AboutScene_Translation_Names);
 
 	private static String getTXT() {
 		return TXT + TXT_CODE + TXT_GRAPHICS + TXT_MUSIC + TXT_SOUND + TXT_THANKS + TXT_EMAIL_US;
