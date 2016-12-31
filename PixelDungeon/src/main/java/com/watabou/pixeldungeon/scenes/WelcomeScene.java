08af0e18ae5bc599a541316374be34aeab62920a
Mikhael-Danilov <mikhael.danilov@gmail.com>
2016-12-31 16:42:43 +0300

    simpler WelcomeScene

--- PixelDungeon/src/main/java/com/watabou/pixeldungeon/scenes/WelcomeScene.java
+++ PixelDungeon/src/main/java/com/watabou/pixeldungeon/scenes/WelcomeScene.java
@@ -16,52 +16,46 @@ import com.watabou.pixeldungeon.ui.ScrollPane;
 
 public class WelcomeScene extends PixelScene {
 
-	private static final String TTL_Welcome = Game.getVar(R.string.Welcome_Title);
-
-	private static final String TXT_Welcome = Game.getVar(R.string.Welcome_Text);
-	private static final String TXT_Welcome_19 = Game.getVar(R.string.Welcome_Text_19);
-	private static final String TXT_Welcome_20 = Game.getVar(R.string.Welcome_Text_20);
-	private static final String TXT_Welcome_20_1 = Game.getVar(R.string.Welcome_Text_20_1);
-	private static final String TXT_Welcome_21 = Game.getVar(R.string.Welcome_Text_20_2);
-	private static final String TXT_Welcome_21_1 = Game.getVar(R.string.Welcome_Text_21_1);
-	private static final String TXT_Welcome_21_2 = Game.getVar(R.string.Welcome_Text_21_2);
-	private static final String TXT_Welcome_21_3 = Game.getVar(R.string.Welcome_Text_21_3);
-	private static final String TXT_Welcome_21_4 = Game.getVar(R.string.Welcome_Text_21_4);
-	private static final String TXT_Welcome_21_5 = Game.getVar(R.string.Welcome_Text_21_5);
-	private static final String TXT_Welcome_22 = Game.getVar(R.string.Welcome_Text_22);
-	private static final String TXT_Welcome_23 = Game.getVar(R.string.Welcome_Text_23);
-	private static final String TXT_Welcome_23_1 = Game.getVar(R.string.Welcome_Text_23_1);
-	private static final String TXT_Welcome_23_2 = Game.getVar(R.string.Welcome_Text_23_2);
-	private static final String TXT_Welcome_24 = Game.getVar(R.string.Welcome_Text_24);
-	private static final String TXT_Welcome_24_1 = Game.getVar(R.string.Welcome_Text_24_1);
-	private static final String TXT_Welcome_24_2 = Game.getVar(R.string.Welcome_Text_24_2);
-	private static final String TXT_Welcome_25 = Game.getVar(R.string.Welcome_Text_25);
-	private static final String TXT_Welcome_25_1 = Game.getVar(R.string.Welcome_Text_25_1);
-	private static final String TXT_Welcome_25_2 = Game.getVar(R.string.Welcome_Text_25_2);
-	private static final String TXT_Welcome_25_3 = Game.getVar(R.string.Welcome_Text_25_3);
-	private static final String TXT_Welcome_25_4 = Game.getVar(R.string.Welcome_Text_25_4);
-	private static final String TXT_Welcome_25_5 = Game.getVar(R.string.Welcome_Text_25_5);
-	private static final String TXT_Welcome_26 = Game.getVar(R.string.Welcome_Text_26);
-
 	private static final int GAP = 4;
 
 	@Override
 	public void create() {
 		super.create();
 
-		String[] upds = { TXT_Welcome, TXT_Welcome_19, TXT_Welcome_20, TXT_Welcome_20_1, TXT_Welcome_21,
-				TXT_Welcome_21_1,TXT_Welcome_21_2, TXT_Welcome_21_3, TXT_Welcome_21_4, TXT_Welcome_21_5,
-				TXT_Welcome_22, TXT_Welcome_23, TXT_Welcome_23_1, TXT_Welcome_23_2, TXT_Welcome_24,
-				TXT_Welcome_24_1, TXT_Welcome_24_2, TXT_Welcome_25, TXT_Welcome_25_1, TXT_Welcome_25_2,
-				TXT_Welcome_25_3, TXT_Welcome_25_4, TXT_Welcome_25_5, TXT_Welcome_26 };
+		String[] upds = {
+				Game.getVar(R.string.Welcome_Text),
+				Game.getVar(R.string.Welcome_Text_19),
+				Game.getVar(R.string.Welcome_Text_20),
+				Game.getVar(R.string.Welcome_Text_20_1),
+				Game.getVar(R.string.Welcome_Text_20_2),
+				Game.getVar(R.string.Welcome_Text_21_1),
+				Game.getVar(R.string.Welcome_Text_21_2),
+				Game.getVar(R.string.Welcome_Text_21_3),
+				Game.getVar(R.string.Welcome_Text_21_4),
+				Game.getVar(R.string.Welcome_Text_21_5),
+				Game.getVar(R.string.Welcome_Text_22),
+				Game.getVar(R.string.Welcome_Text_23),
+				Game.getVar(R.string.Welcome_Text_23_1),
+				Game.getVar(R.string.Welcome_Text_23_2),
+				Game.getVar(R.string.Welcome_Text_24),
+				Game.getVar(R.string.Welcome_Text_24_1),
+				Game.getVar(R.string.Welcome_Text_24_2),
+				Game.getVar(R.string.Welcome_Text_25),
+				Game.getVar(R.string.Welcome_Text_25_1),
+				Game.getVar(R.string.Welcome_Text_25_2),
+				Game.getVar(R.string.Welcome_Text_25_3),
+				Game.getVar(R.string.Welcome_Text_25_4),
+				Game.getVar(R.string.Welcome_Text_25_5),
+				Game.getVar(R.string.Welcome_Text_26)
+		};
 
 		Text[] updTexts = new Text[upds.length];
 
 		for (int i = 0; i < upds.length; i++) {
-			updTexts[i] = createMultiline(upds[upds.length - i - 1],  GuiProperties.regularFontSize());
+			updTexts[i] = createMultiline(upds[upds.length - i - 1], GuiProperties.regularFontSize());
 		}
 
-		Text title = createMultiline(TTL_Welcome, GuiProperties.bigTitleFontSize());
+		Text title = createMultiline(Game.getVar(R.string.Welcome_Title), GuiProperties.bigTitleFontSize());
 
 		int w = Camera.main.width;
 		int h = Camera.main.height;
@@ -111,10 +105,9 @@ public class WelcomeScene extends PixelScene {
 			protected void onClick() {
 				PixelDungeon.version(Game.versionCode);
 
-				if(Preferences.INSTANCE.getInt(Preferences.KEY_COLLECT_STATS,0) == 0) {
+				if (Preferences.INSTANCE.getInt(Preferences.KEY_COLLECT_STATS, 0) == 0) {
 					Game.switchScene(AllowStatisticsCollectionScene.class);
 				} else {
-
 					Game.switchScene(TitleScene.class);
 				}
 			}
