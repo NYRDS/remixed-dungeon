06786c473dc74b37b851cbe6936cd387c84560f8
Mikhael-Danilov <mikhael.danilov@gmail.com>
2017-01-02 00:47:54 +0300

    cleanup

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/necropolis/BlackSkullOfMastery.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/necropolis/BlackSkullOfMastery.java
@@ -3,17 +3,10 @@ package com.nyrds.pixeldungeon.items.necropolis;
 import com.nyrds.pixeldungeon.items.common.MasteryItem;
 import com.nyrds.pixeldungeon.ml.R;
 import com.watabou.noosa.Game;
-import com.watabou.noosa.audio.Sample;
-import com.watabou.pixeldungeon.Assets;
-import com.watabou.pixeldungeon.Badges;
 import com.watabou.pixeldungeon.actors.hero.Hero;
 import com.watabou.pixeldungeon.actors.hero.HeroSubClass;
-import com.watabou.pixeldungeon.effects.Speck;
-import com.watabou.pixeldungeon.effects.SpellSprite;
-import com.watabou.pixeldungeon.items.Item;
 import com.watabou.pixeldungeon.scenes.GameScene;
 import com.watabou.pixeldungeon.utils.GLog;
-import com.watabou.pixeldungeon.utils.Utils;
 import com.watabou.pixeldungeon.windows.WndChooseWay;
 
 import java.util.ArrayList;
@@ -45,7 +38,7 @@ public class BlackSkullOfMastery extends MasteryItem {
 
 			setCurUser(hero);
 			
-			HeroSubClass way = null;
+			HeroSubClass way;
 
 			switch (hero.heroClass) {
 			default:
