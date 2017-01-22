8694bda03c01409a41c46992b800ca44e9f7142d
Mikhael-Danilov <mikhael.danilov@gmail.com>
2017-01-23 00:16:26 +0300

    cleanup

--- PixelDungeon/src/main/java/com/watabou/noosa/SystemTextLine.java
+++ PixelDungeon/src/main/java/com/watabou/noosa/SystemTextLine.java
@@ -46,14 +46,12 @@ public class SystemTextLine extends Visual {
 	public SystemTextLine(Bitmap bitmap) {
 		this();
 
+		//texture = new SmartTexture(bitmap,Texture.NEAREST, Texture.CLAMP);
 		texture = new SmartTexture(bitmap);
 
-		//if(GlUtils.isNpotMipmapsSupported()) {
-		//	texture.filter(GLES20.GL_LINEAR_MIPMAP_LINEAR, GLES20.GL_NEAREST);
-		//	GLES20.glGenerateMipmap(GLES20.GL_TEXTURE_2D);
-		//} else {
-			texture.filter(GLES20.GL_LINEAR, GLES20.GL_LINEAR);
-		//}
+		texture.filter(GLES20.GL_LINEAR, GLES20.GL_LINEAR);
+
+
 		frame( new RectF( 0, 0, 1, 1 ) );
 	}
 
