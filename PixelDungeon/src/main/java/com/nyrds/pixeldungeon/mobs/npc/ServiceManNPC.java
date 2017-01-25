5f922aeb24ed6ddbdf1e92219c4ff4a3a54d51b5
DeadDie <deaddie@inbox.ru>
2017-01-25 20:49:19 +0300

    ServiceMan sprite stuff

---
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mobs/npc/ServiceManNPC.java
@@ -0,0 +1,80 @@
+package com.nyrds.pixeldungeon.mobs.npc;
+
+import com.nyrds.pixeldungeon.ml.R;
+import com.watabou.noosa.Game;
+import com.watabou.pixeldungeon.actors.Char;
+import com.watabou.pixeldungeon.actors.buffs.Buff;
+import com.watabou.pixeldungeon.actors.buffs.Paralysis;
+import com.watabou.pixeldungeon.actors.buffs.Roots;
+import com.watabou.pixeldungeon.actors.hero.Hero;
+import com.watabou.pixeldungeon.actors.mobs.npcs.NPC;
+import com.watabou.utils.Random;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ServiceManNPC extends NPC {
+
+	private static final String TXT_MESSAGE1 = Game.getVar(R.string.ServiceManNPC_Message1);
+
+	private static String[] TXT_PHRASES = {TXT_MESSAGE1};
+
+	public ServiceManNPC() {
+	}
+	
+	@Override
+	public int defenseSkill( Char enemy ) {
+		return 1000;
+	}
+	
+	@Override
+	public String defenseVerb() {
+		return Game.getVar(R.string.Ghost_Defense);
+	}
+	
+	@Override
+	public float speed() {
+		return 0.5f;
+	}
+	
+	@Override
+	protected Char chooseEnemy() {
+		return DUMMY;
+	}
+	
+	@Override
+	public void damage( int dmg, Object src ) {
+	}
+	
+	@Override
+	public void add( Buff buff ) {
+	}
+
+	@Override
+	public boolean reset() {
+		return true;
+	}
+
+
+	@Override
+	public boolean interact(final Hero hero) {
+		getSprite().turnTo( getPos(), hero.getPos() );
+
+		int index = Random.Int(0, TXT_PHRASES.length);
+		say(TXT_PHRASES[index]);
+
+		return true;
+	}
+
+	private static final HashSet<Class<?>> IMMUNITIES = new HashSet<>();
+	static {
+		IMMUNITIES.add( Paralysis.class );
+		IMMUNITIES.add( Roots.class );
+	}
+
+	@Override
+	public Set<Class<?>> immunities() {
+		return IMMUNITIES;
+	}
+
+}
