4564a9ee97723940f9fbc24a262e7e61cbfb9bfc
DeadDie <deaddie@inbox.ru>
2017-03-13 18:49:38 +0300

    here comes plague doctor

---
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mobs/npc/PlagueDoctorNPC.java
@@ -0,0 +1,83 @@
+package com.nyrds.pixeldungeon.mobs.npc;
+
+import com.nyrds.pixeldungeon.ml.R;
+import com.watabou.noosa.Game;
+import com.watabou.pixeldungeon.actors.Char;
+import com.watabou.pixeldungeon.actors.buffs.Buff;
+import com.watabou.pixeldungeon.actors.buffs.Paralysis;
+import com.watabou.pixeldungeon.actors.buffs.Roots;
+import com.watabou.pixeldungeon.actors.hero.Hero;
+import com.watabou.pixeldungeon.actors.mobs.npcs.NPC;
+import com.watabou.utils.Random;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PlagueDoctorNPC extends NPC {
+
+	private static final String TXT_MESSAGE1 = Game.getVar(R.string.TownsfolkNPC_Message1);
+	private static final String TXT_MESSAGE2 = Game.getVar(R.string.TownsfolkNPC_Message2);
+	private static final String TXT_MESSAGE3 = Game.getVar(R.string.TownsfolkNPC_Message3);
+	private static final String TXT_MESSAGE4 = Game.getVar(R.string.TownsfolkNPC_Message4);
+
+	private static String[] TXT_PHRASES = {TXT_MESSAGE1, TXT_MESSAGE2, TXT_MESSAGE3, TXT_MESSAGE4};
+
+	public PlagueDoctorNPC() {
+	}
+	
+	@Override
+	public int defenseSkill( Char enemy ) {
+		return 1000;
+	}
+	
+	@Override
+	public String defenseVerb() {
+		return Game.getVar(R.string.Ghost_Defense);
+	}
+	
+	@Override
+	public float speed() {
+		return 0.5f;
+	}
+	
+	@Override
+	protected Char chooseEnemy() {
+		return DUMMY;
+	}
+	
+	@Override
+	public void damage( int dmg, Object src ) {
+	}
+	
+	@Override
+	public void add( Buff buff ) {
+	}
+
+	@Override
+	public boolean reset() {
+		return true;
+	}
+
+	@Override
+	public boolean interact(final Hero hero) {
+		getSprite().turnTo( getPos(), hero.getPos() );
+
+		int index = Random.Int(0, TXT_PHRASES.length);
+		say(TXT_PHRASES[index]);
+		return true;
+	}
+		
+	private static final HashSet<Class<?>> IMMUNITIES = new HashSet<>();
+	static {
+		IMMUNITIES.add( Paralysis.class );
+		IMMUNITIES.add( Roots.class );
+	}
+	
+	@Override
+	public Set<Class<?>> immunities() {
+		return IMMUNITIES;
+	}
+
+}
+
+
