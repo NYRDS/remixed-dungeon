da42a1e58cb7a09af686941b4ce80a629490e621
Mikhael-Danilov <mikhael.danilov@gmail.com>
2017-04-03 23:54:36 +0300

    separate App classes ( ARCA vs Firebase )

--- PixelDungeon/src/googlePlay/java/com/nyrds/pixeldungeon/ml/RemixedPixelDungeonApp.java
+++ PixelDungeon/src/googlePlay/java/com/nyrds/pixeldungeon/ml/RemixedPixelDungeonApp.java
@@ -2,15 +2,11 @@ package com.nyrds.pixeldungeon.ml;
 
 import android.support.multidex.MultiDexApplication;
 
-//@ReportsCrashes(mailTo = "nyrdsofficial@gmail.com", mode = ReportingInteractionMode.TOAST, resToastText = R.string.RemixedPixelDungeonApp_sendCrash)
 public class RemixedPixelDungeonApp extends MultiDexApplication {
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		if(!BuildConfig.DEBUG) {
-			//ACRA.init(this);
-		}
 
 		try {
 			Class.forName("android.os.AsyncTask");
