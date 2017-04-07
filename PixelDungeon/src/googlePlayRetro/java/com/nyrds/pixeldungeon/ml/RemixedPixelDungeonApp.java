99e74dca10f2aedaebe5ff5bb6bf9691e5c62990
Mikhael-Danilov <mikhael.danilov@gmail.com>
2017-04-08 00:37:57 +0300

    add firebase & ga stuff to dummy

--- PixelDungeon/src/googlePlayRetro/java/com/nyrds/pixeldungeon/ml/RemixedPixelDungeonApp.java
+++ PixelDungeon/src/googlePlayRetro/java/com/nyrds/pixeldungeon/ml/RemixedPixelDungeonApp.java
@@ -1,13 +1,13 @@
 package com.nyrds.pixeldungeon.ml;
 
-import android.support.multidex.MultiDexApplication;
+import android.app.Application;
 
 import org.acra.ACRA;
 import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 
 @ReportsCrashes(mailTo = "nyrdsofficial@gmail.com", mode = ReportingInteractionMode.TOAST, resToastText = R.string.RemixedPixelDungeonApp_sendCrash)
-public class RemixedPixelDungeonApp extends MultiDexApplication {
+public class RemixedPixelDungeonApp extends Application {
 
 	@Override
 	public void onCreate() {
