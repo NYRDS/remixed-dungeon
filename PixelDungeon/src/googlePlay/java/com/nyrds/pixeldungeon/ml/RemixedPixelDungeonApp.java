f37ccdd79b443d67edbd70afaff8b6c62267ae60
Mikhael-Danilov <mikhael.danilov@gmail.com>
2017-04-11 23:22:44 +0300

    looks like where is no advantages in firebase so reverting to GA & ACRA

--- PixelDungeon/src/googlePlay/java/com/nyrds/pixeldungeon/ml/RemixedPixelDungeonApp.java
+++ PixelDungeon/src/googlePlay/java/com/nyrds/pixeldungeon/ml/RemixedPixelDungeonApp.java
@@ -2,11 +2,19 @@ package com.nyrds.pixeldungeon.ml;
 
 import android.support.multidex.MultiDexApplication;
 
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+
+@ReportsCrashes(mailTo = "nyrdsofficial@gmail.com", mode = ReportingInteractionMode.TOAST, resToastText = R.string.RemixedPixelDungeonApp_sendCrash)
 public class RemixedPixelDungeonApp extends MultiDexApplication {
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		if(!BuildConfig.DEBUG) {
+			ACRA.init(this);
+		}
 
 		try {
 			Class.forName("android.os.AsyncTask");
