e76b7f10ff1efa35b021daeb4eb38ee6dc1b0412
Mikhael-Danilov <mikhael.danilov@gmail.com>
2017-04-26 23:50:58 +0300

    WalkingType enum stub

---
+++ PixelDungeon/src/main/java/com/watabou/pixeldungeon/actors/mobs/WalkingType.java
@@ -0,0 +1,9 @@
+package com.watabou.pixeldungeon.actors.mobs;
+
+/**
+ * Created by mike on 26.04.2017.
+ * This file is part of Remixed Pixel Dungeon.
+ */
+public enum WalkingType {
+	NORMAL, WATER, WALL, GRASS, CHASM, ABSOLUTE
+}
