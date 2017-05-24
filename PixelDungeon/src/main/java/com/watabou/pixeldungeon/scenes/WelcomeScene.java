8f5ac06edc981034a64b89545e42b0caf67a5403
DeadDie <deaddie@inbox.ru>
2017-05-24 19:26:30 +0300

    I am totally missed on a welocme

--- PixelDungeon/src/main/java/com/watabou/pixeldungeon/scenes/WelcomeScene.java
+++ PixelDungeon/src/main/java/com/watabou/pixeldungeon/scenes/WelcomeScene.java
@@ -53,7 +53,8 @@ public class WelcomeScene extends PixelScene {
 				Game.getVar(R.string.Welcome_Text_26_1),
 				Game.getVar(R.string.Welcome_Text_26_2),
 				Game.getVar(R.string.Welcome_Text_26_3),
-				Game.getVar(R.string.Welcome_Text_26_4)
+				Game.getVar(R.string.Welcome_Text_26_4),
+				Game.getVar(R.string.Welcome_Text_26_5)
 		};
 
 		int displayUpdates = Math.min(upds.length, 5);
