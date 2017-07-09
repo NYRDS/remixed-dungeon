29bdd5e96207820639ca25fe0d921cc0bf9d7171
DeadDie <deaddie@inbox.ru>
2017-07-09 23:18:54 +0300

    Portal object wip

---
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/levels/objects/PortalGate.java
@@ -0,0 +1,85 @@
+package com.nyrds.pixeldungeon.levels.objects;
+
+import com.nyrds.pixeldungeon.ml.R;
+import com.nyrds.pixeldungeon.windows.WndLibrary;
+import com.watabou.noosa.Game;
+import com.watabou.pixeldungeon.actors.Char;
+import com.watabou.pixeldungeon.actors.hero.Hero;
+import com.watabou.pixeldungeon.levels.Level;
+import com.watabou.pixeldungeon.scenes.GameScene;
+import com.watabou.utils.Bundle;
+import com.watabou.utils.Callback;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by mike on 01.07.2016.
+ */
+public class PortalGate extends LevelObject {
+
+	public PortalGate(){
+		super(-1);
+	}
+
+	public PortalGate(int pos) {
+		super(pos);
+	}
+
+	@Override
+	void setupFromJson(Level level, JSONObject obj) throws JSONException {
+	}
+
+	@Override
+	public boolean interact(Hero hero) {
+
+		sprite.playAnim(10, false, new Callback() {
+			@Override
+			public void call() {
+
+
+			}
+		}, image() + 0, image() + 1, image() + 2, image() + 3, image() + 4);
+
+		return false;
+	}
+
+	@Override
+	public void restoreFromBundle( Bundle bundle ) {
+		super.restoreFromBundle(bundle);
+	}
+
+	@Override
+	public void storeInBundle( Bundle bundle ) {
+		super.storeInBundle(bundle);
+	}
+
+	@Override
+	public void burn() {
+	}
+
+	@Override
+	public boolean stepOn(Char hero) {
+		return false;
+	}
+
+	@Override
+	public String desc() {
+		return Game.getVar(R.string.LibraryBook_Description);
+	}
+
+	@Override
+	public String name() {
+		return Game.getVar(R.string.LibraryBook_Name);
+	}
+
+	@Override
+	public int image() {
+		return 0;
+	}
+
+	@Override
+	public String texture() {
+		return "levelObjects/portals.png";
+	}
+}
