5d4417d51849c92378deea001650245080ee59d2
DeadDie <deaddie@inbox.ru>
2017-07-10 02:04:00 +0300

    Portal wip

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/levels/objects/PortalGate.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/levels/objects/PortalGate.java
@@ -1,10 +1,14 @@
 package com.nyrds.pixeldungeon.levels.objects;
 
 import com.nyrds.pixeldungeon.ml.R;
+import com.nyrds.pixeldungeon.windows.WndPortal;
 import com.watabou.noosa.Game;
 import com.watabou.pixeldungeon.actors.Char;
 import com.watabou.pixeldungeon.actors.hero.Hero;
 import com.watabou.pixeldungeon.levels.Level;
+import com.watabou.pixeldungeon.scenes.GameScene;
+import com.watabou.pixeldungeon.ui.GameLog;
+import com.watabou.pixeldungeon.utils.GLog;
 import com.watabou.utils.Bundle;
 import com.watabou.utils.Callback;
 
@@ -15,6 +19,12 @@ import org.json.JSONObject;
  * Created by mike on 01.07.2016.
  */
 public class PortalGate extends LevelObject {
+
+	private boolean activated = false;
+	private boolean used = false;
+
+	private static final String TXT_USED = Game.getVar(R.string.PortalGate_Used);
+	private static final String TXT_ACTIVATED = Game.getVar(R.string.PortalGate_Activated);
 
 	public PortalGate(){
 		super(-1);
@@ -28,24 +38,38 @@ public class PortalGate extends LevelObject {
 	void setupFromJson(Level level, JSONObject obj) throws JSONException {
 	}
 
-	@Override
-	public boolean interact(Hero hero) {
 
+	private void playStartUpAnim(){
 		sprite.playAnim(6, false, new Callback() {
 			@Override
 			public void call() {
-				sprite.playAnim(6, true, new Callback() {
-					@Override
-					public void call() {
-
-
-					}
-				}, image() + 17,image() + 18,image() + 19);
-
-
+				playActiveLoop();
+				activated = true;
+				GLog.w( TXT_ACTIVATED );
 			}
 		}, image() + 0, image() + 1, image() + 2, image() + 3, image() + 4, image() + 5, image() + 6, image() + 7, image() + 8, image() + 9, image() + 10, image() + 11, image() + 12, image() + 13, image() + 14, image() + 15, image() + 16);
 
+	}
+
+	private void playActiveLoop(){
+		sprite.playAnim(6, true, new Callback() {
+			@Override
+			public void call() {
+			}
+		}, image() + 17, image() + 18, image() + 19, image() + 20, image() + 21);
+	}
+
+	@Override
+	public boolean interact(Hero hero) {
+		if(!used){
+			if(!activated){
+				playStartUpAnim();
+			} else{
+				GameScene.show(new WndPortal());
+			}
+		} else{
+			GLog.w( TXT_USED );
+		}
 		return false;
 	}
 
