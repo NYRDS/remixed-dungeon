66db83f62b01f2ae2c147bdb114afca1fc4f1504
Mikhael-Danilov <mikhael.danilov@gmail.com>
2017-07-30 01:11:58 +0300

    able to generate stubclass

--- processor/src/main/java/com/nyrds/PdAnnotationProcessor.java
+++ processor/src/main/java/com/nyrds/PdAnnotationProcessor.java
@@ -2,9 +2,17 @@ package com.nyrds;
 
 import com.google.auto.service.AutoService;
 import com.google.common.collect.ImmutableSet;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeName;
+import com.squareup.javapoet.TypeSpec;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -12,6 +20,7 @@ import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.tools.JavaFileObject;
 
@@ -19,35 +28,45 @@ import javax.tools.JavaFileObject;
 public class PdAnnotationProcessor extends AbstractProcessor{
 	@Override
 	public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
-		StringBuilder builder = new StringBuilder()
-				.append("package com.nyrds.generated;\n\n")
-				.append("public class GeneratedClass {\n\n") // open class
-				.append("\tpublic String getMessage() {\n") // open method
-				.append("\t\treturn \"");
 
 
+		Map<Element, Set<Element>> fieldsByClass = new HashMap<>();
 		// for each javax.lang.model.element.Element annotated with the CustomAnnotation
 		for (Element element : roundEnvironment.getElementsAnnotatedWith(Packable.class)) {
-			String objectType = element.getSimpleName().toString();
+			Element parent = element.getEnclosingElement();
 
-			// this is appending to the return statement
-			builder.append(objectType).append(" says hello!\\n");
+			if(fieldsByClass.get(parent)==null) {
+				fieldsByClass.put(parent,new HashSet<Element>());
+			}
 
+			fieldsByClass.get(parent).add(element);
 		}
 
 
-		builder.append("\";\n") // end return
-				.append("\t}\n") // close method
-				.append("}\n"); // close class
+		TypeSpec BundleHelper = TypeSpec.classBuilder("BundleHelper")
+				.addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+				.build();
 
+		for(Element clazz:fieldsByClass.keySet()){
+			String clazzName = clazz.getSimpleName().toString();
 
+			MethodSpec methodSpec = MethodSpec.methodBuilder(clazzName+"Pack")
+					.addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+					.returns(void.class)
+					.addParameter(TypeName.get(clazz.asType()), "arg")
+					.addParameter(ClassName.get("com.watabou.utils","Bundle"), "bundle")
+					.build();
+			BundleHelper = BundleHelper.toBuilder().addMethod(methodSpec).build();
+		}
+
+		JavaFile javaFile = JavaFile.builder("com.nydrs.generated", BundleHelper)
+				.build();
 
 		try { // write the file
-			JavaFileObject source = processingEnv.getFiler().createSourceFile("com.nyrds.generated.GeneratedClass");
-
+			JavaFileObject source = processingEnv.getFiler().createSourceFile("com.nyrds.generated.BundleHelper");
 
 			Writer writer = source.openWriter();
-			writer.write(builder.toString());
+			javaFile.writeTo(writer);
 			writer.flush();
 			writer.close();
 
