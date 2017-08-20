679994afcd7e49d9750a413413f84ac501239419
Mikhael-Danilov <mikhael.danilov@gmail.com>
2017-08-20 17:10:50 +0300

    Google Play CheckBox - is intention to connect, not connection status

--- PixelDungeon/src/main/java/com/watabou/pixeldungeon/ui/WndPlayGames.java
+++ PixelDungeon/src/main/java/com/watabou/pixeldungeon/ui/WndPlayGames.java
@@ -1,26 +1,13 @@
 package com.watabou.pixeldungeon.ui;
 
-import android.util.Log;
-
-import com.nyrds.android.util.FileSystem;
 import com.nyrds.android.util.GuiProperties;
-import com.nyrds.android.util.Unzip;
-import com.nyrds.pixeldungeon.items.common.Library;
-import com.nyrds.pixeldungeon.ml.BuildConfig;
 import com.nyrds.pixeldungeon.ml.R;
 import com.nyrds.pixeldungeon.support.PlayGames;
 import com.watabou.noosa.Game;
 import com.watabou.noosa.Text;
-import com.watabou.pixeldungeon.Badges;
-import com.watabou.pixeldungeon.Rankings;
+import com.watabou.pixeldungeon.Preferences;
 import com.watabou.pixeldungeon.scenes.PixelScene;
 import com.watabou.pixeldungeon.windows.WndMessage;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileFilter;
-import java.io.IOException;
 
 /**
  * Created by mike on 14.05.2017.
@@ -47,7 +34,7 @@ class WndPlayGames extends Window {
 
 		y += listTitle.height() + GAP;
 
-		CheckBox usePlayGames = new CheckBox(Game.getVar(R.string.WndPlayGames_Use), playGamesConnected) {
+		CheckBox usePlayGames = new CheckBox(Game.getVar(R.string.WndPlayGames_Use), Preferences.INSTANCE.getBoolean(Preferences.KEY_USE_PLAY_GAMES, false)) {
 			@Override
 			public void checked(boolean value) {
 				super.checked(value);
@@ -86,53 +73,7 @@ class WndPlayGames extends Window {
 			}
 		});
 
-		if(BuildConfig.DEBUG) {
-
-			addButton(new RedButton("Zip Test") {
-				@Override
-				protected void onClick() {
-					super.onClick();
-
-					ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-					try {
-						long t1 = System.nanoTime();
-						FileSystem.zipFolderTo(out, FileSystem.getInternalStorageFile(""), 0, null);
-						float size = out.toByteArray().length;
-						long t2 = System.nanoTime();
-						float time = (t2 - t1) / 1000000f;
-						Log.i("zipped", String.format("size :%4.2f, time: %4.2f", size / 1024f, time));
-
-						FileSystem.deleteRecursive(FileSystem.getInternalStorageFile(""));
-
-						t1 = System.nanoTime();
-						Unzip.unzip(new ByteArrayInputStream(out.toByteArray()),
-								FileSystem.getInternalStorageFile("").getAbsolutePath());
-						t2 = System.nanoTime();
-						time = (t2 - t1) / 1000000f;
-						Log.i("unzipped", String.format("size :%4.2f, time: %4.2f", size / 1024f, time));
-
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			});
-		}
 		resize(width, y);
-	}
-
-	private void showActionResult(final boolean res) {
-		Game.executeInGlThread(new Runnable() {
-			@Override
-			public void run() {
-				if (res) {
-					Game.scene().add(new WndMessage(Game.getVar(R.string.WndPlayGames_Show_Ok)));
-				} else {
-					Game.scene().add(new WndMessage(Game.getVar(R.string.WndPlayGames_Show_Error)));
-				}
-				hide();
-			}
-		});
 	}
 
 	private void addButton(TextButton btn) {
