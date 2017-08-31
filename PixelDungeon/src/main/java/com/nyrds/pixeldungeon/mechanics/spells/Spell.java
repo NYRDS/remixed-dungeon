505e01d28b9b35e9d0e789f9c8ee2dab07d4f605
DeadDie <deaddie@inbox.ru>
2017-08-31 18:34:30 +0300

    Sorting out casting a spell and overriding "default" variables for actual spells

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mechanics/spells/Spell.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mechanics/spells/Spell.java
@@ -4,6 +4,7 @@ import com.nyrds.pixeldungeon.ml.R;
 import com.watabou.gltextures.SmartTexture;
 import com.watabou.gltextures.TextureCache;
 import com.watabou.noosa.Game;
+import com.watabou.noosa.Image;
 import com.watabou.noosa.TextureFilm;
 import com.watabou.pixeldungeon.actors.hero.Hero;
 import com.watabou.pixeldungeon.utils.GLog;
@@ -14,35 +15,50 @@ import org.json.JSONObject;
 
 public class Spell {
     private static final String TXT_NOT_ENOUGH_SOULS   = Game.getVar(R.string.Necromancy_NotEnoughSouls);
+    private int spellCost = 5;
 
     protected String targetingType;
     protected String magicAffinity;
 
-    protected String textureFile = "spells/common.png";
+    protected String textureFile = "spellsIcons/common.png";
     protected String name = "Spell";
+    protected String desc = "Description";
     protected int textureResolution = 16;
-    protected int imageIndex = 0;
-    private int spellCost = 5;
 
-    private SmartTexture icon = TextureCache.get(textureFile);
-    private TextureFilm film = new TextureFilm( icon, textureResolution, textureResolution );
+    private SmartTexture icon = TextureCache.get(texture());
+
+    public int imageIndex = 0;
+    public TextureFilm film = new TextureFilm( icon, textureResolution(), textureResolution() );
 
     void setupFromJson( JSONObject obj) throws JSONException {
         name = obj.optString("name", name);
+        desc = obj.optString("name", desc);
         textureFile = obj.optString("textureFile", textureFile);
         imageIndex  = obj.optInt("imageIndex", imageIndex);
         magicAffinity = obj.optString("magicAffinity", magicAffinity);
         targetingType = obj.optString("targetingType", targetingType);
-        textureResolution  = obj.optInt("textureResolution", textureResolution);
+        textureResolution  = obj.optInt("textureResolution", textureResolution());
         spellCost  = obj.optInt("spellCost", spellCost);
     }
 
-    public void cast(Hero hero){
+    public void use(Hero hero){
         if(!hero.spendSoulPoints(spellCost())){
             GLog.w( notEnoughSouls(name) );
             return;
         }
+        cast(hero);
+    }
 
+    protected void cast(Hero hero){
+
+    }
+
+    public String name(){
+        return name;
+    }
+
+    public String desc(){
+        return desc;
     }
 
     public String getMagicAffinity(){return magicAffinity;}
@@ -51,8 +67,8 @@ public class Spell {
         return textureFile;
     }
 
-    public int image() {
-        return imageIndex;
+    public Image image() {
+        return new Image( icon() );
     }
 
     public int spellCost(){
@@ -61,5 +77,13 @@ public class Spell {
 
     public static String notEnoughSouls (String spell) {
         return Utils.format(TXT_NOT_ENOUGH_SOULS, spell);
+    }
+
+    public SmartTexture icon (){
+        return icon;
+    }
+
+    public int textureResolution(){
+        return textureResolution;
     }
 }
---
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mechanics/spells/SummonDeathling.java
@@ -0,0 +1,76 @@
+package com.nyrds.pixeldungeon.mechanics.spells;
+
+import com.nyrds.pixeldungeon.mechanics.Necromancy;
+import com.nyrds.pixeldungeon.ml.R;
+import com.nyrds.pixeldungeon.mobs.common.Deathling;
+import com.watabou.gltextures.SmartTexture;
+import com.watabou.gltextures.TextureCache;
+import com.watabou.noosa.Game;
+import com.watabou.noosa.TextureFilm;
+import com.watabou.pixeldungeon.Dungeon;
+import com.watabou.pixeldungeon.actors.buffs.Buff;
+import com.watabou.pixeldungeon.actors.hero.Hero;
+import com.watabou.pixeldungeon.actors.mobs.Mob;
+import com.watabou.pixeldungeon.effects.Wound;
+import com.watabou.pixeldungeon.items.Item;
+import com.watabou.pixeldungeon.levels.Level;
+import com.watabou.pixeldungeon.plants.Sungrass;
+import com.watabou.pixeldungeon.utils.GLog;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Collection;
+
+public class SummonDeathling extends Spell {
+
+    private int limit = 1;
+
+    public SummonDeathling(){
+        targetingType = SpellHelper.TARGET_NONE;
+        magicAffinity = SpellHelper.AFFINITIES[0];
+        name          = Game.getVar(R.string.Necromancy_SummonDeathlingName);
+        desc          = Game.getVar(R.string.SummonDeathling_Info);
+        imageIndex = 0;
+    }
+
+    @Override
+    public void cast(Hero hero){
+        super.cast(hero);
+            Collection<Mob> pets = Dungeon.hero.getPets();
+
+            int n = 0;
+            for (Mob mob : pets){
+                if (mob.isAlive() && mob instanceof Deathling) {
+                    n++;
+                }
+            }
+
+            if (n >= limit){
+                GLog.w( Necromancy.getLimitWarning(limit) );
+                return;
+            }
+            int spawnPos = Dungeon.level.getEmptyCellNextTo(hero.getPos());
+
+            Wound.hit(hero);
+            Buff.detach(hero, Sungrass.Health.class);
+
+            if (Dungeon.level.cellValid(spawnPos)) {
+                Mob pet = Mob.makePet(new Deathling(), hero);
+                pet.setPos(spawnPos);
+                Dungeon.level.spawnMob(pet);
+            }
+
+            hero.spend(1/hero.speed());
+    }
+
+    @Override
+    public String texture(){
+        return "spellsIcons/necromancy.png";
+    }
+
+    @Override
+    public int textureResolution(){
+        return 32;
+    }
+}
