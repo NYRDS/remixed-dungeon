f7da3096af0fbc9ecfb2e988fac09f5aea0dc324
DeadDie <deaddie@inbox.ru>
2017-09-08 01:03:55 +0300

    clean up

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mechanics/Necromancy.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mechanics/Necromancy.java
@@ -1,35 +1,11 @@
 package com.nyrds.pixeldungeon.mechanics;
 
-import com.nyrds.pixeldungeon.items.common.armor.NecromancerArmor;
 import com.nyrds.pixeldungeon.ml.R;
-import com.nyrds.pixeldungeon.mobs.common.Deathling;
 import com.watabou.noosa.Game;
-import com.watabou.pixeldungeon.Dungeon;
-import com.watabou.pixeldungeon.actors.buffs.Buff;
-import com.watabou.pixeldungeon.actors.hero.Hero;
-import com.watabou.pixeldungeon.actors.hero.HeroSubClass;
-import com.watabou.pixeldungeon.actors.mobs.Mob;
-import com.watabou.pixeldungeon.effects.Wound;
-import com.watabou.pixeldungeon.items.Item;
-import com.watabou.pixeldungeon.plants.Sungrass;
-import com.watabou.pixeldungeon.utils.GLog;
-import com.watabou.pixeldungeon.utils.Utils;
-
-import java.util.Collection;
 
 public class Necromancy {
-
-
-
 	private static final String TXT_SUMMON_DEATHLING   = Game.getVar(R.string.Necromancy_SummonDeathlingName);
 	private static final String TXT_REINCARNATION   = Game.getVar(R.string.Necromancy_ReincarnationName);
 
-
 	private static String REINCARNATION = "REINCARNATION";
-
-
-
-
-
-
 }
