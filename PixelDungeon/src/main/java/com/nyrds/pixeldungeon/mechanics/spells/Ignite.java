bddfb24798071e5cbe0289ef723aa3d769ae7f32
DeadDie <deaddie@inbox.ru>
2017-09-20 16:28:49 +0300

    check for valid cell on ignite

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mechanics/spells/Ignite.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mechanics/spells/Ignite.java
@@ -1,23 +1,12 @@
 package com.nyrds.pixeldungeon.mechanics.spells;
 
-import com.nyrds.pixeldungeon.ml.R;
-import com.watabou.noosa.Game;
 import com.watabou.pixeldungeon.Dungeon;
-import com.watabou.pixeldungeon.ResultDescriptions;
-import com.watabou.pixeldungeon.actors.Actor;
 import com.watabou.pixeldungeon.actors.Char;
 import com.watabou.pixeldungeon.actors.blobs.Blob;
 import com.watabou.pixeldungeon.actors.blobs.Fire;
-import com.watabou.pixeldungeon.actors.blobs.LiquidFlame;
-import com.watabou.pixeldungeon.actors.buffs.Buff;
-import com.watabou.pixeldungeon.actors.buffs.Burning;
 import com.watabou.pixeldungeon.actors.hero.Hero;
-import com.watabou.pixeldungeon.effects.particles.FlameParticle;
 import com.watabou.pixeldungeon.mechanics.Ballistica;
 import com.watabou.pixeldungeon.scenes.GameScene;
-import com.watabou.pixeldungeon.utils.GLog;
-import com.watabou.pixeldungeon.utils.Utils;
-import com.watabou.utils.Random;
 
 public class Ignite extends Spell{
 
@@ -33,13 +22,15 @@ public class Ignite extends Spell{
 
 	@Override
 	public boolean cast(Char chr, int cell){
-		if(Ballistica.cast(chr.getPos(), cell, false, true) == cell) {
-			GameScene.add( Blob.seed( cell, 5, Fire.class ) );
-			if(chr instanceof Hero) {
-				Hero hero = (Hero) chr;
-				castCallback(hero);
+		if(Dungeon.level.cellValid(cell)) {
+			if(Ballistica.cast(chr.getPos(), cell, false, true) == cell) {
+				GameScene.add( Blob.seed( cell, 5, Fire.class ) );
+				if(chr instanceof Hero) {
+					Hero hero = (Hero) chr;
+					castCallback(hero);
+				}
+				return true;
 			}
-			return true;
 		}
 		return false;
 	}
