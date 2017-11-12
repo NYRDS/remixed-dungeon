eabf33f39d298778eedda6fa9163e584ab1e8c08
Mikhael-Danilov <mikhael.danilov@gmail.com>
2017-11-12 22:42:55 +0300

    Spawner script example

--- PixelDungeon/src/main/assets/scripts/commonClasses.lua
+++ PixelDungeon/src/main/assets/scripts/commonClasses.lua
@@ -10,25 +10,26 @@ RPD = {
     Dungeon = luajava.bindClass("com.watabou.pixeldungeon.Dungeon"),
     Terrain = luajava.bindClass("com.watabou.pixeldungeon.levels.Terrain"),
     Actor = luajava.bindClass("com.watabou.pixeldungeon.actors.Actor"),
+    MobFactory = luajava.bindClass("com.nyrds.pixeldungeon.mobs.common.MobFactory"),
+    ItemFactory = luajava.bindClass("com.nyrds.pixeldungeon.items.common.ItemFactory"),
     Blobs = {
-        Blob      = luajava.bindClass("com.watabou.pixeldungeon.actors.blobs.Blob"),
-        Fire      = luajava.bindClass("com.watabou.pixeldungeon.actors.blobs.Fire"),
-        Foliage   = luajava.bindClass("com.watabou.pixeldungeon.actors.blobs.Foliage")
+        Blob = luajava.bindClass("com.watabou.pixeldungeon.actors.blobs.Blob"),
+        Fire = luajava.bindClass("com.watabou.pixeldungeon.actors.blobs.Fire"),
+        Foliage = luajava.bindClass("com.watabou.pixeldungeon.actors.blobs.Foliage")
     },
     Sfx = {
-        CellEmitter   = luajava.bindClass("com.watabou.pixeldungeon.effects.CellEmitter"),
-        Emitter       = luajava.bindClass("com.watabou.noosa.particles.Emitter"),
+        CellEmitter = luajava.bindClass("com.watabou.pixeldungeon.effects.CellEmitter"),
+        Emitter = luajava.bindClass("com.watabou.noosa.particles.Emitter"),
         FlameParticle = luajava.bindClass("com.watabou.pixeldungeon.effects.particles.FlameParticle"),
-        SnowParticle  = luajava.bindClass("com.watabou.pixeldungeon.effects.particles.SnowParticle")
+        SnowParticle = luajava.bindClass("com.watabou.pixeldungeon.effects.particles.SnowParticle")
     },
-
     Objects = {
         Ui = {
             WndMessage = "com.watabou.pixeldungeon.windows.WndMessage",
-            WndStory   = "com.watabou.pixeldungeon.windows.WndStory"
+            WndStory = "com.watabou.pixeldungeon.windows.WndStory"
         },
         Actors = {
-          ScriptedActor = "com.nyrds.pixeldungeon.mechanics.actors.ScriptedActor"
+            ScriptedActor = "com.nyrds.pixeldungeon.mechanics.actors.ScriptedActor"
         },
     },
     new = function(class, ...)
---
+++ PixelDungeon/src/main/assets/scripts/traps/Spawner.lua
@@ -0,0 +1,32 @@
+--
+-- User: mike
+-- Date: 12.11.2017
+-- Time: 22:16
+-- This file is part of Remixed Pixel Dungeon.
+--
+
+local RPD = require "scripts/commonClasses"
+
+local trap = require"scripts/TrapCommon"
+
+return trap.init(
+    function (cell, char, data)
+        local mobs  = {"Rat","Gnoll"}
+        local items = {"Dagger","RatHide"}
+        local level = RPD.Dungeon.level
+
+        if math.random() > 0.5 then
+            local item = RPD.ItemFactory:itemByName(items[math.random(1,#items)])
+            item:upgrade()
+            level:drop(item,cell)
+        else
+            local mob = RPD.MobFactory:mobByName(mobs[math.random(1,#mobs)])
+            local mobPos = level:getEmptyCellNextTo(cell)
+            if level:cellValid(mobPos) then
+                mob:setPos(mobPos)
+                level:spawnMob(mob)
+            end
+        end
+    end
+)
+
