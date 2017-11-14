eea20d8ce026b1065755b9b0dca38d936995d7f6
DeadDie <deaddie@inbox.ru>
2017-11-14 14:50:04 +0300

    yet another dialog system wip

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mechanics/dialog/Dialog.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/mechanics/dialog/Dialog.java
@@ -1,41 +1,57 @@
 package com.nyrds.pixeldungeon.mechanics.dialog;
 
-import com.nyrds.Packable;
-import com.watabou.noosa.Image;
+import com.nyrds.android.util.JsonHelper;
+import com.nyrds.android.util.TrackedRuntimeException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Dialog {
 
-    // Диалог это объект, имеющий идентификатор, по которому игра должна его искать, после того как диалог будет зарегестрирован через Json
-    // Диалог имеет изображение, которое содержит набор спрайтов, которые должны быть показаны во время диалога
-    // Диалог имеет массив Диалоговых карточек, каждая из которых хранит описание конкретного "этапа" диалога
-    // Диалог движется путём смены диалоговых карточек, будь то последовательное пролистывание или вызов конкретного набора карт при нажатии диалоговой опции ("варианта ответа")
-    @Packable
-    private String imageName;
-    @Packable
-    private DialogCard[] cardTable;
-    @Packable
-    private String dialogID;
+    public int imageIndex;
 
-    private void setUpFromJson() {
+    public String dialogID;
+    public String imagePath;
+    public String title;
+    public String text;
+    public String portraitPos;
 
+    public Dialog getFromJson(String targetID, int cardNum) {
+        JSONObject initDialogs = JsonHelper.readJsonFromAsset("dialogDesc/Dialogs.json");
+        if (initDialogs.has("dialogs")) {
+            Dialog dialog = new Dialog();
+            try {
+                JSONArray dialogs = initDialogs.getJSONArray("dialogs");
+                for (int i = 0; i < dialogs.length(); ++i) {
+                    JSONObject currentDialog = dialogs.getJSONObject(i);
+                    if (currentDialog.has("id")) {
+                        String tempID = currentDialog.optString("id", "test");
+                        if (tempID.equals(targetID)) {
+                            dialog.dialogID = tempID;
+                        }
+                        break;
+                    }
+                    if (currentDialog.has("image")) {
+                        dialog.imagePath = currentDialog.optString("image", "mobs/dialogAvatars.png");
+                    }
+                    if (currentDialog.has("dialogCards")) {
+                        JSONArray cards = currentDialog.getJSONArray("dialogCards");
+                        for (int j = 0; j < cards.length(); ++j) {
+                            if(j == cardNum) {
+                                dialog.imageIndex = cards.getJSONObject(j).optInt("imageIndex", 0);
+                                dialog.portraitPos = cards.getJSONObject(j).optString("portraitPos", "right");
+                                dialog.title = cards.getJSONObject(j).optString("title", "N/A");
+                                dialog.text = cards.getJSONObject(j).optString("text", "No text found");
+                                return dialog;
+                            }
+                        }
+                    }
+                }
+            } catch (JSONException e) {
+                throw new TrackedRuntimeException(e);
+            }
+        }
+        return null;
     }
-
-    public void setPortraitImage(){
-
-    }
-
-    public void setDialogCardTable(){
-
-    }
-
-    public String getDialogID(){
-        return dialogID;
-    }
-
-    public DialogCard getNextCard() {
-        int next = 0;
-
-        return cardTable[next];
-    }
-
 }
