1321f6cc0bd15a08fba7c10e3afdc71ccc267765
Mikhael-Danilov <mikhael.danilov@gmail.com>
2017-12-30 22:11:16 +0300

    magicAffinity should not be static

--- PixelDungeon/src/main/java/com/watabou/pixeldungeon/actors/hero/HeroClass.java
+++ PixelDungeon/src/main/java/com/watabou/pixeldungeon/actors/hero/HeroClass.java
@@ -52,237 +52,238 @@ import org.json.JSONObject;
 
 public enum HeroClass {
 
-	WARRIOR(Game.getVar(R.string.HeroClass_War), WarriorArmor.class, Ordinary.instance),
-	MAGE(Game.getVar(R.string.HeroClass_Mag), MageArmor.class, Ordinary.instance),
-	ROGUE(Game.getVar(R.string.HeroClass_Rog), RogueArmor.class, Ordinary.instance),
-	HUNTRESS(Game.getVar(R.string.HeroClass_Hun), HuntressArmor.class, Ordinary.instance),
-	ELF(Game.getVar(R.string.HeroClass_Elf), ElfArmor.class, Ordinary.instance),
-	NECROMANCER(Game.getVar(R.string.HeroClass_Necromancer), NecromancerArmor.class, Ordinary.instance);
+    WARRIOR(Game.getVar(R.string.HeroClass_War), WarriorArmor.class, Ordinary.instance),
+    MAGE(Game.getVar(R.string.HeroClass_Mag), MageArmor.class, Ordinary.instance),
+    ROGUE(Game.getVar(R.string.HeroClass_Rog), RogueArmor.class, Ordinary.instance),
+    HUNTRESS(Game.getVar(R.string.HeroClass_Hun), HuntressArmor.class, Ordinary.instance),
+    ELF(Game.getVar(R.string.HeroClass_Elf), ElfArmor.class, Ordinary.instance),
+    NECROMANCER(Game.getVar(R.string.HeroClass_Necromancer), NecromancerArmor.class, Ordinary.instance);
 
-	private final Class<? extends ClassArmor> armorClass;
+    private final Class<? extends ClassArmor> armorClass;
 
-	private String    title;
-	private Abilities abilities;
-	static private JSONObject initHeroes = JsonHelper.readJsonFromAsset(BuildConfig.DEBUG ? "hero/initHeroesDebug.json":"hero/initHeroes.json");
+    private String    title;
+    private Abilities abilities;
+    static private JSONObject initHeroes = JsonHelper.readJsonFromAsset(BuildConfig.DEBUG ? "hero/initHeroesDebug.json" : "hero/initHeroes.json");
 
-	private        boolean isSpellUser;
-	private static String  magicAffinity;
+    private boolean isSpellUser;
+    private String  magicAffinity;
 
-	private static final String[] WAR_PERKS         = Game
-			.getVars(R.array.HeroClass_WarPerks);
-	private static final String[] MAG_PERKS         = Game
-			.getVars(R.array.HeroClass_MagPerks);
-	private static final String[] ROG_PERKS         = Game
-			.getVars(R.array.HeroClass_RogPerks);
-	private static final String[] HUN_PERKS         = Game
-			.getVars(R.array.HeroClass_HunPerks);
-	private static final String[] ELF_PERKS         = Game
-			.getVars(R.array.HeroClass_ElfPerks);
-	private static final String[] NECROMANCER_PERKS = Game
-			.getVars(R.array.HeroClass_NecromancerPerks);
+    private static final String[] WAR_PERKS         = Game
+            .getVars(R.array.HeroClass_WarPerks);
+    private static final String[] MAG_PERKS         = Game
+            .getVars(R.array.HeroClass_MagPerks);
+    private static final String[] ROG_PERKS         = Game
+            .getVars(R.array.HeroClass_RogPerks);
+    private static final String[] HUN_PERKS         = Game
+            .getVars(R.array.HeroClass_HunPerks);
+    private static final String[] ELF_PERKS         = Game
+            .getVars(R.array.HeroClass_ElfPerks);
+    private static final String[] NECROMANCER_PERKS = Game
+            .getVars(R.array.HeroClass_NecromancerPerks);
 
-	HeroClass(String title, Class<? extends ClassArmor> armorClass, Abilities abilities) {
-		this.title = title;
-		this.armorClass = armorClass;
-		this.abilities = abilities;
-	}
+    HeroClass(String title, Class<? extends ClassArmor> armorClass, Abilities abilities) {
+        this.title = title;
+        this.armorClass = armorClass;
+        this.abilities = abilities;
+    }
 
-	public boolean allowed() {
-		return initHeroes.has(name());
-	}
+    public boolean allowed() {
+        return initHeroes.has(name());
+    }
 
-	public void initHero(Hero hero) {
-		hero.heroClass = this;
-		initCommon(hero);
-		initForClass(hero, hero.heroClass.name());
+    public void initHero(Hero hero) {
+        hero.heroClass = this;
+        initCommon(hero);
+        initForClass(hero, hero.heroClass.name());
 
-		hero.setGender(getGender());
+        hero.setGender(getGender());
 
-		if (Badges.isUnlocked(masteryBadge()) && hero.getDifficulty() < 3) {
-			{
-				if (hero.heroClass != HeroClass.NECROMANCER) {
-					new TomeOfMastery().collect(hero);
-				}
-			}
-		}
-		hero.updateAwareness();
-	}
+        if (Badges.isUnlocked(masteryBadge()) && hero.getDifficulty() < 3) {
+            {
+                if (hero.heroClass != HeroClass.NECROMANCER) {
+                    new TomeOfMastery().collect(hero);
+                }
+            }
+        }
+        hero.updateAwareness();
+    }
 
-	private static void initForClass(Hero hero, String className) {
-		if (initHeroes.has(className)) {
-			try {
-				JSONObject classDesc = initHeroes.getJSONObject(className);
-				if (classDesc.has("armor")) {
-					Armor armor = (Armor) ItemFactory.createItemFromDesc(classDesc.getJSONObject("armor"));
-					hero.belongings.armor = armor;
-				}
+    private static void initForClass(Hero hero, String className) {
+        if (initHeroes.has(className)) {
+            try {
+                JSONObject classDesc = initHeroes.getJSONObject(className);
+                if (classDesc.has("armor")) {
+                    Armor armor = (Armor) ItemFactory.createItemFromDesc(classDesc.getJSONObject("armor"));
+                    hero.belongings.armor = armor;
+                }
 
-				if (classDesc.has("weapon")) {
-					KindOfWeapon weapon = (KindOfWeapon) ItemFactory.createItemFromDesc(classDesc.getJSONObject("weapon"));
-					hero.belongings.weapon = weapon;
-				}
+                if (classDesc.has("weapon")) {
+                    KindOfWeapon weapon = (KindOfWeapon) ItemFactory.createItemFromDesc(classDesc.getJSONObject("weapon"));
+                    hero.belongings.weapon = weapon;
+                }
 
-				if (classDesc.has("ring1")) {
-					hero.belongings.ring1 = (Artifact) ItemFactory.createItemFromDesc(classDesc.getJSONObject("ring1"));
-					hero.belongings.ring1.activate(hero);
-				}
+                if (classDesc.has("ring1")) {
+                    hero.belongings.ring1 = (Artifact) ItemFactory.createItemFromDesc(classDesc.getJSONObject("ring1"));
+                    hero.belongings.ring1.activate(hero);
+                }
 
-				if (classDesc.has("ring2")) {
-					hero.belongings.ring2 = (Artifact) ItemFactory.createItemFromDesc(classDesc.getJSONObject("ring2"));
-					hero.belongings.ring2.activate(hero);
-				}
+                if (classDesc.has("ring2")) {
+                    hero.belongings.ring2 = (Artifact) ItemFactory.createItemFromDesc(classDesc.getJSONObject("ring2"));
+                    hero.belongings.ring2.activate(hero);
+                }
 
-				if (classDesc.has("items")) {
-					JSONArray items = classDesc.getJSONArray("items");
-					for (int i = 0; i < items.length(); ++i) {
-						hero.collect(ItemFactory.createItemFromDesc(items.getJSONObject(i)));
-					}
-				}
+                if (classDesc.has("items")) {
+                    JSONArray items = classDesc.getJSONArray("items");
+                    for (int i = 0; i < items.length(); ++i) {
+                        hero.collect(ItemFactory.createItemFromDesc(items.getJSONObject(i)));
+                    }
+                }
 
-				if (classDesc.has("quickslot")) {
-					int slot = 0;
-					JSONArray quickslots = classDesc.getJSONArray("quickslot");
-					for (int i = 0; i < quickslots.length(); ++i) {
-						Item item = ItemFactory.createItemFromDesc(quickslots.getJSONObject(i));
+                if (classDesc.has("quickslot")) {
+                    int slot = 0;
+                    JSONArray quickslots = classDesc.getJSONArray("quickslot");
+                    for (int i = 0; i < quickslots.length(); ++i) {
+                        Item item = ItemFactory.createItemFromDesc(quickslots.getJSONObject(i));
 
-						if (hero.belongings.getItem(item.getClass()) != null) {
-							QuickSlot.selectItem(hero.belongings.getItem(item.getClass()), slot);
-							slot++;
-						}
-					}
-				}
+                        if (hero.belongings.getItem(item.getClass()) != null) {
+                            QuickSlot.selectItem(hero.belongings.getItem(item.getClass()), slot);
+                            slot++;
+                        }
+                    }
+                }
 
-				if (classDesc.has("knownItems")) {
-					JSONArray knownItems = classDesc.getJSONArray("knownItems");
-					for (int i = 0; i < knownItems.length(); ++i) {
-						Item item = ItemFactory.createItemFromDesc(knownItems.getJSONObject(i));
-						if (item instanceof UnknownItem) {
-							((UnknownItem) item).setKnown();
-						}
-					}
-				}
+                if (classDesc.has("knownItems")) {
+                    JSONArray knownItems = classDesc.getJSONArray("knownItems");
+                    for (int i = 0; i < knownItems.length(); ++i) {
+                        Item item = ItemFactory.createItemFromDesc(knownItems.getJSONObject(i));
+                        if (item instanceof UnknownItem) {
+                            ((UnknownItem) item).setKnown();
+                        }
+                    }
+                }
 
-				hero.STR(classDesc.optInt("str", hero.STR()));
-				hero.hp(hero.ht(classDesc.optInt("hp", hero.ht())));
-				hero.heroClass.isSpellUser(classDesc.optBoolean("isSpellUser", false));
-				hero.heroClass.setMagicAffinity(classDesc.optString("magicAffinity", "Common"));
-				hero.setMaxSoulPoints(classDesc.optInt("maxSp", 10));
-				hero.setSoulPoints(classDesc.optInt("startingSp", 0));
+                hero.STR(classDesc.optInt("str", hero.STR()));
+                hero.hp(hero.ht(classDesc.optInt("hp", hero.ht())));
+                hero.heroClass.isSpellUser(classDesc.optBoolean("isSpellUser", false));
+                hero.heroClass.setMagicAffinity(classDesc.optString("magicAffinity", "Common"));
+                hero.setMaxSoulPoints(classDesc.optInt("maxSp", 10));
+                hero.setSoulPoints(classDesc.optInt("startingSp", 0));
 
-			} catch (JSONException e) {
-				throw new TrackedRuntimeException(e);
-			} catch (InstantiationException e) {
-				throw new TrackedRuntimeException(e);
-			} catch (IllegalAccessException e) {
-				throw new TrackedRuntimeException(e);
-			}
-		}
-	}
+            } catch (JSONException e) {
+                throw new TrackedRuntimeException(e);
+            } catch (InstantiationException e) {
+                throw new TrackedRuntimeException(e);
+            } catch (IllegalAccessException e) {
+                throw new TrackedRuntimeException(e);
+            }
+        }
+    }
 
-	private static void initCommon(Hero hero) {
-		QuickSlot.cleanStorage();
-		initForClass(hero, "common");
-		if (hero.getDifficulty() < 3) {
-			initForClass(hero, "non_expert");
-		}
-	}
+    private static void initCommon(Hero hero) {
+        QuickSlot.cleanStorage();
+        initForClass(hero, "common");
+        if (hero.getDifficulty() < 3) {
+            initForClass(hero, "non_expert");
+        }
+    }
 
-	public Badges.Badge masteryBadge() {
-		switch (this) {
-			case WARRIOR:
-				return Badges.Badge.MASTERY_WARRIOR;
-			case MAGE:
-				return Badges.Badge.MASTERY_MAGE;
-			case ROGUE:
-				return Badges.Badge.MASTERY_ROGUE;
-			case HUNTRESS:
-				return Badges.Badge.MASTERY_HUNTRESS;
-			case ELF:
-				return Badges.Badge.MASTERY_ELF;
-			case NECROMANCER:
-				return Badges.Badge.MASTERY_NECROMANCER;
-		}
-		return null;
-	}
+    public Badges.Badge masteryBadge() {
+        switch (this) {
+            case WARRIOR:
+                return Badges.Badge.MASTERY_WARRIOR;
+            case MAGE:
+                return Badges.Badge.MASTERY_MAGE;
+            case ROGUE:
+                return Badges.Badge.MASTERY_ROGUE;
+            case HUNTRESS:
+                return Badges.Badge.MASTERY_HUNTRESS;
+            case ELF:
+                return Badges.Badge.MASTERY_ELF;
+            case NECROMANCER:
+                return Badges.Badge.MASTERY_NECROMANCER;
+        }
+        return null;
+    }
 
 
-	public String title() {
-		return title;
-	}
+    public String title() {
+        return title;
+    }
 
-	@NonNull
-	public String[] perks() {
+    @NonNull
+    public String[] perks() {
 
-		switch (this) {
-			case WARRIOR:
-				return WAR_PERKS;
-			case MAGE:
-				return MAG_PERKS;
-			case ROGUE:
-			default:
-				return ROG_PERKS;
-			case HUNTRESS:
-				return HUN_PERKS;
-			case ELF:
-				return ELF_PERKS;
-			case NECROMANCER:
-				return NECROMANCER_PERKS;
-		}
-	}
+        switch (this) {
+            case WARRIOR:
+                return WAR_PERKS;
+            case MAGE:
+                return MAG_PERKS;
+            case ROGUE:
+            default:
+                return ROG_PERKS;
+            case HUNTRESS:
+                return HUN_PERKS;
+            case ELF:
+                return ELF_PERKS;
+            case NECROMANCER:
+                return NECROMANCER_PERKS;
+        }
+    }
 
-	public int getGender() {
-		switch (this) {
-			case WARRIOR:
-			case MAGE:
-			case ROGUE:
-			case ELF:
-			case NECROMANCER:
-				return Utils.MASCULINE;
-			case HUNTRESS:
-				return Utils.FEMININE;
-		}
-		return Utils.NEUTER;
-	}
+    public int getGender() {
+        switch (this) {
+            case WARRIOR:
+            case MAGE:
+            case ROGUE:
+            case ELF:
+            case NECROMANCER:
+                return Utils.MASCULINE;
+            case HUNTRESS:
+                return Utils.FEMININE;
+        }
+        return Utils.NEUTER;
+    }
 
-	private static final String CLASS          = "class";
-	private static final String SPELL_AFFINITY = "affinity";
+    private static final String CLASS          = "class";
+    private static final String SPELL_AFFINITY = "affinity";
 
-	public void storeInBundle(Bundle bundle) {
-		bundle.put(CLASS, toString());
-		bundle.put(SPELL_AFFINITY, getMagicAffinity());
-	}
+    public void storeInBundle(Bundle bundle) {
+        bundle.put(CLASS, toString());
+        bundle.put(SPELL_AFFINITY, getMagicAffinity());
+    }
 
-	public static HeroClass restoreFromBundle(Bundle bundle) {
-		String value = bundle.getString(CLASS);
-		setMagicAffinity(bundle.getString(SPELL_AFFINITY));
-		return value.length() > 0 ? valueOf(value) : ROGUE;
-	}
+    public static HeroClass restoreFromBundle(Bundle bundle) {
+        String value = bundle.getString(CLASS);
+        HeroClass ret = value.length() > 0 ? valueOf(value) : ROGUE;
+        ret.setMagicAffinity(bundle.getString(SPELL_AFFINITY));
+        return ret;
+    }
 
-	public ClassArmor classArmor() {
-		try {
-			return armorClass.newInstance();
-		} catch (Exception e) {
-			throw new TrackedRuntimeException(e);
-		}
-	}
+    public ClassArmor classArmor() {
+        try {
+            return armorClass.newInstance();
+        } catch (Exception e) {
+            throw new TrackedRuntimeException(e);
+        }
+    }
 
-	Abilities getAbilities() {
-		return abilities;
-	}
+    Abilities getAbilities() {
+        return abilities;
+    }
 
-	public boolean isSpellUser() {
-		return isSpellUser;
-	}
+    public boolean isSpellUser() {
+        return isSpellUser;
+    }
 
-	public void isSpellUser(boolean b) {
-		isSpellUser = b;
-	}
+    public void isSpellUser(boolean b) {
+        isSpellUser = b;
+    }
 
-	public String getMagicAffinity() {
-		return magicAffinity;
-	}
+    public String getMagicAffinity() {
+        return magicAffinity;
+    }
 
-	public static void setMagicAffinity(String affinity) {
-		magicAffinity = affinity;
-	}
+    public void setMagicAffinity(String affinity) {
+        magicAffinity = affinity;
+    }
 }
