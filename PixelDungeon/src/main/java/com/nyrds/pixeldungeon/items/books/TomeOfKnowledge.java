f6741aceeafc040812088e438c840b11be825b38
Mikhael-Danilov <mikhael.danilov@gmail.com>
2018-01-06 15:47:16 +0300

    Fixed sprite for TomeOfKnowledge

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/books/TomeOfKnowledge.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/items/books/TomeOfKnowledge.java
@@ -1,11 +1,7 @@
 package com.nyrds.pixeldungeon.items.books;
 
-import com.nyrds.pixeldungeon.mechanics.spells.SpellHelper;
-import com.nyrds.pixeldungeon.ml.R;
-import com.watabou.noosa.Game;
 import com.watabou.noosa.audio.Sample;
 import com.watabou.pixeldungeon.Assets;
-import com.watabou.pixeldungeon.Dungeon;
 import com.watabou.pixeldungeon.actors.hero.Hero;
 import com.watabou.pixeldungeon.effects.Speck;
 import com.watabou.pixeldungeon.effects.SpellSprite;
@@ -15,18 +11,7 @@ import com.watabou.pixeldungeon.sprites.CharSprite;
 public class TomeOfKnowledge extends Book {
 
 	{
-		image = getImageIndex();
-	}
-
-	private int getImageIndex() {
-		String affinity = Dungeon.hero.heroClass.getMagicAffinity();
-		if (affinity.equals(SpellHelper.AFFINITY_ELEMENTAL)) {
-			return 1;
-		}
-		if (affinity.equals(SpellHelper.AFFINITY_NECROMANCY)) {
-			return 2;
-		}
-		return 0;
+		image = 1;
 	}
 
 	@Override
