644221b486b205007c1007dca8c0f9ddfa3f2015
Mikhael-Danilov <mikhael.danilov@gmail.com>
2018-01-23 21:51:51 +0300

    this event can be safely removed now

--- PixelDungeon/src/main/java/com/watabou/utils/Bundle.java
+++ PixelDungeon/src/main/java/com/watabou/utils/Bundle.java
@@ -40,7 +40,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Vector;
 
 public class Bundle {
 
@@ -207,8 +206,7 @@ public class Bundle {
 				}
 			}
 		} catch (JSONException e) {
-			EventCollector.logException(e);
-			return new Vector<>();
+			return new ArrayList<>();
 		}
 		
 		return list;
