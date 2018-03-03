aac78a369304968b41b7f100c9e499d36a4fd31c
Mikhael-Danilov <mikhael.danilov@gmail.com>
2018-03-04 00:33:12 +0300

    bind samplers

--- PixelDungeon/src/main/java/com/watabou/noosa/MaskedTilemapScript.java
+++ PixelDungeon/src/main/java/com/watabou/noosa/MaskedTilemapScript.java
@@ -25,145 +25,138 @@ import com.watabou.glwrap.Quad;
 import com.watabou.glwrap.Uniform;
 
 import java.nio.FloatBuffer;
-import java.nio.ShortBuffer;
 
 public class MaskedTilemapScript extends Script {
 
-	public Uniform uCamera;
-	public Uniform uModel;
-	public Uniform uTex;
-	public Uniform uTex_mask;
-	public Uniform uColorM;
-	public Uniform uColorA;
-	public Attribute aXY;
-	public Attribute aUV;
-	public Attribute aUV_mask;
+    public Uniform   uCamera;
+    public Uniform   uModel;
+    public Uniform   uTex;
+    public Uniform   uTex_mask;
+    public Uniform   uColorM;
+    public Uniform   uColorA;
+    public Attribute aXY;
+    public Attribute aUV;
+    public Attribute aUV_mask;
 
-	private Camera lastCamera;
+    private Camera lastCamera;
 
-	public MaskedTilemapScript() {
-		
-		super();
-		compile( shader() );
-		
-		uCamera	= uniform( "uCamera" );
-		uModel	= uniform( "uModel" );
-		uTex	= uniform( "uTex" );
-		uColorM	= uniform( "uColorM" );
-		uColorA	= uniform( "uColorA" );
-		aXY		= attribute( "aXYZW" );
-		aUV		= attribute( "aUV" );
-		
-	}
-	
-	@Override
-	public void use() {
-		
-		super.use();
-		
-		aXY.enable();
-		aUV.enable();
-		
-	}
+    public MaskedTilemapScript() {
 
-	public void drawElements( FloatBuffer vertices, ShortBuffer indices, int size ) {
-		
-		vertices.position( 0 );
-		aXY.vertexPointer( 2, 4, vertices );
-		
-		vertices.position( 2 );
-		aUV.vertexPointer( 2, 4, vertices );
-		
-		GLES20.glDrawElements( GLES20.GL_TRIANGLES, size, GLES20.GL_UNSIGNED_SHORT, indices );
-		
-	}
-	
-	public void drawQuad( FloatBuffer vertices ) {
-		
-		vertices.position( 0 );
-		aXY.vertexPointer( 2, 4, vertices );
-		
-		vertices.position( 2 );
-		aUV.vertexPointer( 2, 4, vertices );
+        super();
+        compile(shader());
 
-		GLES20.glDrawElements( GLES20.GL_TRIANGLES, Quad.SIZE, GLES20.GL_UNSIGNED_SHORT, Quad.getIndices( 1 ) );
-		
-	}
-	
-	public void drawQuadSet( FloatBuffer vertices, int size ) {
-		
-		if (size == 0) {
-			return;
-		}
-		
-		vertices.position( 0 );
-		aXY.vertexPointer( 2, 4, vertices );
-		
-		vertices.position( 2 );
-		aUV.vertexPointer( 2, 4, vertices );
+        uCamera = uniform("uCamera");
+        uModel = uniform("uModel");
+        uTex = uniform("uTex");
+        uTex_mask = uniform("uTex_mask");
+        uColorM = uniform("uColorM");
+        uColorA = uniform("uColorA");
+        aXY = attribute("aXYZW");
+        aUV = attribute("aUV");
+        aUV_mask = attribute("aUV_mask");
 
-		GLES20.glDrawElements( 
-			GLES20.GL_TRIANGLES, 
-			Quad.SIZE * size, 
-			GLES20.GL_UNSIGNED_SHORT, 
-			Quad.getIndices( size ) );
-		
-	}
-	
-	public void lighting( float rm, float gm, float bm, float am, float ra, float ga, float ba, float aa ) {
-		uColorM.value4f( rm, gm, bm, am );
-		uColorA.value4f( ra, ga, ba, aa );
-	}
-	
-	public void resetCamera() {
-		lastCamera = null;
-	}
-	
-	public void camera( Camera camera ) {
-		if (camera == null) {
-			camera = Camera.main;
-		}
-		if (camera != lastCamera) {
-			lastCamera = camera;
-			uCamera.valueM4( camera.matrix );
-			
-			GLES20.glScissor( 
-				camera.x, 
-				Game.height() - camera.screenHeight - camera.y, 
-				camera.screenWidth, 
-				camera.screenHeight );
-		}
-	}
-	
-	public static MaskedTilemapScript get() {
-		return Script.use( MaskedTilemapScript.class );
-	}
-	
-	
-	protected String shader() {
-		return SHADER;
-	}
-	
-	private static final String SHADER =
-		
-		"uniform mat4 uCamera;" +
-		"uniform mat4 uModel;" +
-		"attribute vec4 aXYZW;" +
-		"attribute vec2 aUV;" +
-		"varying vec2 vUV;" +
-		"void main() {" +
-		"  gl_Position = uCamera * uModel * aXYZW;" +
-		"  vUV = aUV;" +
-		"}" +
-		
-		"//\n" +
-		
-		"precision mediump float;" +
-		"varying vec2 vUV;" +
-		"uniform sampler2D uTex;" +
-		"uniform vec4 uColorM;" +
-		"uniform vec4 uColorA;" +
-		"void main() {" +
-		"  gl_FragColor = texture2D( uTex, vUV ) * uColorM + uColorA;" +
-		"}";
+    }
+
+    @Override
+    public void use() {
+
+        super.use();
+
+        aXY.enable();
+        aUV.enable();
+        aUV_mask.enable();
+
+        int texSampler = GLES20.glGetUniformLocation(this.handle(),"uTex");
+        int maskSampler = GLES20.glGetUniformLocation(this.handle(),"uTex_mask");
+
+        GLES20.glUniform1i(texSampler,0);
+        GLES20.glUniform1i(maskSampler,1);
+    }
+
+
+    public void drawQuadSet(FloatBuffer vertices, int size) {
+
+        if (size == 0) {
+            return;
+        }
+
+        vertices.position(0);
+        aXY.vertexPointer(2, 4, vertices);
+
+        vertices.position(2);
+        aUV.vertexPointer(2, 4, vertices);
+
+        vertices.position(0);
+        aUV_mask.vertexPointer(2, 4, vertices);
+
+        GLES20.glDrawElements(
+                GLES20.GL_TRIANGLES,
+                Quad.SIZE * size,
+                GLES20.GL_UNSIGNED_SHORT,
+                Quad.getIndices(size));
+
+    }
+
+    public void lighting(float rm, float gm, float bm, float am, float ra, float ga, float ba, float aa) {
+        uColorM.value4f(rm, gm, bm, am);
+        uColorA.value4f(ra, ga, ba, aa);
+    }
+
+    public void resetCamera() {
+        lastCamera = null;
+    }
+
+    public void camera(Camera camera) {
+        if (camera == null) {
+            camera = Camera.main;
+        }
+        if (camera != lastCamera) {
+            lastCamera = camera;
+            uCamera.valueM4(camera.matrix);
+
+            GLES20.glScissor(
+                    camera.x,
+                    Game.height() - camera.screenHeight - camera.y,
+                    camera.screenWidth,
+                    camera.screenHeight);
+        }
+    }
+
+    public static MaskedTilemapScript get() {
+        return Script.use(MaskedTilemapScript.class);
+    }
+
+
+    protected String shader() {
+        return SHADER;
+    }
+
+    private static final String SHADER =
+
+            "uniform mat4 uCamera;" +
+                    "uniform mat4 uModel;" +
+                    "attribute vec4 aXYZW;" +
+                    "attribute vec2 aUV;" +
+                    "attribute vec2 aUV_mask;" +
+                    "varying vec2 vUV;" +
+                    "varying vec2 vUV_mask;" +
+                    "void main() {" +
+                    "  gl_Position = uCamera * uModel * aXYZW;" +
+                    "  vUV = aUV;" +
+                    "  vUV_mask = aUV_mask;" +
+                    "}" +
+
+                    "//\n" +
+
+                    "precision mediump float;" +
+                    "varying vec2 vUV;" +
+                    "varying vec2 vUV_mask;" +
+                    "uniform sampler2D uTex;" +
+                    "uniform sampler2D uTex_mask;" +
+                    "uniform vec4 uColorM;" +
+                    "uniform vec4 uColorA;" +
+                    "void main() {" +
+//                    " gl_FragColor = vec4(0.7,0.7,0.7,0.7);" +
+                    "  gl_FragColor = texture2D( uTex_mask, vUV )*texture2D( uTex, vUV );" +
+                    "}";
 }
