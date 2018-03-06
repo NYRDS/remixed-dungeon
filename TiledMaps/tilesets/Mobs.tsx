746458e7caf7ee180ad38c74cb6ac0674d26abaa
Mikhael-Danilov <mikhael.danilov@gmail.com>
2018-03-07 00:00:02 +0300

    extract mobs tileset

---
+++ TiledMaps/tilesets/Mobs.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Mobs" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="../Png/Mobs.png" width="256" height="256"/>
+ <tile id="0">
+  <properties>
+   <property name="kind" value="Rat"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="kind" value="Albino"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="kind" value="Gnoll"/>
+  </properties>
+ </tile>
+</tileset>
