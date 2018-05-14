7b7b6e3d9fb08d41c9ccbb8eab230b1406c73897
Mikhael-Danilov <mikhael.danilov@gmail.com>
2018-05-15 01:05:44 +0300

    app for GooglePlay flavor

--- PixelDungeon/src/googlePlay/java/com/nyrds/pixeldungeon/ml/RemixedPixelDungeonApp.java
+++ PixelDungeon/src/googlePlay/java/com/nyrds/pixeldungeon/ml/RemixedPixelDungeonApp.java
@@ -1,5 +1,7 @@
 package com.nyrds.pixeldungeon.ml;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.support.multidex.MultiDexApplication;
 
 import org.acra.ACRA;
@@ -9,16 +11,27 @@ import org.acra.annotation.ReportsCrashes;
 @ReportsCrashes(mailTo = "nyrdsofficial@gmail.com", mode = ReportingInteractionMode.TOAST, resToastText = R.string.RemixedPixelDungeonApp_sendCrash)
 public class RemixedPixelDungeonApp extends MultiDexApplication {
 
+	@SuppressLint("StaticFieldLeak")
+	static Context instanceContext;
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
+
+		instanceContext = getApplicationContext();
+
 		if(!BuildConfig.DEBUG) {
 			ACRA.init(this);
 		}
+
 
 		try {
 			Class.forName("android.os.AsyncTask");
 		} catch (Throwable ignore) {
 		}
+	}
+
+	static public Context getContext() {
+		return instanceContext;
 	}
 }
