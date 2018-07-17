9f104146955ae3225b54886a51764191225ee082
Mikhael Danilov <mikhaeldanilov@gmail.com>
2018-07-17 22:35:33 +0300

    start EuConsent early

--- PixelDungeon/src/main/java/com/nyrds/pixeldungeon/support/EuConsent.java
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/support/EuConsent.java
@@ -1,7 +1,12 @@
 package com.nyrds.pixeldungeon.support;
 
+import android.content.Context;
+
+import com.google.ads.consent.ConsentInfoUpdateListener;
 import com.google.ads.consent.ConsentInformation;
 import com.google.ads.consent.ConsentStatus;
+import com.nyrds.pixeldungeon.ml.EventCollector;
+import com.nyrds.pixeldungeon.ml.R;
 import com.watabou.noosa.Game;
 import com.watabou.pixeldungeon.Preferences;
 
@@ -14,6 +19,29 @@ public class EuConsent {
     public static final int UNKNOWN          = -1;
     public static final int NON_PERSONALIZED = 0;
     public static final int PERSONALIZED     = 1;
+
+    static public void check(final Context context) {
+        if (getConsentLevel() < NON_PERSONALIZED) {
+
+            final ConsentInformation consentInformation = ConsentInformation.getInstance(context);
+
+
+            String[] publisherIds = {Game.getVar(R.string.admob_publisher_id)};
+            consentInformation.requestConsentInfoUpdate(publisherIds, new ConsentInfoUpdateListener() {
+                @Override
+                public void onConsentInfoUpdated(ConsentStatus consentStatus) {
+                    if (!ConsentInformation.getInstance(context).isRequestLocationInEeaOrUnknown()) {
+                        setConsentLevel(PERSONALIZED);
+                    }
+                }
+
+                @Override
+                public void onFailedToUpdateConsentInfo(String errorDescription) {
+                    EventCollector.logEvent("eu_consent", errorDescription);
+                }
+            });
+        }
+    }
 
     static public void setConsentLevel(int level) {
 
