9ae3632a0908e65e0723e7bb840d79953165c90d
Mikhael Danilov <mikhaeldanilov@gmail.com>
2018-08-20 01:12:10 +0300

    Horrified status

---
+++ PixelDungeon/src/main/java/com/nyrds/pixeldungeon/ai/Horrified.java
@@ -0,0 +1,45 @@
+package com.nyrds.pixeldungeon.ai;
+
+import com.nyrds.pixeldungeon.ml.R;
+import com.watabou.noosa.Game;
+import com.watabou.pixeldungeon.actors.Actor;
+import com.watabou.pixeldungeon.actors.buffs.Terror;
+import com.watabou.pixeldungeon.actors.mobs.Mob;
+import com.watabou.pixeldungeon.utils.Utils;
+
+public class Horrified extends MobAi implements AiState{
+
+    @Override
+    public boolean act(Mob me) {
+        if(me.hasBuff(Terror.class)) {
+            me.setState(MobAi.getStateByClass(Wandering.class));
+            return true;
+        }
+
+        me.enemySeen = me.isEnemyInFov();
+        if (me.enemySeen) {
+            me.target = me.getEnemy().getPos();
+        }
+
+        if(!me.doStepFrom(me.target)) {
+            me.spend(Actor.TICK);
+
+        }
+        return true;
+    }
+
+    @Override
+    public void gotDamage(Mob me, Object src, int dmg) {
+        seekRevenge(me, src);
+    }
+
+    @Override
+    public String status(Mob me) {
+        if (me.getEnemy().valid()) {
+            return Utils.format(Game.getVar(R.string.Mob_StaTerrorStatus2),
+                    me.getName(), me.getEnemy().getName_objective());
+        }
+        return Utils.format(Game.getVar(R.string.Mob_StaTerrorStatus),
+                me.getName());
+    }
+}
