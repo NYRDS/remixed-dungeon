83dc51b04fc3cc28f79e0db4310d9cca3dca549e
Mikhael Danilov <mikhaeldanilov@gmail.com>
2018-08-28 21:56:27 +0300

    cleanup

--- PixelDungeon/src/main/java/com/watabou/pixeldungeon/items/weapon/melee/Kusarigama.java
+++ PixelDungeon/src/main/java/com/watabou/pixeldungeon/items/weapon/melee/Kusarigama.java
@@ -18,94 +18,93 @@ import com.watabou.utils.Random;
 import java.util.ArrayList;
 
 public class Kusarigama extends SpecialWeapon {
-        {
-		animation_class = SWORD_ATTACK;
-	}
+    {
+        animation_class = SWORD_ATTACK;
+    }
 
+    private static final String AC_PULL = "Kusarigama_Pull";
+    private static final float TIME_TO_IMPALE = 1.5f;
 
-	private static final String AC_PULL = "Kusarigama_Pull";
-	private static final float TIME_TO_IMPALE = 1.5f;
+    public Kusarigama() {
+        super(3, 2f, 1f);
 
-	public Kusarigama() {
-		super(3, 2f, 1f);
+        image = 0;
+        imageFile = "items/kusarigama.png";
 
-		image = 0;
-		imageFile = "items/kusarigama.png";
+        range = 2;
+    }
 
-		range = 2;
-	}
+    private static CellSelector.Listener impaler = new CellSelector.Listener() {
+        @Override
+        public void onSelect(Integer target) {
 
-	protected static CellSelector.Listener impaler = new CellSelector.Listener() {
-		@Override
-		public void onSelect(Integer target) {
+            if (target != null) {
+                getCurUser().spendAndNext(TIME_TO_IMPALE);
+                int hitCell = Ballistica.cast(getCurUser().getPos(), target, false, true);
 
-			if (target != null) {
-				getCurUser().spendAndNext(TIME_TO_IMPALE);
-				int hitCell = Ballistica.cast(getCurUser().getPos(), target, false, true);
-				
-				if(hitCell == getCurUser().getPos()) {
-					return;
-				}
-				
-				if (Dungeon.level.distance(getCurUser().getPos(), hitCell) < 4) {
-					Char chr = Actor.findChar(hitCell);
-					
-					if (chr != null && chr.isMovable()) {
-						chr.move(Ballistica.trace[1]);
-						chr.getSprite().move(chr.getPos(), Ballistica.trace[1]);
+                if (hitCell == getCurUser().getPos()) {
+                    return;
+                }
 
-						Dungeon.observe();
-					}
+                if (Dungeon.level.distance(getCurUser().getPos(), hitCell) < 4) {
+                    Char chr = Actor.findChar(hitCell);
 
-					drawChain(hitCell);
-				} else {
-					drawChain(Ballistica.trace[4]);
-				}
-			}
-		}
+                    if (chr != null && chr.isMovable()) {
+                        chr.move(Ballistica.trace[1]);
+                        chr.getSprite().move(chr.getPos(), Ballistica.trace[1]);
 
-		@Override
-		public String prompt() {
-			return Game.getVar(R.string.Item_DirThrow);
-		}
-	};
+                        Dungeon.observe();
+                    }
 
-	private static void drawChain(int tgt) {
-		getCurUser().getSprite().zap(tgt);
-		getCurUser().getSprite()
-				.getParent()
-				.add(new KusarigamaChain(getCurUser().getSprite().center(),
-						DungeonTilemap.tileCenterToWorld(tgt)));
-	}
+                    drawChain(hitCell);
+                } else {
+                    drawChain(Ballistica.trace[4]);
+                }
+            }
+        }
 
-	@Override
-	public void execute(Hero hero, String action) {
-		setCurUser(hero);
-		if (action.equals(AC_PULL)) {
-			GameScene.selectCell(impaler);
-		} else {
-			super.execute(hero, action);
-		}
-	}
+        @Override
+        public String prompt() {
+            return Game.getVar(R.string.Item_DirThrow);
+        }
+    };
 
-	@Override
-	public ArrayList<String> actions(Hero hero) {
-		ArrayList<String> actions = super.actions(hero);
-		if (isEquipped(hero)) {
-			actions.add(AC_PULL);
-		}
-		return actions;
-	}
+    private static void drawChain(int tgt) {
+        getCurUser().getSprite().zap(tgt);
+        getCurUser().getSprite()
+                .getParent()
+                .add(new KusarigamaChain(getCurUser().getSprite().center(),
+                        DungeonTilemap.tileCenterToWorld(tgt)));
+    }
 
-	public void postAttack(Hero hero, Char tgt) {
-		setCurUser(hero);
+    @Override
+    public void execute(Hero hero, String action) {
+        setCurUser(hero);
+        if (action.equals(AC_PULL)) {
+            GameScene.selectCell(impaler);
+        } else {
+            super.execute(hero, action);
+        }
+    }
 
-		if (Dungeon.level.distance(getCurUser().getPos(), tgt.getPos()) > 1) {
-			drawChain(tgt.getPos());
-		}
+    @Override
+    public ArrayList<String> actions(Hero hero) {
+        ArrayList<String> actions = super.actions(hero);
+        if (isEquipped(hero)) {
+            actions.add(AC_PULL);
+        }
+        return actions;
+    }
 
-		if (Random.Float(1) < 0.1f) {
-			Buff.prolong(tgt, Vertigo.class, 3);
-		}
-	}
+    public void postAttack(Hero hero, Char tgt) {
+        setCurUser(hero);
+
+        if (Dungeon.level.distance(getCurUser().getPos(), tgt.getPos()) > 1) {
+            drawChain(tgt.getPos());
+        }
+
+        if (Random.Float(1) < 0.1f) {
+            Buff.prolong(tgt, Vertigo.class, 3);
+        }
+    }
 }
