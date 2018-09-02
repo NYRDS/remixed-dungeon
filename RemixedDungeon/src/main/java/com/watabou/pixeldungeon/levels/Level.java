0a58b872d169b0c4e2adf84d10791510b8715703
Mikhael Danilov <mikhaeldanilov@gmail.com>
2018-09-02 19:15:11 +0300

    ContolledAi Attack - WiP

--- RemixedDungeon/src/main/java/com/watabou/pixeldungeon/levels/Level.java
+++ RemixedDungeon/src/main/java/com/watabou/pixeldungeon/levels/Level.java
@@ -1266,7 +1266,7 @@ public abstract class Level implements Bundlable {
 
 	public void updateFieldOfView(Char c) {
 
-		GLog.i("fov: %s",c.toString());
+		//GLog.i("fov: %s",c.toString());
 
 		if (noFogOfWar()) {
 			Arrays.fill(fieldOfView, true);
