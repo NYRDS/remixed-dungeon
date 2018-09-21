2e882a757e93aea1d4afd7c33ebf09ba0e0bc21c
Mikhael Danilov <mikhaeldanilov@gmail.com>
2018-09-21 16:38:43 +0300

    No notifications for retro...

---
+++ PixelDungeon/src/googlePlayRetro/java/com/nyrds/android/util/TrackedRuntimeException.java
@@ -0,0 +1,27 @@
+package com.nyrds.android.util;
+
+import com.nyrds.pixeldungeon.ml.EventCollector;
+import com.watabou.pixeldungeon.utils.GLog;
+
+/**
+ * Created by DeadDie on 18.03.2016
+ */
+public class TrackedRuntimeException extends RuntimeException {
+
+	public TrackedRuntimeException( Exception e) {
+		super(e);
+
+		GLog.toFile(e.getMessage());
+		EventCollector.logException(e,"");
+	}
+
+	public TrackedRuntimeException( String s) {
+		super(s);
+		EventCollector.logException(this,s);
+	}
+
+    public TrackedRuntimeException( String s,Exception e) {
+        super(s,e);
+        EventCollector.logException(this,s);
+    }
+}
