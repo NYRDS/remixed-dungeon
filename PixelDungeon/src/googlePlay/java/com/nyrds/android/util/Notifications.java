8fb19c0378cf219bd7836eba8abecf632a26a1ff
Mikhael Danilov <mikhaeldanilov@gmail.com>
2018-09-30 01:41:38 +0300

    retro notifications

--- PixelDungeon/src/googlePlay/java/com/nyrds/android/util/Notifications.java
+++ PixelDungeon/src/googlePlay/java/com/nyrds/android/util/Notifications.java
@@ -25,7 +25,7 @@ public class Notifications {
         notificationManager.notify(1, builder.build());
     }
 
-    static void createNotificationChannel(String channelId) {
+    static private void createNotificationChannel(String channelId) {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
