4c1fca501fcca62a5c69f4b694f8be804ad7be55
Mikhael Danilov <mikhaeldanilov@gmail.com>
2018-10-02 22:42:16 +0300

    it was a bad idea

--- PixelDungeon/src/main/java/com/watabou/pixeldungeon/ui/GoldIndicator.java
+++ PixelDungeon/src/main/java/com/watabou/pixeldungeon/ui/GoldIndicator.java
@@ -17,8 +17,6 @@
  */
 package com.watabou.pixeldungeon.ui;
 
-import android.support.annotation.NonNull;
-
 import com.watabou.noosa.BitmapText;
 import com.watabou.noosa.Game;
 import com.watabou.noosa.ui.Component;
@@ -33,13 +31,13 @@ public class GoldIndicator extends Component {
 	
 	private int lastValue = 0;
 
-	@NonNull
-	private BitmapText tf = new BitmapText( PixelScene.font1x );
+	private BitmapText tf;
 	
 	private float time;
 
 	@Override
 	protected void createChildren() {
+		tf = new BitmapText( PixelScene.font1x );
 		tf.hardlight( 0xFFFF00 );
 		add( tf );
 		
