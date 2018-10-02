94ea8e4ad4763915ad2340031d136f54afc2a439
Mikhael Danilov <mikhaeldanilov@gmail.com>
2018-10-02 22:43:31 +0300

    fix MazeShadow?

--- PixelDungeon/src/main/assets/scripts/mobs/MazeShadow.lua
+++ PixelDungeon/src/main/assets/scripts/mobs/MazeShadow.lua
@@ -12,7 +12,11 @@ local mob = require"scripts/lib/mob"
 return mob.init({
     interact = function(self, chr)
         local ownPos  = self:getPos()
-        local newPos = chr:getPos()
+        local newPos  = chr:getPos()
+
+        if ownPos==newPos then --hell I know how this can happen but it is
+            return
+        end
 
         self:move(newPos)
         self:getSprite():move(ownPos, newPos)
