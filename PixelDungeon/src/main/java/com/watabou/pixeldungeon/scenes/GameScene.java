4f760ed2a70ab1b3f478d3a4baefdb9bca24ac9a
Mikhael Danilov <mikhaeldanilov@gmail.com>
2018-10-14 00:39:39 +0300

    Reset interlevel state before another one can occur.

--- PixelDungeon/src/main/java/com/watabou/pixeldungeon/scenes/GameScene.java
+++ PixelDungeon/src/main/java/com/watabou/pixeldungeon/scenes/GameScene.java
@@ -354,7 +354,11 @@ public class GameScene extends PixelScene {
 
         sceneCreated = true;
 
-        switch (InterlevelScene.mode) {
+        InterlevelScene.Mode appearMode = InterlevelScene.mode;
+        InterlevelScene.mode = InterlevelScene.Mode.CONTINUE;
+
+        //it is a chance for another level change right here if level entrance is at CHASM for example
+        switch (appearMode) {
             case RESURRECT:
                 Dungeon.hero.regenSprite();
                 WandOfBlink.appear(Dungeon.hero, level.entrance);
@@ -376,7 +380,6 @@ public class GameScene extends PixelScene {
                 break;
             default:
         }
-        InterlevelScene.mode = InterlevelScene.Mode.CONTINUE;
 
         Camera.main.target = Dungeon.hero.getHeroSprite();
 
