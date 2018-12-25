00d4540f206386a55726b02832d0acbdc26ba34c
Mikhael Danilov <mikhaeldanilov@gmail.com>
2018-12-25 23:36:17 +0300

    Settable objects in tiled

---
+++ TiledMaps/tilesets/Objects.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Objects" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="../Png/Objects.png" width="256" height="256"/>
+ <tile id="32">
+  <properties>
+   <property name="kind" value="ConcreteBlock"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="kind" value="Sign"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="kind" value="Barrel"/>
+  </properties>
+ </tile>
+</tileset>
