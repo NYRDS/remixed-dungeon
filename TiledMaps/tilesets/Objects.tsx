cfcc81a64058bb76227b02581829f9e7e90c4f85
Mikhael Danilov <mikhaeldanilov@gmail.com>
2018-12-30 00:33:37 +0300

    Add CutScene demo script on ScriptsArena map

--- TiledMaps/tilesets/Objects.tsx
+++ TiledMaps/tilesets/Objects.tsx
@@ -1,5 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Objects" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <properties>
+  <property name="kind" value="Trap"/>
+ </properties>
  <image source="../Png/Objects.png" width="256" height="256"/>
  <tile id="32">
   <properties>
@@ -20,6 +23,14 @@
  <tile id="36">
   <properties>
    <property name="kind" value="Barrel"/>
+  </properties>
+ </tile>
+ <tile id="176">
+  <properties>
+   <property name="kind" value="Trap"/>
+   <property name="script" value="scripts/traps/CutScene"/>
+   <property name="trapKind" value="scriptFile"/>
+   <property name="uses" type="int" value="-1"/>
   </properties>
  </tile>
 </tileset>
