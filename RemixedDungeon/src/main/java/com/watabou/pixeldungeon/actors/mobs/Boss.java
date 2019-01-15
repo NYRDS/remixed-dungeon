911fab665245c1f1adafc26f39e089749db6d4ff
Mikhael Danilov <mikhaeldanilov@gmail.com>
2019-01-15 22:46:54 +0300

    Fix level access before creation

--- RemixedDungeon/src/main/java/com/watabou/pixeldungeon/actors/mobs/Boss.java
+++ RemixedDungeon/src/main/java/com/watabou/pixeldungeon/actors/mobs/Boss.java
@@ -3,14 +3,17 @@ package com.watabou.pixeldungeon.actors.mobs;
 import com.nyrds.pixeldungeon.ai.AiState;
 import com.nyrds.pixeldungeon.ai.Hunting;
 import com.watabou.noosa.audio.Music;
-import com.watabou.pixeldungeon.Dungeon;
 import com.watabou.pixeldungeon.items.scrolls.ScrollOfPsionicBlast;
 import com.watabou.pixeldungeon.items.weapon.enchantments.Death;
 import com.watabou.pixeldungeon.scenes.GameScene;
 
+import androidx.annotation.Nullable;
+
 abstract public class Boss extends Mob {
 
 	private static final String BATTLE_MUSIC = "battleMusic";
+
+	@Nullable
 	private String battleMusic;
 
 	public Boss() {
@@ -43,6 +46,6 @@ abstract public class Boss extends Mob {
 	@Override
 	protected void readCharData() {
 		super.readCharData();
-		battleMusic = getClassDef().optString(BATTLE_MUSIC, Dungeon.level.music());
+		battleMusic = getClassDef().optString(BATTLE_MUSIC, null);
 	}
 }
