ccd40ece0be7393abdbb52fd9f95101389f553c5
Mikhael Danilov <mikhaeldanilov@gmail.com>
2019-03-12 22:41:10 +0300

    StartScene cleanup

--- RemixedDungeon/src/googlePlay/java/com/nyrds/pixeldungeon/ml/RemixedDungeonApp.java
+++ RemixedDungeon/src/googlePlay/java/com/nyrds/pixeldungeon/ml/RemixedDungeonApp.java
@@ -5,15 +5,24 @@ import android.content.Context;
 
 import com.crashlytics.android.Crashlytics;
 import com.google.firebase.FirebaseApp;
+import com.watabou.noosa.Game;
+
+import org.jetbrains.annotations.NotNull;
 
 import androidx.multidex.MultiDex;
 import androidx.multidex.MultiDexApplication;
 import io.fabric.sdk.android.Fabric;
+import io.humanteq.hqsdkapps.InstalledApplicationsCollector;
+import io.humanteq.hqsdkcore.HQSdk;
+import io.humanteq.hqsdkcore.api.impl.HqmApi;
+import io.humanteq.hqsdkcore.api.interfaces.HqmCallback;
 
 public class RemixedDungeonApp extends MultiDexApplication {
 
 	@SuppressLint("StaticFieldLeak")
 	static Context instanceContext;
+
+	private static boolean hqmStarted = false;
 
 	@Override
 	public void onCreate() {
@@ -34,6 +43,30 @@ public class RemixedDungeonApp extends MultiDexApplication {
 	protected void attachBaseContext(Context base) {
 		super.attachBaseContext(base);
 		MultiDex.install(this);
+	}
+
+	static public void startScene() {
+		if(hqmStarted) {
+			return;
+		}
+
+		HQSdk.init(Game.instance(), "22b4f34f2616d7f", true, false,
+				new HqmCallback<HqmApi>() {
+
+					@Override
+					public void onSuccess(HqmApi hqmApi) {
+						hqmStarted = true;
+						hqmApi.start(new InstalledApplicationsCollector());
+						hqmApi.startSystemEventsTracking();
+					}
+
+					@Override
+					public void onError(@NotNull Throwable throwable) {
+						EventCollector.logException(throwable, "hq sdk init error");
+					}
+				}
+
+		);
 	}
 
 	static public Context getContext() {
