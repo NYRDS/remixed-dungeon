63f07122f67ff43d637d6b2fd71d208db3822f66
Mikhael Danilov <mikhaeldanilov@gmail.com>
2019-05-30 20:06:27 +0300

    Do not crash even if level file is missing

--- RemixedDungeon/src/main/java/com/watabou/pixeldungeon/scenes/InterlevelScene.java
+++ RemixedDungeon/src/main/java/com/watabou/pixeldungeon/scenes/InterlevelScene.java
@@ -233,18 +233,19 @@ public class InterlevelScene extends PixelScene {
         }
 
         Position next;
+        Level newLevel;
+
         try {
             next = DungeonGenerator.descend(Dungeon.currentPosition());
+            Dungeon.depth = DungeonGenerator.getLevelDepth(next.levelId);
+            newLevel = Dungeon.loadLevel(next);
         } catch (Exception e) {
             restore();
             return;
         }
 
-        Dungeon.depth = DungeonGenerator.getLevelDepth(next.levelId);
-        Level level = Dungeon.loadLevel(next);
-
-        Dungeon.switchLevel(level,
-                level.entrance,
+        Dungeon.switchLevel(newLevel,
+                newLevel.entrance,
                 followers);
     }
 
