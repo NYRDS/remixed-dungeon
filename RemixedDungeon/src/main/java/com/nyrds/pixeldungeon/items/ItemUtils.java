f16776982bd379e0b82342ca8a44fe1802882ef8
Mikhael Danilov <mikhaeldanilov@gmail.com>
2019-07-15 20:57:28 +0300

    Fix crash, also fix compass in town

--- RemixedDungeon/src/main/java/com/nyrds/pixeldungeon/items/ItemUtils.java
+++ RemixedDungeon/src/main/java/com/nyrds/pixeldungeon/items/ItemUtils.java
@@ -9,10 +9,12 @@ import com.watabou.pixeldungeon.items.Item;
 public class ItemUtils {
     public static void throwItemAway(int pos) {
 		Heap heap = Dungeon.level.getHeap( pos );
-		Item item = heap.pickUp();
-		int cell = Dungeon.level.getEmptyCellNextTo(pos);
-		if(Dungeon.level.cellValid(cell)) {
-			Dungeon.level.drop( item, cell ).sprite.drop( cell );
+		if(heap!=null) {
+			Item item = heap.pickUp();
+			int cell = Dungeon.level.getEmptyCellNextTo(pos);
+			if (Dungeon.level.cellValid(cell)) {
+				Dungeon.level.drop(item, cell).sprite.drop(cell);
+			}
 		}
 	}
 
