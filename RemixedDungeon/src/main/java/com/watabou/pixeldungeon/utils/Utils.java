84f443cab7a366a1876b6bf997b32f32cb5dddf7
Mikhael Danilov <mikhaeldanilov@gmail.com>
2019-08-03 01:44:09 +0300

    Check for empty string - may happens in mods

--- RemixedDungeon/src/main/java/com/watabou/pixeldungeon/utils/Utils.java
+++ RemixedDungeon/src/main/java/com/watabou/pixeldungeon/utils/Utils.java
@@ -46,6 +46,9 @@ public class Utils {
     }
 
     public static String capitalize(String str) {
+        if(str.isEmpty()) {
+            return str;
+        }
         return Character.toUpperCase(str.charAt(0)) + str.substring(1);
     }
 
