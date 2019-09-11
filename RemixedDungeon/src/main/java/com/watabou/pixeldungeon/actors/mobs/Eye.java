e16eb6d10004e6d4a7747f0715d29b588765f37f
Mikhael  Danilov <mikhael.danilov@gmail.com>
2019-09-11 21:54:58 +0300

    Fix Eye attack

--- RemixedDungeon/src/main/java/com/watabou/pixeldungeon/actors/mobs/Eye.java
+++ RemixedDungeon/src/main/java/com/watabou/pixeldungeon/actors/mobs/Eye.java
@@ -40,117 +40,124 @@ import com.watabou.utils.Random;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Eye extends Mob {
 
-	public Eye() {
-		spriteClass = EyeSprite.class;
+    private Set<Char> victims = new HashSet<>();
 
-		hp(ht(100));
-		defenseSkill = 20;
+    public Eye() {
+        spriteClass = EyeSprite.class;
+
+        hp(ht(100));
+        defenseSkill = 20;
 
 
-		exp = 13;
-		maxLvl = 25;
+        exp = 13;
+        maxLvl = 25;
 
-		flying = true;
+        flying = true;
 
-		loot = Dewdrop.class;
-		lootChance = 0.5f;
+        loot = Dewdrop.class;
+        lootChance = 0.5f;
 
-		addResistance( WandOfDisintegration.class );
-		addResistance( Death.class );
-		addResistance( Leech.class );
+        addResistance(WandOfDisintegration.class);
+        addResistance(Death.class);
+        addResistance(Leech.class);
 
-		addImmunity( Terror.class );
-	}
+        addImmunity(Terror.class);
+    }
 
-	@Override
-	public void onSpawn(Level level) {
-		super.onSpawn(level);
-		viewDistance = level.getViewDistance() + 1;
-	}
+    @Override
+    public void onSpawn(Level level) {
+        super.onSpawn(level);
+        viewDistance = level.getViewDistance() + 1;
+    }
 
-	@Override
-	public int dr() {
-		return 10;
-	}
-	
-	private int hitCell;
-	
-	@Override
+    @Override
+    public int dr() {
+        return 10;
+    }
+
+    private int hitCell;
+
+    @Override
     public boolean canAttack(Char enemy) {
-		
-		hitCell = Ballistica.cast( getPos(), enemy.getPos(), true, false );
-		
-		for (int i=1; i < Ballistica.distance; i++) {
-			if (Ballistica.trace[i] == enemy.getPos()) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	@Override
-	public int attackSkill( Char target ) {
-		return 30;
-	}
-	
-	@Override
-	protected float attackDelay() {
-		return 1.6f;
-	}
-	
-	@Override
-	public boolean doAttack(Char enemy) {
 
-		spend( attackDelay() );
-		
-		boolean rayVisible = false;
-		
-		for (int i=0; i < Ballistica.distance; i++) {
-			if (Dungeon.visible[Ballistica.trace[i]]) {
-				rayVisible = true;
-			}
-		}
-		
-		if (rayVisible) {
-			getSprite().attack( hitCell );
-			return false;
-		} else {
-			attack( enemy );
-			return true;
-		}
-	}
-	
-	@Override
-	public boolean attack(@NotNull Char enemy ) {
-		
-		for (int i=1; i < Ballistica.distance; i++) {
-			
-			int pos = Ballistica.trace[i];
-			
-			Char ch = Actor.findChar( pos );
-			if (ch == null) {
-				continue;
-			}
-			
-			if (hit( this, ch, true )) {
-				ch.damage( Random.NormalIntRange( 14, 20 ), this );
-				
-				if (Dungeon.visible[pos]) {
-					ch.getSprite().flash();
-					CellEmitter.center( pos ).burst( PurpleParticle.BURST, Random.IntRange( 1, 2 ) );
-				}
-				
-				if (!ch.isAlive() && ch == Dungeon.hero) {
-					Dungeon.fail( Utils.format( ResultDescriptions.getDescription(ResultDescriptions.Reason.MOB), Utils.indefinite( getName() ), Dungeon.depth ) );
-					GLog.n( Game.getVar(R.string.Eye_Kill), getName() );
-				}
-			} else {
-				ch.getSprite().showStatus( CharSprite.NEUTRAL,  ch.defenseVerb() );
-			}
-		}
-		
-		return true;
-	}
+        hitCell = Ballistica.cast(getPos(), enemy.getPos(), true, false);
+
+        for (int i = 1; i < Ballistica.distance; i++) {
+            if (Ballistica.trace[i] == enemy.getPos()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int attackSkill(Char target) {
+        return 30;
+    }
+
+    @Override
+    protected float attackDelay() {
+        return 1.6f;
+    }
+
+    @Override
+    public boolean doAttack(Char enemy) {
+
+        spend(attackDelay());
+
+        boolean rayVisible = false;
+
+        for (int i = 0; i < Ballistica.distance; i++) {
+            int cell = Ballistica.trace[i];
+
+            Char victim = Actor.findChar(cell);
+
+            if (victim != null) {
+                victims.add(victim);
+            }
+
+            if (Dungeon.visible[cell]) {
+                rayVisible = true;
+            }
+        }
+
+        if (rayVisible) {
+            getSprite().attack(hitCell);
+            return false;
+        } else {
+            attack(enemy);
+            return true;
+        }
+    }
+
+    @Override
+    public boolean attack(@NotNull Char enemy) {
+
+        for (Char ch : victims) {
+            if (hit(this, ch, true)) {
+                ch.damage(Random.NormalIntRange(14, 20), this);
+                int pos = ch.getPos();
+
+                if (Dungeon.visible[pos]) {
+                    ch.getSprite().flash();
+                    CellEmitter.center(pos).burst(PurpleParticle.BURST, Random.IntRange(1, 2));
+                }
+
+                if (!ch.isAlive() && ch == Dungeon.hero) {
+                    Dungeon.fail(Utils.format(ResultDescriptions.getDescription(ResultDescriptions.Reason.MOB), Utils.indefinite(getName()), Dungeon.depth));
+                    GLog.n(Game.getVar(R.string.Eye_Kill), getName());
+                }
+            } else {
+                ch.getSprite().showStatus(CharSprite.NEUTRAL, ch.defenseVerb());
+            }
+        }
+
+        victims.clear();
+        return true;
+    }
 }
