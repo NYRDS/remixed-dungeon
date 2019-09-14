9206c4df5b1d9921e48ef5980d2b4e907fae3ac6
Mikhael  Danilov <mikhael.danilov@gmail.com>
2019-09-15 00:06:16 +0300

    crush it  pixel perfect

---
+++ tools/crush/crush.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# loop through all files ending in .png
+for file in `find ../../ -name '*.png' `
+do
+
+  # crush image and save it's output
+  pngcrush -brute $file /tmp/crushed.png
+
+  # overwrite original image with crushed version
+  mv /tmp/crushed.png $file
+
+done
