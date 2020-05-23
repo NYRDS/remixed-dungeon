69ff85c3af3b223bbda21db9004358b805caa684
Mikhael  Danilov <mikhael.danilov@gmail.com>
2020-05-24 00:33:58 +0300

    Backstab & Haste abilities

--- RemixedDungeon/src/main/assets/scripts/spells/CustomSpellsList.lua
+++ RemixedDungeon/src/main/assets/scripts/spells/CustomSpellsList.lua
@@ -9,6 +9,6 @@ spells["Necromancy"] = {"RaiseDead","Exhumation", "DarkSacrifice","Possess"}
 spells["Common"] = {"TownPortal"}
 
 spells["Combat"] = {"DieHard","Dash","BodyArmor","Smash"}
-spells["Rogue"] = {"Cloak"}
+spells["Rogue"] = {"Cloak","Haste","Backstab"}
 
 return spells
