b03c0d273044e3562db4f0d0ad24c65f4d242bec
PavelProvotorov <pavel.provotorov1998@gmail.com>
2020-12-25 00:21:03 +0200

    Inn Commit No3 (Sprites, Descriptions, Translation)
    - Uploaded sprites for Inn NPCs and added framing
    - Fixed logic layer for Inn.json and added object layer with corresponding NPCs
    - Updated Eng and Rus string values and added additional lines, due to changes in .tsx files

--- TiledMaps/tilesets/logic.tsx
+++ TiledMaps/tilesets/logic.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tiles0" tilewidth="16" tileheight="16" tilecount="64" columns="16">
+<tileset name="logic" tilewidth="16" tileheight="16" tilecount="64" columns="16">
  <image source="../Png/tiles0.png" width="256" height="64"/>
 </tileset>
