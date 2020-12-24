4f96e21ada22da010eec0fbd3a9edd9fc30fbf39
PavelProvotorov <pavel.provotorov1998@gmail.com>
2020-12-24 11:59:40 +0200

    Inn Commit #2
    - Fixed paths to assets and tilesets
    - Removed unnecessary files
    - Added missing tile properties

--- TiledMaps/tilesets/logic.tsx
+++ TiledMaps/tilesets/logic.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="tiles0" tilewidth="16" tileheight="16" tilecount="64" columns="16">
- <image source="../../RemixedDungeon/src/main/assets/tiles0.png" width="256" height="64"/>
+ <image source="../Png/tiles0.png" width="256" height="64"/>
 </tileset>
