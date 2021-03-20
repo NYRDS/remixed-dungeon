c3f3616e6a8aba00cfc44dbe0e74498d5b2123c0
Mikhael  Danilov <mikhael.danilov@gmail.com>
2021-03-20 23:58:13 +0300

    Upgrade rest of admob too

--- RemixedDungeon/src/googlePlay/java/com/nyrds/pixeldungeon/support/AdMobComboProvider.java
+++ RemixedDungeon/src/googlePlay/java/com/nyrds/pixeldungeon/support/AdMobComboProvider.java
@@ -1,99 +1,84 @@
 package com.nyrds.pixeldungeon.support;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
-import android.util.DisplayMetrics;
-import android.view.Display;
+
+import androidx.annotation.NonNull;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.FullScreenContentCallback;
 import com.google.android.gms.ads.LoadAdError;
-import com.nyrds.pixeldungeon.ml.EventCollector;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.nyrds.pixeldungeon.ml.R;
 import com.watabou.noosa.Game;
 import com.watabou.noosa.InterstitialPoint;
-import com.watabou.pixeldungeon.RemixedDungeon;
 
 public class AdMobComboProvider implements AdsUtilsCommon.IInterstitialProvider, AdsUtilsCommon.IBannerProvider {
-    private static InterstitialAd mInterstitialAd;
+    private static InterstitialAd mInterstitialAd = null;
     private AdView adView;
 
-    static {
-        Game.instance().runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                if (mInterstitialAd == null) {
-                    mInterstitialAd = new InterstitialAd(Game.instance());
-                    mInterstitialAd.setAdUnitId(Game.getVar(R.string.saveLoadAdUnitId));
-                    requestNewInterstitial();
-                }
-            }
-        });
+    AdMobComboProvider() {
+        Game.instance().runOnUiThread(this::requestNewInterstitial);
     }
 
-    private static void requestNewInterstitial() {
+    private void requestNewInterstitial() {
 
-        if (mInterstitialAd.isLoaded() || mInterstitialAd.isLoading()) {
+        if (mInterstitialAd!=null) {
             return;
         }
 
-        mInterstitialAd.setAdListener(new AdMobAdListener());
+        InterstitialAd.load(
+                Game.instance(),
+                Game.getVar(R.string.saveLoadAdUnitId),
+                AdMob.makeAdRequest(),
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        mInterstitialAd = interstitialAd;
+                    }
 
-        mInterstitialAd.loadAd(AdMob.makeAdRequest());
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                    }
+                }
+        );
     }
-
 
     @Override
     public void showInterstitial(final InterstitialPoint ret) {
         Game.instance().runOnUiThread(() -> {
             if (mInterstitialAd == null) {
-                EventCollector.logException("mInterstitialAd == null");
                 AdsUtilsCommon.interstitialFailed(AdMobComboProvider.this, ret);
                 return;
             }
 
-            if (!mInterstitialAd.isLoaded()) {
-                //EventCollector.logException("not loaded");
-                AdsUtilsCommon.interstitialFailed(AdMobComboProvider.this, ret);
-                return;
-            }
-
-            mInterstitialAd.setAdListener(new AdListener() {
+            FullScreenContentCallback fullScreenContentCallback = new FullScreenContentCallback() {
                 @Override
-                public void onAdClosed() {
-                    requestNewInterstitial();
+                public void onAdDismissedFullScreenContent() {
+                    mInterstitialAd = null;
                     ret.returnToWork(true);
                 }
-            });
-            mInterstitialAd.show();
+            };
+
+            mInterstitialAd.setFullScreenContentCallback(fullScreenContentCallback);
+            mInterstitialAd.show(Game.instance());
         });
 
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public void displayBanner() {
         adView = new AdView(Game.instance());
         adView.setAdUnitId(Game.getVar(R.string.easyModeAdUnitId));
         adView.setBackgroundColor(Color.TRANSPARENT);
         adView.setAdListener(new AdmobBannerListener());
-        adView.setAdSize(getAdSize());
+        adView.setAdSize(AdSize.SMART_BANNER);
         adView.loadAd(AdMob.makeAdRequest());
     }
-
-    private AdSize getAdSize() {
-        Display display = RemixedDungeon.instance().getWindowManager().getDefaultDisplay();
-        DisplayMetrics outMetrics = new DisplayMetrics();
-        display.getMetrics(outMetrics);
-
-        float widthPixels = outMetrics.widthPixels;
-        float density = outMetrics.density;
-
-        int adWidth = (int) (widthPixels / density);
-
-        return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(RemixedDungeon.instance(), adWidth);
-    }
-
 
     @Override
     public boolean isReady() {
@@ -109,35 +94,7 @@ public class AdMobComboProvider implements AdsUtilsCommon.IInterstitialProvider,
 
         @Override
         public void onAdFailedToLoad(LoadAdError reason) {
-            //EventCollector.logException( "admob banner:" + reason.toString());
             AdsUtilsCommon.bannerFailed(AdMobComboProvider.this);
-        }
-    }
-
-    private static class AdMobAdListener extends AdListener {
-        @Override
-        public void onAdClosed() {
-            super.onAdClosed();
-        }
-
-        @Override
-        public void onAdFailedToLoad(LoadAdError reason) {
-            //EventCollector.logException( "admob is:" + reason.toString());
-        }
-
-        @Override
-        public void onAdLoaded() {
-            super.onAdLoaded();
-        }
-
-        @Override
-        public void onAdOpened() {
-            super.onAdOpened();
-        }
-
-        @Override
-        public void onAdLeftApplication() {
-            super.onAdLeftApplication();
         }
     }
 }
