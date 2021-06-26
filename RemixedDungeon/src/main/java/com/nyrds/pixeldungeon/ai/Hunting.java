5e68696676a75da8c079a5c4edea8b60d9a2b1e9
Mikhael  Danilov <mikhael.danilov@gmail.com>
2021-06-26 13:23:50 +0300

    * post merge fix

--- RemixedDungeon/src/main/java/com/nyrds/pixeldungeon/ai/Hunting.java
+++ RemixedDungeon/src/main/java/com/nyrds/pixeldungeon/ai/Hunting.java
@@ -3,6 +3,7 @@ package com.nyrds.pixeldungeon.ai;
 import com.nyrds.pixeldungeon.mechanics.NamedEntityKind;
 import com.nyrds.pixeldungeon.ml.R;
 import com.watabou.noosa.Game;
+import com.watabou.pixeldungeon.actors.Actor;
 import com.watabou.pixeldungeon.actors.Char;
 import com.watabou.pixeldungeon.actors.mobs.Mob;
 import com.watabou.pixeldungeon.utils.Utils;
