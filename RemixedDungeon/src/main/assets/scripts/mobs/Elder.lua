80380177fa57aae5a080949beccdea84dfd85fd5
Mikhael  Danilov <mikhael.danilov@gmail.com>
2021-07-05 01:36:52 +0300

    * Fix elderShaman script

--- RemixedDungeon/src/main/assets/scripts/mobs/Elder.lua
+++ RemixedDungeon/src/main/assets/scripts/mobs/Elder.lua
@@ -7,29 +7,9 @@
 
 local RPD = require "scripts/lib/commonClasses"
 local mob = require "scripts/lib/mob"
-local buffState = false
 
 return mob.init{
-    attackProc = function(self, enemy, dmg)
-        return dmg
-    end,
-	
     zapProc = function(self, enemy, dmg)
-	if buffState == false then
-		buffState = true
-        RPD.permanentBuff(self, "ManaShield")
-		return dmg
-	else 
-		return dmg
-    end
-end,
-
-    defenceProc = function(self, buff, enemy, damage)
-	if buffState == true then
-		RPD.topEffect(self:getPos(),"mana_shield_effect")
-		buffState = false
-	else 
-		return dmg
-    end
-end
+		RPD.affectBuff(self, "ManaShield", self:skillLevel())
+	end
 }
