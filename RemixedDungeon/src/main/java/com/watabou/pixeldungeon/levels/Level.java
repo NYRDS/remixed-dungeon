438a369339a7b72e4231944080f015577814a363
Mikhael  Danilov <mikhael.danilov@gmail.com>
2021-07-07 21:40:25 +0300

    * Plants in doorway fix

--- RemixedDungeon/src/main/java/com/watabou/pixeldungeon/levels/Level.java
+++ RemixedDungeon/src/main/java/com/watabou/pixeldungeon/levels/Level.java
@@ -1084,13 +1084,6 @@ public abstract class Level implements Bundlable {
 		pit[cell] = (flags & TerrainFlags.PIT) != 0;
 		water[cell] = terrain == Terrain.WATER
 				|| terrain >= Terrain.WATER_TILES;
-
-		if(solid[cell] || pit[cell]) {
-			LevelObject obj;
-			while((obj=getTopLevelObject(cell))!=null) {
-				remove(obj);
-			}
-		}
 	}
 
 	public void drop(Item item, int cell, Heap.Type type) {
