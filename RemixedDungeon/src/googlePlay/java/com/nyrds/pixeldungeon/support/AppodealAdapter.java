d3ffe64ee7d9fbe19ece13a34445fb93541d354f
Mikhael  Danilov <mikhael.danilov@gmail.com>
2021-07-16 22:14:44 +0300

    l10n sync

--- RemixedDungeon/src/googlePlay/java/com/nyrds/pixeldungeon/support/AppodealAdapter.java
+++ RemixedDungeon/src/googlePlay/java/com/nyrds/pixeldungeon/support/AppodealAdapter.java
@@ -37,6 +37,7 @@ public class AppodealAdapter {
             return;
         }
 
+        Appodeal.disableWriteExternalStoragePermissionCheck();
         Appodeal.disableLocationPermissionCheck();
 
 
