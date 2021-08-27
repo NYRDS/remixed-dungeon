b8b462d622bf6de0178d154c838f5e61b7a34c08
Mikhael  Danilov <mikhael.danilov@gmail.com>
2021-08-27 11:33:43 +0300

    Pet duplication fix

--- RemixedDungeon/src/main/java/com/nyrds/pixeldungeon/utils/CharsList.java
+++ RemixedDungeon/src/main/java/com/nyrds/pixeldungeon/utils/CharsList.java
@@ -1,5 +1,6 @@
 package com.nyrds.pixeldungeon.utils;
 
+import com.google.android.gms.common.util.ArrayUtils;
 import com.nyrds.LuaInterface;
 import com.nyrds.platform.util.TrackedRuntimeException;
 import com.nyrds.util.Util;
@@ -8,11 +9,14 @@ import com.watabou.pixeldungeon.actors.DummyChar;
 import com.watabou.pixeldungeon.actors.hero.Hero;
 import com.watabou.pixeldungeon.actors.mobs.Mob;
 import com.watabou.pixeldungeon.utils.GLog;
+import com.watabou.utils.Bundle;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -22,6 +26,8 @@ public class CharsList {
     public static final Char DUMMY = new DummyChar();
 
     private static final ConcurrentHashMap<Integer, Char> charsMap = new ConcurrentHashMap<>();
+    private static final HashSet<Integer> destroyedChars = new HashSet<>();
+    private static final String GRAVEYARD = "graveyard";
 
     static public List<Mob> emptyMobList = Collections.unmodifiableList(new ArrayList<>());
 
@@ -49,6 +55,25 @@ public class CharsList {
 
     static public void remove(int id) {
         charsMap.remove(id);
+    }
+
+    static public void destroy(int id) {
+        destroyedChars.add(id);
+        remove(id);
+    }
+
+    static public boolean isDestroyed(int id) {
+        return destroyedChars.contains(id);
+    }
+
+
+    static public void storeInBundle(Bundle bundle) {
+        bundle.put(GRAVEYARD,  ArrayUtils.toPrimitiveArray(destroyedChars));
+    }
+
+    static public void restoreFromBundle(Bundle bundle) {
+        destroyedChars.clear();
+        destroyedChars.addAll(Arrays.asList(ArrayUtils.toWrapperArray( bundle.getIntArray(GRAVEYARD))));
     }
 
     static public void reset() {
