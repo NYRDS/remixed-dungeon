1c45ed20e9000fa4977dd210d269c9b1c0873215
Mikhael  Danilov <mikhael.danilov@gmail.com>
2022-12-19 02:04:35 +0300

    Script for mods info update

---
+++ tools/mods/mods_data.py
@@ -0,0 +1,40 @@
+import json
+import os
+import urllib.request
+import zipfile
+
+properties = ["Mod_Author", "Mod_Link","Mod_Email","Mod_Name","Mod_Description"]
+
+mods_json = urllib.request.urlopen("https://nyrds.github.io/NYRDS/mods2.json").read().decode()
+mods = json.loads(mods_json)
+mods['info'] = {}
+
+for cat in mods['known_mods']:
+    for mod in mods['known_mods'][cat]:
+        urllib.request.urlretrieve(mod['url'], mod['name'])
+        with zipfile.ZipFile(mod['name'], 'r') as zip_ref:
+            zip_ref.extractall(f"./mods")
+
+for dir in os.listdir("mods"):
+
+    for lang in ["en","ru","fr","es"]:
+        try:
+            mod_data = {}
+
+            with open(f"mods/{dir}/strings_{lang}.json", 'r') as strings_en:
+                for line in strings_en:
+                    data = json.loads(line)
+                    for prop in properties:
+                        if prop == data[0]:
+                            mod_data[data[0]] = data[1]
+
+            if mod_data['Mod_Name'] not in mods['info']:
+                mods['info'][mod_data['Mod_Name']] = {}
+
+            mods['info'][mod_data['Mod_Name']][lang] = mod_data
+
+        except Exception as e:
+            pass
+
+with open("mods2.json", 'w') as mods_json:
+    mods_json.write(json.dumps(mods,indent=2))
