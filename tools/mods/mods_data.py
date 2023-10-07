4efd12f8b57a8153bddb422b030882dc1c076cf2
Mikhael  Danilov <mikhael.danilov@gmail.com>
2023-10-08 02:16:34 +0300

    mods scripts

--- tools/mods/mods_data.py
+++ tools/mods/mods_data.py
@@ -1,13 +1,16 @@
 import json
 import os
-import urllib.request
+import requests
 import zipfile
 import codecs
 
 
+
 properties = ["Mod_Author", "Mod_Link","Mod_Email","Mod_Name","Mod_Description"]
 
-mods_json = urllib.request.urlopen("https://nyrds.github.io/NYRDS/mods2.json").read().decode()
+#mods_json = urllib.request.urlopen("https://nyrds.github.io/NYRDS/mods2.json").read().decode()
+
+mods_json = open("mods2.json").read()
 mods = json.loads(mods_json)
 mods['info'] = {}
 
@@ -15,7 +18,12 @@ print("Downloading mods...")
 for cat in mods['known_mods']:
     for mod in mods['known_mods'][cat]:
         print(mod)
-        urllib.request.urlretrieve(mod['url'], mod['name'])
+
+        response = requests.get(mod['url'], timeout=5)
+
+        with open(mod['name'], 'wb') as f:
+            f.write(response.content)
+
         with zipfile.ZipFile(mod['name'], 'r') as zip_ref:
             print("Extracting", mod['name'])
             zip_ref.extractall(f"./mods")
@@ -23,6 +31,7 @@ for cat in mods['known_mods']:
 print("Parsing...")
 for dir in os.listdir("mods"):
     print(dir)
+
     for lang in ["en","ru","fr","es"]:
         try:
             mod_data = {}
