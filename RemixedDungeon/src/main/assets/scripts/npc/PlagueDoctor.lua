8aaa8f338ead8228e184bdae48fe8a8f68b3a7cc
Mikhael  Danilov <mikhael.danilov@gmail.com>
2024-07-14 00:38:55 +0300

    PlagueDoctor quest - more data & debugging

--- RemixedDungeon/src/main/assets/scripts/npc/PlagueDoctor.lua
+++ RemixedDungeon/src/main/assets/scripts/npc/PlagueDoctor.lua
@@ -14,14 +14,14 @@ questList = {
             prologue = { text = 'PlagueDoctorQuest_1_1_Prologue' },
             requirements = { item = { kind = "Carcass of Rat", quantity = 5 } },
             in_progress = { text = "PlagueDoctorQuest_1_1_InProgress" },
-            reward = { kind = "RatArmor", quantity = 1 },
+            reward = { item = { kind = "RatArmor", quantity = 1 } },
             epilogue = { text = "PlagueDoctorQuest_1_1_Epilogue" }
         },
         {
             prologue = { text = 'PlagueDoctorQuest_1_2_Prologue' },
             requirements = { item = { kind = "Carcass of Snail", quantity = 10 } },
             in_progress = { text = "PlagueDoctorQuest_1_2_InProgress" },
-            reward = { kind = "Gold", quantity = 50 },
+            reward = { item = { kind = "Gold", quantity = 50 } },
             epilogue = { text = "PlagueDoctorQuest_1_2_Epilogue" }
         }
     },
@@ -30,14 +30,14 @@ questList = {
             prologue = { text = 'PlagueDoctorQuest_2_1_Prologue' },
             requirements = { item = { kind = "Moongrace.Seed", quantity = 2 } },
             in_progress = { text = "PlagueDoctorQuest_2_1_InProgress" },
-            reward = { kind = "PotionOfMana", quantity = 1 },
+            reward = { item = { kind = "PotionOfMana", quantity = 1 } },
             epilogue = { text = "PlagueDoctorQuest_2_1_Epilogue" }
         },
         {
             prologue = { text = 'PlagueDoctorQuest_2_2_Prologue' },
             requirements = { item = { kind = "Sungrass.Seed", quantity = 2 } },
             in_progress = { text = "PlagueDoctorQuest_2_2_InProgress" },
-            reward = { kind = "PotionOfHealth", quantity = 1 },
+            reward = { item = { kind = "PotionOfHealth", quantity = 1 } },
             epilogue = { text = "PlagueDoctorQuest_2_2_Epilogue" }
         }
     },
@@ -46,14 +46,14 @@ questList = {
             prologue = { text = 'PlagueDoctorQuest_3_1_Prologue' },
             requirements = { mob = { kind = "Bat", quantity = 1 } },
             in_progress = { text = "PlagueDoctorQuest_3_1_InProgress" },
-            reward = { kind = "ScrollOfUpgrade", quantity = 1 },
+            reward = { item = { kind = "ScrollOfUpgrade", quantity = 1 } },
             epilogue = { text = "PlagueDoctorQuest_3_1_Epilogue" }
         },
         {
             prologue = { text = 'PlagueDoctorQuest_3_2_Prologue' },
             requirements = { mob = { kind = "DeathKnight", quantity = 1 } },
             in_progress = { text = "PlagueDoctorQuest_3_2_InProgress" },
-            reward = { kind = "PotionOfStrength", quantity = 1 },
+            reward = { item = { kind = "PotionOfStrength", quantity = 1 } },
             epilogue = { text = "PlagueDoctorQuest_3_2_Epilogue" }
         }
     },
@@ -62,17 +62,27 @@ questList = {
             prologue = { text = 'PlagueDoctorQuest_4_1_Prologue' },
             requirements = { item = { kind = "Carcass of Warlock", quantity = 5 } },
             in_progress = { text = "PlagueDoctorQuest_4_1_InProgress" },
-            reward = { kind = "PotionOfMight", quantity = 1 },
+            reward = { item = { kind = "PotionOfMight", quantity = 1 } },
             epilogue = { text = "PlagueDoctorQuest_4_1_Epilogue" }
         },
         {
             prologue = { text = 'PlagueDoctorQuest_4_2_Prologue' },
             requirements = { item = { kind = "Carcass of KoboldIcemancer", quantity = 5 } },
             in_progress = { text = "PlagueDoctorQuest_4_2_InProgress" },
-            reward = { kind = "PotionOfMight", quantity = 1 },
+            reward = { item = { kind = "PotionOfMight", quantity = 1 } },
             epilogue = { text = "PlagueDoctorQuest_4_2_Epilogue" }
         }
+    },
+    {
+        {
+            prologue = { text = 'PlagueDoctorQuest_5_1_Prologue' },
+            requirements = { mob = { kind = "Succubus", quantity = 2 } },
+            in_progress = { text = "PlagueDoctorQuest_5_1_InProgress" },
+            reward = { special = {} },
+            epilogue = { text = "PlagueDoctorQuest_5_1_Epilogue" }
+        }
     }
+
 }
 
 return mob.init({
@@ -89,6 +99,7 @@ return mob.init({
 
         if not data["questInProgress"] then
             questVariant = math.random(1, #questList[questIndex])
+            RPD.debug("Quest "..questIndex.." "..questVariant.." "..#questList[questIndex])
 
             RPD.showQuestWindow(self, questList[questIndex][questVariant].prologue.text)
 
@@ -99,6 +110,23 @@ return mob.init({
             return
         else
             local quest = questList[questIndex][questVariant]
+
+            local function giveReward()
+                local rewardItem = quest.reward.item
+                local reward = RPD.item(rewardItem.kind, rewardItem.quantity)
+                chr:collectAnimated(reward)
+
+                RPD.showQuestWindow(self, quest.epilogue.text)
+
+                data["questInProgress"] = false
+                data["questIndex"] = questIndex + 1
+                mob.storeData(self, data)
+            end
+
+            local function inProgress()
+                RPD.showQuestWindow(self, quest.in_progress.text)
+            end
+
             local requirements = quest.requirements
 
             if requirements.item then
@@ -115,19 +143,12 @@ return mob.init({
                         wantedItem:quantity(actualQty - wantedQty)
                     end
 
-                    RPD.showQuestWindow(self, quest.epilogue.text)
-
-                    data["questInProgress"] = false
-                    data["questIndex"] = questIndex + 1
-
-                    local reward = RPD.item(quest.reward.kind, quest.reward.quantity)
-                    chr:collectAnimated(reward)
-
-                    mob.storeData(self, data)
-                    return
+                    return giveReward()
                 end
+                return inProgress()
+            end
 
-            elseif requirements.mob then
+            if requirements.mob then
                 local wantedMob = requirements.mob.kind
                 local wantedQty = requirements.mob.quantity
 
@@ -150,21 +171,11 @@ return mob.init({
                         end
                     end
 
-                    RPD.showQuestWindow(self, quest.epilogue.text)
-                    data["questInProgress"] = false
-                    data["questIndex"] = questIndex + 1
-                    mob.storeData(self, data)
-                    return
+                    return giveReward()
                 end
-
-            else
-                RPD.showQuestWindow(self, quest.in_progress.text)
-                return
+                return inProgress()
             end
-
-
         end
-
     end,
     spawn = function(self, level)
         level:setCompassTarget(self:getPos())
