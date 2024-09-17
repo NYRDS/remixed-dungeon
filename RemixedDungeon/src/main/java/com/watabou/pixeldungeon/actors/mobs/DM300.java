a082ffe0429312f59dcb8593f5f38bffa94dfe02
Mikhael <mikhael.danilov@gmail.com>
2024-09-18 00:43:12 +0300

    DM300 now immune to terror

--- RemixedDungeon/src/main/java/com/watabou/pixeldungeon/actors/mobs/DM300.java
+++ RemixedDungeon/src/main/java/com/watabou/pixeldungeon/actors/mobs/DM300.java
@@ -22,6 +22,7 @@ import com.watabou.pixeldungeon.actors.blobs.ToxicGas;
 import com.watabou.pixeldungeon.actors.buffs.Bleeding;
 import com.watabou.pixeldungeon.actors.buffs.Buff;
 import com.watabou.pixeldungeon.actors.buffs.Stun;
+import com.watabou.pixeldungeon.actors.buffs.Terror;
 import com.watabou.pixeldungeon.effects.CellEmitter;
 import com.watabou.pixeldungeon.effects.Speck;
 import com.watabou.pixeldungeon.effects.particles.ElmoParticle;
@@ -37,92 +38,93 @@ import com.watabou.utils.Random;
 import org.jetbrains.annotations.NotNull;
 
 public class DM300 extends Boss {
-	
-	public DM300() {
-		spriteClass = DM300Sprite.class;
-		
-		hp(ht(200));
-		expForKill = 30;
-		baseDefenseSkill = 18;
-		baseAttackSkill  = 28;
-		dmgMin = 18;
-		dmgMax = 24;
-		dr = 10;
 
-		float dice = Random.Float();
-		if( dice < 0.5 ) {
-			loot(new ChaosCrystal(), 0.333f);
-		} else {
-			loot(new RingOfThorns().random(), 0.333f);
-		}
+    public DM300() {
+        spriteClass = DM300Sprite.class;
 
-		addImmunity( ToxicGas.class );
-		addImmunity( Bleeding.class );
+        hp(ht(200));
+        expForKill = 30;
+        baseDefenseSkill = 18;
+        baseAttackSkill = 28;
+        dmgMin = 18;
+        dmgMax = 24;
+        dr = 10;
 
-		collect(new SkeletonKey());
-	}
+        float dice = Random.Float();
+        if (dice < 0.5) {
+            loot(new ChaosCrystal(), 0.333f);
+        } else {
+            loot(new RingOfThorns().random(), 0.333f);
+        }
 
-	@Override
-	public boolean act() {
+        addImmunity(ToxicGas.class);
+        addImmunity(Bleeding.class);
+        addImmunity(Terror.class);
 
-		GameScene.add( Blob.seed( getPos(), 30, ToxicGas.class ) );
-		
-		return super.act();
-	}
-	
-	@Override
-	public void move( int step ) {
-		super.move( step );
+        collect(new SkeletonKey());
+    }
 
-		final LevelObject object = level().getTopLevelObject(step);
+    @Override
+    public boolean act() {
 
-		if (object instanceof Trap && hp() < ht()) {
+        GameScene.add(Blob.seed(getPos(), 30, ToxicGas.class));
 
-			Trap tr = (Trap) object;
-			tr.reactivate(LevelObjectsFactory.TOXIC_TRAP, GameLoop.getDifficulty() + 1);
+        return super.act();
+    }
 
-			heal(Random.Int( 1, ht() - hp() ), this, true);
+    @Override
+    public void move(int step) {
+        super.move(step);
 
-			getSprite().emitter().burst( ElmoParticle.FACTORY, 5 );
-			
-			if (CharUtils.isVisible(this) && Dungeon.hero.isAlive()) {
+        final LevelObject object = level().getTopLevelObject(step);
+
+        if (object instanceof Trap && hp() < ht()) {
+
+            Trap tr = (Trap) object;
+            tr.reactivate(LevelObjectsFactory.TOXIC_TRAP, GameLoop.getDifficulty() + 1);
+
+            heal(Random.Int(1, ht() - hp()), this, true);
+
+            getSprite().emitter().burst(ElmoParticle.FACTORY, 5);
+
+            if (CharUtils.isVisible(this) && Dungeon.hero.isAlive()) {
                 GLog.n(StringsManager.getVar(R.string.DM300_Info1));
-			}
-		}
-		
-		int cell = step + Level.NEIGHBOURS8[Random.Int( Level.NEIGHBOURS8.length )];
-		
-		if (CharUtils.isVisible(this)) {
-			CellEmitter.get( cell ).start( Speck.factory( Speck.ROCK ), 0.07f, 10 );
-			Camera.main.shake( 3, 0.7f );
-			Sample.INSTANCE.play( Assets.SND_ROCKS );
+            }
+        }
 
-			if (level().water[cell]) {
-				GameScene.ripple( cell );
-			} else if (level().map[cell] == Terrain.EMPTY) {
-				level().set( cell, Terrain.EMPTY_DECO );
-				GameScene.updateMap( cell );
-			}
-		}
+        int cell = step + Level.NEIGHBOURS8[Random.Int(Level.NEIGHBOURS8.length)];
 
-		Char ch = Actor.findChar( cell );
-		if (ch != null && ch != this) {
-			Buff.prolong( ch, Stun.class, 2 );
-		}
-	}
-	
-	@Override
-	public void die(@NotNull NamedEntityKind cause) {
-		super.die( cause );
+        if (CharUtils.isVisible(this)) {
+            CellEmitter.get(cell).start(Speck.factory(Speck.ROCK), 0.07f, 10);
+            Camera.main.shake(3, 0.7f);
+            Sample.INSTANCE.play(Assets.SND_ROCKS);
 
-		Badges.validateBossSlain(Badges.Badge.BOSS_SLAIN_3);
+            if (level().water[cell]) {
+                GameScene.ripple(cell);
+            } else if (level().map[cell] == Terrain.EMPTY) {
+                level().set(cell, Terrain.EMPTY_DECO);
+                GameScene.updateMap(cell);
+            }
+        }
+
+        Char ch = Actor.findChar(cell);
+        if (ch != null && ch != this) {
+            Buff.prolong(ch, Stun.class, 2);
+        }
+    }
+
+    @Override
+    public void die(@NotNull NamedEntityKind cause) {
+        super.die(cause);
+
+        Badges.validateBossSlain(Badges.Badge.BOSS_SLAIN_3);
 
         yell(StringsManager.getVar(R.string.DM300_Info2));
-	}
-	
-	@Override
-	public void notice() {
-		super.notice();
+    }
+
+    @Override
+    public void notice() {
+        super.notice();
         yell(StringsManager.getVar(R.string.DM300_Info3));
-	}
+    }
 }
