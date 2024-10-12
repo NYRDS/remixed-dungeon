657c4e409467fea3a4a339f1ac17d03c193136e2
Mikhael  Danilov <mikhael.danilov@gmail.com>
2024-10-12 19:08:29 +0300

    MirrorImage fix

--- RemixedDungeon/src/main/assets/scripts/items/test/TestItem.lua
+++ RemixedDungeon/src/main/assets/scripts/items/test/TestItem.lua
@@ -229,13 +229,13 @@ return itemLib.init{
 
     activate = function(self, item, hero)
 
-        local Buff = RPD.affectBuff(hero,"NotImplementedTestBuff", 10)
-        Buff:level(3)
-        Buff:setSource(item)
+      --  local Buff = RPD.affectBuff(hero,"NotImplementedTestBuff", 10)
+      --  Buff:level(3)
+      --  Buff:setSource(item)
     end,
 
     deactivate = function(self, item, hero)
-        RPD.removeBuff(hero,"NotImplementedTestBuff")
+      --  RPD.removeBuff(hero,"NotImplementedTestBuff")
     end,
 
     act = function(self,item)
