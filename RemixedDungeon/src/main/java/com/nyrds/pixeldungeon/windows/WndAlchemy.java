b8534e083b8019d9233d303f5d7443c8ac441ceb
Mikhael  Danilov <mikhael.danilov@gmail.com>
2025-06-01 03:25:10 +0300

    WndAlchemy stub - wip

--- RemixedDungeon/src/main/java/com/nyrds/pixeldungeon/windows/WndAlchemy.java
+++ RemixedDungeon/src/main/java/com/nyrds/pixeldungeon/windows/WndAlchemy.java
@@ -9,22 +9,29 @@ import com.watabou.pixeldungeon.ui.Window;
 import com.watabou.pixeldungeon.utils.GLog;
 import com.watabou.utils.PointF;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WndAlchemy extends Window {
 
     TouchController touch;
-
-    ItemSprite sprite;
+    List<ItemSprite> sprites = new ArrayList<>();
 
     public WndAlchemy() {
-        sprite = new ItemSprite(ItemFactory.itemByName("Sword"));
-        sprite.setPos(20, 20);
-        add(sprite);
+
+        ItemSprite sword = new ItemSprite(ItemFactory.itemByName("Sword"));
+        sword.setPos(20, 20);
+        sprites.add(sword);
+        add(sword);
+
+        ItemSprite woodenShield = new ItemSprite(ItemFactory.itemByName("WoodenShield"));
+        woodenShield.setPos(60, 20);
+        sprites.add(woodenShield);
+        add(woodenShield);
 
         touch = new TouchController();
         touch.camera = PixelScene.uiCamera;
-
         add(touch);
-
 
         resize(160, 120);
     }
@@ -32,6 +39,10 @@ public class WndAlchemy extends Window {
     public class TouchController extends TouchArea {
 
         private final float dragThreshold;
+        private boolean touchStartedOnSprite = false;
+        private boolean dragging = false;
+        private ItemSprite draggedSprite;
+        private final PointF lastPos = new PointF();
 
         public TouchController() {
             super(WndAlchemy.this.chrome);
@@ -39,49 +50,51 @@ public class WndAlchemy extends Window {
         }
 
         @Override
-        protected void onClick(Touchscreen.Touch touch) {
-            GLog.debug("click %3.0f %3.0f %b", x,y, dragging);
-            if (dragging) {
-                dragging = false;
-            }
-        }
-
-        @Override
         protected void onTouchDown(Touchscreen.Touch touch) {
+            touchStartedOnSprite = false;
+            for (int i = sprites.size() - 1; i >= 0; i--) {
+                ItemSprite sprite = sprites.get(i);
+                if (sprite.overlapsScreenPoint((int) touch.current.x, (int) touch.current.y)) {
+                    touchStartedOnSprite = true;
+                    draggedSprite = sprite;
+                    break;
+                }
+            }
             GLog.debug("Touch: %3.0f %3.0f", touch.current.x, touch.current.y);
             super.onTouchDown(touch);
         }
 
         @Override
-        protected void onTouchUp(Touchscreen.Touch touch) {
-            GLog.debug("Touch: %3.0f %3.0f", touch.current.x, touch.current.y);
-            super.onTouchUp(touch);
-        }
-
-        // true if dragging is in progress
-        private boolean dragging = false;
-        // last touch cords
-        private final PointF lastPos = new PointF();
-
-        @Override
         protected void onDrag(Touchscreen.Touch t) {
             if (dragging) {
+                PointF scrollStep = PointF.diff(lastPos, t.current);
+                float zoom = camera.zoom;
 
-                if (sprite.overlapsScreenPoint((int) t.current.x, (int) t.current.y)){
-                    PointF scrollStep = PointF.diff(lastPos, t.current);
-                    sprite.x -= scrollStep.x;
-                    sprite.y -= scrollStep.y;
-                    sprite.dirtyMatrix = true;
+                scrollStep.x /= zoom;
+                scrollStep.y /= zoom;
 
-                    GLog.debug("scroll this: %3.0f : %3.0f", scrollStep.x, scrollStep.y);                }
+                if (draggedSprite != null) {
+                    draggedSprite.x -= scrollStep.x;
+                    draggedSprite.y -= scrollStep.y;
+                    draggedSprite.dirtyMatrix = true;
+                }
 
+                GLog.debug("scroll this: %3.0f : %3.0f", scrollStep.x, scrollStep.y);
             } else if (PointF.distance(t.current, t.start) > dragThreshold) {
-
-                dragging = true;
-
+                if (touchStartedOnSprite) {
+                    dragging = true;
+                }
             }
             lastPos.set(t.current);
         }
+
+        @Override
+        protected void onTouchUp(Touchscreen.Touch touch) {
+            touchStartedOnSprite = false;
+            dragging = false;
+            draggedSprite = null;
+            GLog.debug("Touch: %3.0f %3.0f", touch.current.x, touch.current.y);
+            super.onTouchUp(touch);
+        }
     }
 }
-
