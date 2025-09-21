e8f7508a9f8085c797c4c811b14e961b2e745096
Mikhael  Danilov <mikhael.danilov@gmail.com>
2025-09-22 02:41:11 +0300

    Improve DownloadTask redirect handling. Added explicit redirect following for HTTP 301, 302, 303, 307, and 308 response codes. Implemented manual redirect following with recursion limit to ensure reliable downloads even when underlying HTTP connection implementation doesn't properly follow redirects. Added better error handling, logging, and resource cleanup.

--- RemixedDungeon/src/main/java/com/nyrds/util/DownloadTask.java
+++ RemixedDungeon/src/main/java/com/nyrds/util/DownloadTask.java
@@ -12,6 +12,8 @@ import java.net.URL;
 
 public class DownloadTask implements Runnable {
 
+    private static final int MAX_REDIRECTS = 5;
+    
     private final DownloadStateListener m_listener;
     private final String                m_url;
     private final String                m_downloadTo;
@@ -29,43 +31,73 @@ public class DownloadTask implements Runnable {
         m_listener.DownloadProgress(m_url, 0);
 
         try {
-            URL url = new URL(m_url);
-            File file = new File(m_downloadTo);
-
-            HttpURLConnection ucon = HttpConnectionFactory.create(url);
-            ucon.setReadTimeout(10000);
-            ucon.setInstanceFollowRedirects(true);
-            ucon.connect();
-
-            int repCode = ucon.getResponseCode();
-
-            if (repCode == HttpURLConnection.HTTP_OK) {
-                m_listener.DownloadProgress(m_url, 0);
-                int bytesTotal = ucon.getContentLength();
-
-                GLog.debug("bytes in file: " + bytesTotal);
-
-                try (InputStream is = ucon.getInputStream();
-                     FileOutputStream fos = new FileOutputStream(file)) {
-                    byte[] buffer = new byte[1024 * 128];
-                    int count;
-                    int bytesDownloaded = 0;
-
-                    while ((count = is.read(buffer)) != -1) {
-                        fos.write(buffer, 0, count);
-                        bytesDownloaded += count;
-                        m_listener.DownloadProgress(m_url, bytesDownloaded);
-                        Thread.yield();
-                    }
-                }
-
-                result = true;
-            }
-
+            result = downloadFile(m_url, m_downloadTo, 0);
         } catch (Exception e) {
-            EventCollector.logException(new ModError("Downloading",e));
+            EventCollector.logException(new ModError("Downloading", e));
         }
 
-         m_listener.DownloadComplete(m_url, result);
+        m_listener.DownloadComplete(m_url, result);
+    }
+
+    private boolean downloadFile(String urlStr, String downloadTo, int redirectCount) throws Exception {
+        if (redirectCount > MAX_REDIRECTS) {
+            GLog.debug("Too many redirects, aborting download");
+            return false;
+        }
+
+        URL url = new URL(urlStr);
+        File file = new File(downloadTo);
+
+        HttpURLConnection ucon = HttpConnectionFactory.create(url);
+        ucon.setReadTimeout(10000);
+        ucon.setInstanceFollowRedirects(true);
+        ucon.setConnectTimeout(10000);
+        ucon.connect();
+
+        int repCode = ucon.getResponseCode();
+        GLog.debug("HTTP response code: " + repCode + " for URL: " + urlStr);
+
+        // Handle redirects manually if needed
+        if (repCode == HttpURLConnection.HTTP_MOVED_PERM || 
+            repCode == HttpURLConnection.HTTP_MOVED_TEMP || 
+            repCode == HttpURLConnection.HTTP_SEE_OTHER ||
+            repCode == 307 || // TEMPORARY_REDIRECT
+            repCode == 308) { // PERMANENT_REDIRECT
+            
+            String redirectUrl = ucon.getHeaderField("Location");
+            if (redirectUrl != null && !redirectUrl.isEmpty()) {
+                GLog.debug("Following redirect to: " + redirectUrl);
+                ucon.disconnect();
+                return downloadFile(redirectUrl, downloadTo, redirectCount + 1);
+            }
+        }
+
+        if (repCode == HttpURLConnection.HTTP_OK) {
+            m_listener.DownloadProgress(m_url, 0);
+            int bytesTotal = ucon.getContentLength();
+
+            GLog.debug("bytes in file: " + bytesTotal);
+
+            try (InputStream is = ucon.getInputStream();
+                 FileOutputStream fos = new FileOutputStream(file)) {
+                byte[] buffer = new byte[1024 * 128];
+                int count;
+                int bytesDownloaded = 0;
+
+                while ((count = is.read(buffer)) != -1) {
+                    fos.write(buffer, 0, count);
+                    bytesDownloaded += count;
+                    m_listener.DownloadProgress(m_url, bytesDownloaded);
+                    Thread.yield();
+                }
+            }
+
+            ucon.disconnect();
+            return true;
+        } else {
+            GLog.debug("Download failed with HTTP response code: " + repCode);
+            ucon.disconnect();
+            return false;
+        }
     }
 }
