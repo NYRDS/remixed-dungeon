2510513d1ab6ac70aed640285674b9e5c10613b0
Mikhael  Danilov <mikhael.danilov@gmail.com>
2025-10-22 23:09:48 +0300

    Show localized mod info for installed mods and restore previous mod context on close
    When a mod is installed, switch the active mod context to the mod’s install dir to fetch its localized name, description, author, and site link from resource strings. Fall back to the provided ModDesc for non-installed mods. Track the previously active mod and restore it when the window is closed to avoid leaking context.

--- RemixedDungeon/src/main/java/com/watabou/pixeldungeon/windows/WndModInfo.java
+++ RemixedDungeon/src/main/java/com/watabou/pixeldungeon/windows/WndModInfo.java
@@ -1,6 +1,7 @@
 package com.watabou.pixeldungeon.windows;
 
 import com.nyrds.pixeldungeon.game.GameLoop;
+import com.nyrds.pixeldungeon.game.GamePreferences;
 import com.nyrds.pixeldungeon.ml.R;
 import com.nyrds.pixeldungeon.utils.ModDesc;
 import com.nyrds.pixeldungeon.windows.VBox;
@@ -23,53 +24,105 @@ import java.io.IOException;
 public class WndModInfo extends Window {
 
     public static String exportedModDir;
+    private final String prevMod;
 
     public WndModInfo(ModDesc desc) {
         super();
         resizeLimited(120);
 
+        this.prevMod = GamePreferences.activeMod();
+
         VBox mainLayout = new VBox();
 
-        Text modInfo = PixelScene.createMultiline(GuiProperties.titleFontSize());
-        modInfo.maxWidth(width);
-        modInfo.hardlight(Window.TITLE_COLOR);
-        modInfo.text(desc.name+"\n\n");
-        mainLayout.add(modInfo);
+        // Use the mod's data if it's installed, otherwise use the provided description
+        if (desc.installed) {
+            // Set the mod context to get localized strings from the mod itself
+            GamePreferences.activeMod(desc.installDir);
 
-        Text description = PixelScene.createMultiline(GuiProperties.titleFontSize());
-        description.maxWidth(width);
-        description.text(desc.description + "\n\n");
-        mainLayout.add(description);
+            Text modInfo = PixelScene.createMultiline(GuiProperties.titleFontSize());
+            modInfo.maxWidth(width);
+            modInfo.hardlight(Window.TITLE_COLOR);
+            modInfo.text(StringsManager.getVar(R.string.Mod_Name)+"\n\n");
+            mainLayout.add(modInfo);
 
-        Text author = PixelScene.createMultiline(GuiProperties.titleFontSize());
-        author.maxWidth(width);
-        author.text(StringsManager.getVar(R.string.Mods_CreatedBy) + "\n" +  desc.author + "\n\n");
+            Text description = PixelScene.createMultiline(GuiProperties.titleFontSize());
+            description.maxWidth(width);
+            description.text(StringsManager.getVar(R.string.Mod_Description) + "\n\n");
+            mainLayout.add(description);
 
-        mainLayout.add(author);
+            Text author = PixelScene.createMultiline(GuiProperties.titleFontSize());
+            author.maxWidth(width);
+            author.text(StringsManager.getVar(R.string.Mods_CreatedBy) + "\n" +  StringsManager.getVar(R.string.Mod_Author) + "\n\n");
 
-        final String siteUrl = desc.url;
+            mainLayout.add(author);
 
-        if (!siteUrl.isEmpty()) {
-            Text siteTitle = PixelScene.createMultiline(GuiProperties.titleFontSize());
-            siteTitle.maxWidth(width);
-            siteTitle.text(StringsManager.getVar(R.string.Mods_AuthorSite) + "\n");
+            final String siteUrl = StringsManager.getVar(R.string.Mod_Link);
 
-            mainLayout.add(siteTitle);
+            if (!siteUrl.isEmpty()) {
+                Text siteTitle = PixelScene.createMultiline(GuiProperties.titleFontSize());
+                siteTitle.maxWidth(width);
+                siteTitle.text(StringsManager.getVar(R.string.Mods_AuthorSite) + "\n");
 
-            Text site = PixelScene.createMultiline();
-            site.maxWidth(width);
-            site.text( siteUrl + "\n\n");
+                mainLayout.add(siteTitle);
 
-            site.hardlight(Window.TITLE_COLOR);
-            mainLayout.add(site);
+                Text site = PixelScene.createMultiline();
+                site.maxWidth(width);
+                site.text( siteUrl + "\n\n");
 
-            TouchArea siteTouch = new TouchArea(site) {
-                @Override
-                protected void onClick(Touchscreen.Touch touch) {
-                    Game.openUrl(StringsManager.getVar(R.string.Mods_AuthorSite), siteUrl);
-                }
-            };
-            add(siteTouch);
+                site.hardlight(Window.TITLE_COLOR);
+                mainLayout.add(site);
+
+                TouchArea siteTouch = new TouchArea(site) {
+                    @Override
+                    protected void onClick(Touchscreen.Touch touch) {
+                        Game.openUrl(StringsManager.getVar(R.string.Mods_AuthorSite), siteUrl);
+                    }
+                };
+                add(siteTouch);
+            }
+        } else {
+            // Use the old method for non-installed mods
+            Text modInfo = PixelScene.createMultiline(GuiProperties.titleFontSize());
+            modInfo.maxWidth(width);
+            modInfo.hardlight(Window.TITLE_COLOR);
+            modInfo.text(desc.name+"\n\n");
+            mainLayout.add(modInfo);
+
+            Text description = PixelScene.createMultiline(GuiProperties.titleFontSize());
+            description.maxWidth(width);
+            description.text(desc.description + "\n\n");
+            mainLayout.add(description);
+
+            Text author = PixelScene.createMultiline(GuiProperties.titleFontSize());
+            author.maxWidth(width);
+            author.text(StringsManager.getVar(R.string.Mods_CreatedBy) + "\n" +  desc.author + "\n\n");
+
+            mainLayout.add(author);
+
+            final String siteUrl = desc.url;
+
+            if (!siteUrl.isEmpty()) {
+                Text siteTitle = PixelScene.createMultiline(GuiProperties.titleFontSize());
+                siteTitle.maxWidth(width);
+                siteTitle.text(StringsManager.getVar(R.string.Mods_AuthorSite) + "\n");
+
+                mainLayout.add(siteTitle);
+
+                Text site = PixelScene.createMultiline();
+                site.maxWidth(width);
+                site.text( siteUrl + "\n\n");
+
+                site.hardlight(Window.TITLE_COLOR);
+                mainLayout.add(site);
+
+                TouchArea siteTouch = new TouchArea(site) {
+                    @Override
+                    protected void onClick(Touchscreen.Touch touch) {
+                        Game.openUrl(StringsManager.getVar(R.string.Mods_AuthorSite), siteUrl);
+                    }
+                };
+                add(siteTouch);
+            }
         }
 
         if(desc.installed && Utils.isAndroid()) {
@@ -103,5 +156,12 @@ public class WndModInfo extends Window {
                 }
             });
         }
+    }
+    
+    @Override
+    public void hide() {
+        super.hide();
+        // Restore previous mod context if we switched to an installed mod
+        GamePreferences.activeMod(prevMod);
     }
 }
